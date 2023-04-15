08edec3ab1f788d2a4d1c8554544fe5dc76a0ed9
Denis Yordanov <122925849+deyordanov@users.noreply.github.com>
2023-04-15 20:47:50 +0300

    Add files via upload

---
+++ C# Advanced/Inheritance - Exercise/Animals/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Channels;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string end;
+            List<Animal> animals = new List<Animal>();
+            while((end = Console.ReadLine()) != "Beast!")
+            {
+                string type = end;
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    switch (type)
+                    {
+                        case "Cat":
+                            Cat cat = new Cat(tokens[0], int.Parse(tokens[1]), tokens[2]);
+                            animals.Add(cat);
+                            break;
+                        case "Dog":
+                            Dog dog = new Dog(tokens[0], int.Parse(tokens[1]), tokens[2]);
+                            animals.Add(dog);
+                            break;
+                        case "Frog":
+                            Frog frog = new Frog(tokens[0], int.Parse(tokens[1]), tokens[2]);
+                            animals.Add(frog);
+                            break;
+                        case "Kitten":
+                            Kitten kitten = new Kitten(tokens[0], int.Parse(tokens[1]));
+                            animals.Add(kitten);
+                            break;
+                        case "Tomcat":
+                            Tomcat tomcat = new Tomcat(tokens[0], int.Parse(tokens[1]));
+                            animals.Add(tomcat);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }           
+            animals.ForEach(x => Console.WriteLine(x.ToString()));
+        }
+    }
+}
