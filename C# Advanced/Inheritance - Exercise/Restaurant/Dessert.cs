090d7e1c3b851f1f39f5a173ae081acc43112502
Denis Yordanov <122925849+deyordanov@users.noreply.github.com>
2023-04-15 20:47:25 +0300

    Add files via upload

---
+++ C# Advanced/Inheritance - Exercise/Restaurant/Dessert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Dessert : Food
+    {
+        public Dessert(string name, decimal price, double grams, double cals) : base(name, price, grams)
+        {
+            this.Calories = cals;
+        }
+        public double Calories { get; private set; }
+    }
+}
