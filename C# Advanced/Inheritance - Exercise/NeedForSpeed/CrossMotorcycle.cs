5c1e04fec7ec00d5f15d60bfb393b0039370b790
Denis Yordanov <122925849+deyordanov@users.noreply.github.com>
2023-04-15 20:46:54 +0300

    Add files via upload

---
+++ C# Advanced/Inheritance - Exercise/NeedForSpeed/CrossMotorcycle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class CrossMotorcycle : Motorcycle
+    {
+        public CrossMotorcycle(int power, double fuel) : base(power, fuel)
+        {
+        }
+    }
+}
