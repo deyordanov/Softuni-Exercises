3fdfcbe7350de366e0c64287ac7193e40466c7cc
Denis Yordanov <122925849+deyordanov@users.noreply.github.com>
2023-04-17 12:21:44 +0300

    Add files via upload

---
+++ C# Advanced/Encapsulation - Lab/People/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string first, string last, int age)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.Age = age;
+        }
+        public string FirstName
+        {
+            get => this.firstName;
+            set => this.firstName = value;
+        }
+        public string LastName
+        {
+            get => this.lastName;
+            set => this.lastName = value;
+        }
+        
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+}
