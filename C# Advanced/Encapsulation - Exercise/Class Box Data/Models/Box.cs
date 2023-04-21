4b8df971af50714b11cdb61885229fdbb1347ade
Denis Yordanov <deyordanov777@gmail.com>
2023-04-21 13:27:13 +0300

    Ex1

---
+++ C# Advanced/Encapsulation - Exercise/Class Box Data/Models/Box.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData.Models
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+        public double Length 
+        {
+            get => length; 
+            private set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        public double Width 
+        {
+            get => width;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get => height;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea() => 2 * (this.Length * (this.Width + this.Height) + this.Width * this.Height);
+        public double LateralSurfaceArea() => 2*this.Height*(this.Length + this.Width);
+        public double Volume() => this.Length * this.Width * this.Height;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {this.SurfaceArea():F2}");
+            sb.AppendLine($"Lateral Surface Area - {this.LateralSurfaceArea():F2}");
+            sb.AppendLine($"Volume - {this.Volume():F2}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Encapsulation - Exercise/Class Box Data/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassBoxData.Models;
+
+double length = double.Parse(Console.ReadLine());
+double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+try
+{
+    Box box = new Box(length, width, height);
+    Console.WriteLine(box);
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
