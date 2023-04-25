d828316a7c7a02662203c9222643539031bb2ddc
Denis Yordanov <deyordanov777@gmail.com>
2023-04-25 21:44:05 +0300

    Added A New Exercise

---
+++ C# OOP/Interfaces and Abstraction - Lab/Cars/Car/Seat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Car
+{
+    public class Seat : ICar
+    {
+        private string model;
+        private string color;
+
+        public Seat(string name, string color)
+        {
+            this.Model = name;
+            this.color = color;
+        }
+        public string Model 
+        { 
+            get => this.model;
+            set => this.model = value;
+        }
+        public string Color { 
+            get => this.color; 
+            set => this.color = value; 
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Start()}\n{this.Stop()}";
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction - Lab/Cars/Car/Tesla.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Car
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        private string model;
+        private string color;
+        private int battery;
+
+        public Tesla(string name, string color, int battery)
+        {
+            this.Model = name;
+            this.Color = color;
+            this.Battery = battery;
+        }
+        public string Model 
+        { 
+            get => this.model;
+            set => this.model = value; 
+        }
+        public string Color
+        {
+            get => this.color;
+            set => this.color = value;
+        }
+        public int Battery
+        {
+            get => this.battery;
+            set => this.battery = value;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Start()}\n{this.Stop()}";
+        }
+    }
+}
