1dfaf99de9b41e2f14f16dee0fe8b18a0da1e8aa
Denis Yordanov <deyordanov777@gmail.com>
2023-04-25 21:45:37 +0300

    Added A New Exercise

---
+++ C# OOP/Interfaces and Abstraction - Exercise/Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name 
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+    }
+}
