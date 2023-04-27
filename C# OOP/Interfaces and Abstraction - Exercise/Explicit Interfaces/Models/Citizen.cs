5f7e2e431624e3f01d1594718fda422e2f356cb1
Denis Yordanov <deyordanov777@gmail.com>
2023-04-27 17:47:53 +0300

    Added A New Exercise

---
+++ C# OOP/Interfaces and Abstraction - Exercise/Explicit Interfaces/Models/Citizen.cs
@@ -0,0 +1,35 @@
+﻿using ExplicitInterfaces.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterfaces.Models
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, int age)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Country { get; private set; }
+
+        string IPerson.GetName()
+        {
+            return Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {Name}";
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction - Exercise/Explicit Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExplicitInterfaces.Models;
+using ExplicitInterfaces.Models.Interfaces;
+
+string end;
+while((end = Console.ReadLine()) != "End")
+{
+    string[] input = end.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    IPerson citizenOne = new Citizen(input[0], input[1], int.Parse(input[2]));
+    IResident citizenTwo = new Citizen(input[0], input[1], int.Parse(input[2]));
+    Console.WriteLine(citizenOne.GetName());
+    Console.WriteLine(citizenTwo.GetName());
+}
