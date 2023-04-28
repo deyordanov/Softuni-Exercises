6bbf6cf46e8f3c886587a3d84af443a3cc393e9d
Denis Yordanov <deyordanov777@gmail.com>
2023-04-28 22:30:55 +0300

    Added A New Exercise

---
+++ JS Front-End/JS Syntax Fundamentals - Lab/05. Math Operations.js
@@ -0,0 +1,22 @@
+function mathOperations(num1, num2, symbol) {
+  switch (symbol) {
+    case "+":
+      console.log(num1 + num2);
+      break;
+    case "-":
+      console.log(num1 - num2);
+      break;
+    case "*":
+      console.log(num1 * num2);
+      break;
+    case "/":
+      console.log(num1 / num2);
+      break;
+    case "%":
+      console.log(num1 % num2);
+      break;
+    case "**":
+      console.log(num1 ** num2);
+      break;
+  }
+}
