7f844a759a67ad13486f11fc92a63dab89a95b19
Denis Yordanov <deyordanov777@gmail.com>
2023-04-30 22:29:37 +0300

    Added A New Exercise

---
+++ JS Front-End/JS Syntax Fundamentals - Exercises/12. Cooking by Numbers.js
@@ -0,0 +1,23 @@
+function cookingByNumbers(number, ...operations) {
+  for (let i = 0; i < operations.length; i++) {
+    switch (operations[i]) {
+      case "chop":
+        number /= 2;
+        break;
+      case "dice":
+        number = Math.sqrt(number);
+        break;
+      case "spice":
+        number += 1;
+        break;
+      case "bake":
+        number *= 3;
+        break;
+      case "fillet":
+        number -= number * 0.2;
+        break;
+    }
+
+    console.log(number);
+  }
+}
