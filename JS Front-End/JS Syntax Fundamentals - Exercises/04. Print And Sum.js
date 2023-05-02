44db200398f8f5417456cc80ff74877dced63be5
Denis Yordanov <deyordanov777@gmail.com>
2023-05-02 22:26:28 +0300

    Added A New Exercise

---
+++ JS Front-End/JS Syntax Fundamentals - Exercises/04. Print And Sum.js
@@ -0,0 +1,11 @@
+function printAndSum(num1, num2) {
+  let numbers = [];
+  let sum = 0;
+  for (let i = num1; i <= num2; i++) {
+    numbers.push(i);
+    sum += i;
+  }
+
+  console.log(numbers.join(" "));
+  console.log(`Sum: ${sum}`);
+}
