f91573d2f71d7927a01f4d3ec5e53f5b2b6f0be9
Denis Yordanov <deyordanov777@gmail.com>
2023-05-03 22:05:43 +0300

    Added A New Exercise

---
+++ C# OOP/Polymorphism - Lab/Animals/Models/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.Models
+{
+    public abstract class Animal
+    {
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; private set; }
+        public string FavouriteFood { get; private set; }
+        public virtual string ExplainSelf()
+        {
+            return this.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
