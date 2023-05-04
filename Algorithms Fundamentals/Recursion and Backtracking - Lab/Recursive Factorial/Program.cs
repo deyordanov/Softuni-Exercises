debec05d9d790314863e052b2dc172d228a1f363
Denis Yordanov <deyordanov777@gmail.com>
2023-05-04 21:27:45 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Recursion and Backtracking - Lab/Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(factorial));
+        }
+
+        private static int Factorial(int factorial)
+        {
+            if(factorial == 0)
+            {
+                return 1;
+            }
+
+            return factorial * Factorial(factorial - 1);
+        }
+    }
+}
