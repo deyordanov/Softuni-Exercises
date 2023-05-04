babea31228c10c4ec2d100befbd5a92d16ce7b5b
Denis Yordanov <deyordanov777@gmail.com>
2023-05-04 21:27:38 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Recursion and Backtracking - Lab/Generating 01 Vectors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3._Generating_01_Vectors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+            GenerateVector(vector, 0);
+        }
+
+        private static void GenerateVector(int[] vector, int index)
+        {
+            if(index >= vector.Length)
+            {
+                Console.WriteLine(string.Join("", vector));
+                return;
+            }
+
+            for(int i = 0; i <= 1; i++)
+            {
+                vector[index] = i;
+                GenerateVector(vector, index + 1);
+            }
+        }
+    }
+}
