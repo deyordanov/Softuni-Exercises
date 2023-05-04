4eb767440195daf0cf4eea6626aac7b544d1929d
Denis Yordanov <deyordanov777@gmail.com>
2023-05-04 21:25:05 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Recursion and Backtracking - Lab/Recursive Array Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(numbers, 0));
+        }
+
+        static int Sum(int[] numbers, int index)
+        {
+            if(index == numbers.Length - 1)
+            {
+                return numbers[index];
+            }
+
+            return numbers[index] + Sum(numbers, index + 1);    
+        }
+    }
+}
