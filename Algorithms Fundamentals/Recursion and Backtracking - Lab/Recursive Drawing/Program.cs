be50236f3eed152198091e974b96b5487ee6ebf9
Denis Yordanov <deyordanov777@gmail.com>
2023-05-04 21:26:31 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Recursion and Backtracking - Lab/Recursive Drawing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Recursive_Drawing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Figure(n);
+        }
+
+        static void Figure(int n)
+        {
+            if(n <= 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', n));
+
+            Figure(n - 1);
+
+            Console.WriteLine(new string('#', n));
+        }
+    }
+}
