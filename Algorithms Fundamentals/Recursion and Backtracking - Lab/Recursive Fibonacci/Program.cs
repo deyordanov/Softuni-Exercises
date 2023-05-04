e592d7ab4e6b2d33695fadb0eb6adf2f3aacdf44
Denis Yordanov <deyordanov777@gmail.com>
2023-05-04 21:29:40 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Recursion and Backtracking - Lab/Recursive Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07._Recursive_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fib = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(fib));
+        }
+
+        private static int Fibonacci(int fib)
+        {
+            if(fib == 0 || fib == 1)
+            {
+                return 1;
+            }
+
+            return Fibonacci(fib - 1) + Fibonacci(fib - 2);
+        }
+    }
+}
