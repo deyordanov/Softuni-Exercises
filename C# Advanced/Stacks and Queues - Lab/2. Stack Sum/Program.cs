cefe73377093fa09581ef936221ad1075c7d702e
Denis Yordanov <deyordanov777@gmail.com>
2023-05-10 23:42:17 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Stack<int> stack = new Stack<int>(numbers);
+string end;
+while((end = Console.ReadLine().ToLower()) != "end")
+{
+    string[] input = end.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    switch (input[0])
+    {
+        case "add":
+            stack.Push(int.Parse(input[1]));
+            stack.Push(int.Parse(input[2]));
+        break;
+        case "remove":
+            int numberOfElements = int.Parse(input[1]);
+            if(stack.Count >= numberOfElements)
+                for (int i = 0; i < numberOfElements; i++)
+                {
+                    stack.Pop();
+                }
+            break;
+    }
+}
+Console.WriteLine($"Sum: {stack.Sum()}");
