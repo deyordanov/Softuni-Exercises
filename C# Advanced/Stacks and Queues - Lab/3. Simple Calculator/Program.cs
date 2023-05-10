83a871f6e7c463e87ba13700ab3289941c6a60d8
Denis Yordanov <deyordanov777@gmail.com>
2023-05-10 23:42:34 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Array.Reverse(tokens);
+Stack<string> stack = new Stack<string>(tokens);
+int result = int.Parse(stack.Pop());
+while(stack.Count > 0)
+{
+    string symbol = stack.Pop();
+    int currentNumber = int.Parse(stack.Pop());
+    switch (symbol)
+    {
+        case "+":
+            result += currentNumber;
+            break;
+        case "-":
+            result -= currentNumber;
+            break;
+    }
+}
+Console.WriteLine(result);
