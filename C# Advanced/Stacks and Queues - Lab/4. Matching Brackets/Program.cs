a6120996e10339cc7e9ec915e16d4934da978ca2
Denis Yordanov <deyordanov777@gmail.com>
2023-05-10 23:42:52 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,14 @@
+﻿string expression = Console.ReadLine();
+Stack<int> stack = new Stack<int>();
+for (int i = 0; i < expression.Length; i++)
+{
+    if (expression[i] == '(')
+    {
+        stack.Push(i);
+    }
+    else if (expression[i] == ')')
+    {
+        int start = stack.Pop();
+        Console.WriteLine(expression.Substring(start, i - start + 1));
+    }
+}
