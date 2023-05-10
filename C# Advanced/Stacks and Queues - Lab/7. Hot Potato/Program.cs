b3d043a8cec34ad34442c11e90d4c8bd25ce4d7e
Denis Yordanov <deyordanov777@gmail.com>
2023-05-10 23:43:54 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,18 @@
+﻿Queue<string> kids = new Queue<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+int n = int.Parse(Console.ReadLine());
+while(kids.Count > 1)
+{
+    for(int i = 0; i < n; i++)
+    {
+        string currentKid = kids.Dequeue(); 
+        if(i == n - 1)
+        {
+            Console.WriteLine($"Removed {currentKid}");
+        }
+        else
+        {
+            kids.Enqueue(currentKid);
+        }
+    }  
+}
+Console.WriteLine($"Last is {kids.Dequeue()}");
