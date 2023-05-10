9cc5a89b7e0aad2c3fb3c97820420c2a6a457bcb
Denis Yordanov <deyordanov777@gmail.com>
2023-05-10 23:44:15 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,22 @@
+﻿int carsThatCanPass = int.Parse(Console.ReadLine());
+Queue<string> queue = new Queue<string>();
+string end;
+int carsPassed = 0;
+while((end = Console.ReadLine()) != "end")
+{
+    string input = end;
+    if(input == "green")
+    {
+        int carsToPass = queue.Count < carsThatCanPass ? queue.Count : carsThatCanPass;
+        for (int i = 0; i < carsToPass; i++)
+        {
+            Console.WriteLine($"{queue.Dequeue()} passed!");
+            carsPassed++;
+        }
+    }
+    else
+    {
+        queue.Enqueue(input);
+    }
+}
+Console.WriteLine($"{carsPassed} cars passed the crossroads.");
