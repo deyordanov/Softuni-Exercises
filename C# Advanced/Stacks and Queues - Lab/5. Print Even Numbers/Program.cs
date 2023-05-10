a0e02be0a5c74aeaaf0ae090061c380d9bd497d9
Denis Yordanov <deyordanov777@gmail.com>
2023-05-10 23:43:17 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+Queue<int> queue = new Queue<int>(numbers.Where(x => x % 2 == 0));
+while(queue.Count > 0)
+{
+    string output = queue.Count == 1 ? queue.Dequeue().ToString() : queue.Dequeue() + ", ";
+    Console.Write(output);
+}
