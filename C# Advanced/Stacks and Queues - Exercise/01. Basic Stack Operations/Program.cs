50a49ca72cbcbbcfd8ebab7af9ab95a6bdba6529
Denis Yordanov <deyordanov777@gmail.com>
2023-05-11 21:01:27 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+Stack<int> stack = new Stack<int>();
+for(int i = 0; i < input[0]; i++)
+{
+    stack.Push(numbers[i]);
+}
+for(int i = 0; i < input[1]; i++)
+{
+    stack.Pop();
+}
+Console.WriteLine(Result(stack, input[2]));
+
+static string Result(Stack<int> stack, int numberToFind)
+{
+    if (stack.Count == 0)
+        return "0";
+
+    return stack.Any(x => x == numberToFind) ? "true" : $"{stack.Min()}";
+}
