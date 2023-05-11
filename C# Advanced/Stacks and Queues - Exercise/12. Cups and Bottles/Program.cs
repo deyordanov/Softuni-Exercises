53cb61f26b82a9713f3c11ff2445e4758d73eeb2
Denis Yordanov <deyordanov777@gmail.com>
2023-05-11 21:04:52 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,21 @@
+﻿Queue<int> cups = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+Stack<int> bottles = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+int wastedWater = 0;
+int currentCup = cups.Peek();
+while (cups.Count > 0 && bottles.Count > 0)
+{
+    currentCup -= bottles.Pop();
+
+    if(currentCup <= 0)
+    {
+        wastedWater += Math.Abs(currentCup);
+        cups.Dequeue();
+        if(cups.Count > 0)
+            currentCup = cups.Peek();
+    }
+}
+string output = cups.Count == 0 ? $"Bottles: {string.Join(" ", bottles)}"
+    : $"Cups: {string.Join(" ", cups)}";
+Console.WriteLine(output);
+Console.WriteLine($"Wasted litters of water: {wastedWater}");
+ 
