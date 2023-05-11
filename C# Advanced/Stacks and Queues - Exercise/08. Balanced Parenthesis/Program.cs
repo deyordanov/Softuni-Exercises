8163fdad9eda4bbf9dcd06599c880caf73d4322a
Denis Yordanov <deyordanov777@gmail.com>
2023-05-11 21:04:37 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,38 @@
+﻿string parentheses = Console.ReadLine();
+Stack<char> stack = new Stack<char>();
+for (int i = 0; i < parentheses.Length; i++)
+{
+    if (parentheses[i] == '(' || parentheses[i] == '[' || parentheses[i] == '{')
+    {
+        stack.Push(parentheses[i]);
+    }
+    else
+    {
+        char opening = stack.Count == 0 ? 'n' : stack.Pop();
+        switch (opening)
+        {
+            case '(':
+                Balanced(parentheses[i], ')');
+                break;
+            case '[':
+                Balanced(parentheses[i], ']');
+                break;
+            case '{':
+                Balanced(parentheses[i], '}');
+                break;
+            default:
+                Balanced(parentheses[i], 'n');
+                break;
+        }
+    }
+}
+Console.WriteLine("YES");
+
+static void Balanced(char currentClosingBracket, char bracket)
+{
+    if (currentClosingBracket != bracket)
+    {
+        Console.WriteLine("NO");
+        Environment.Exit(0);
+    }
+}
