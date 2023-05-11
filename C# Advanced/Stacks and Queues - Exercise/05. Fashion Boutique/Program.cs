131c057f1e0536a2681658fc7b541b1c04c8b522
Denis Yordanov <deyordanov777@gmail.com>
2023-05-11 21:03:02 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,17 @@
+﻿Stack<int> clothes = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+int rackCapacity = int.Parse(Console.ReadLine());
+int currentRack = 0;
+int racksUsed = 0;
+while(clothes.Count > 0)
+{
+    if(currentRack + clothes.Peek() <= rackCapacity)
+    {
+        currentRack += clothes.Pop();
+    }
+    else
+    {
+        currentRack = 0;
+        racksUsed++;
+    }
+}
+Console.WriteLine(racksUsed + 1);
