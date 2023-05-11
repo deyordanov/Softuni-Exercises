2dbde1d6b4e86bbed7670f8a55028414ea9c3a6d
Denis Yordanov <deyordanov777@gmail.com>
2023-05-11 21:01:54 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+Queue<int> queue = new Queue<int>();    
+for(int i = 0; i < input[0] + input[1]; i++)
+{
+    string command = i < input[0] ? "Enqueue" : "Dequeue";
+    switch (command)
+    {
+        case "Enqueue":
+            queue.Enqueue(numbers[i]);
+            break;
+        case "Dequeue":
+            queue.Dequeue();
+            break;
+    }
+}
+Console.WriteLine(Result(queue, input[2]));
+
+static string Result(Queue<int> queue, int numberToFind)
+{
+    if (queue.Count == 0)
+        return "0";
+
+    return queue.Any(x => x == numberToFind) ? "true" : $"{queue.Min()}";
+}
