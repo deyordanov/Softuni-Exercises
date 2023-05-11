4762939ac0cf54b4741e69c1def2d3cf61f53b2d
Denis Yordanov <deyordanov777@gmail.com>
2023-05-11 21:03:06 +0300

    Added A New Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,16 @@
+﻿int foodForTheDay = int.Parse(Console.ReadLine());
+Queue<int> clients = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+Console.WriteLine(clients.Max());
+while(clients.Count > 0)
+{
+    if (foodForTheDay >= clients.Peek())
+    {
+        foodForTheDay -= clients.Dequeue();
+    }
+    else
+    {
+        Console.WriteLine($"Orders left: {string.Join(" ", clients)}");
+        Environment.Exit(0);
+    }
+}
+Console.WriteLine("Orders complete");
