47521f426a699601be6169ef3e014c15f3ebc8bd
Denis Yordanov <deyordanov777@gmail.com>
2023-05-13 22:23:56 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Utilities/Validators/TimeValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace LogForU.Core.Utilities.Validators
+{
+    using System.Globalization;
+
+    public static class TimeValidator
+    {
+        private static readonly ICollection<string> formats = new HashSet<string>() { "M/dd/yyyy h:mm:ss tt" };
+        public static bool ValidateTime(string time)
+        {
+            foreach (string format in formats)
+            {
+                if (DateTime.TryParseExact(time, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void AddFormat(string format)
+            => formats.Add(format);
+    }
+}
