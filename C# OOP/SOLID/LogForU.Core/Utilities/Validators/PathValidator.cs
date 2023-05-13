e6cdf7e6fd3a3cd6387b04a88c19c8468d1c6a65
Denis Yordanov <deyordanov777@gmail.com>
2023-05-13 22:23:52 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Utilities/Validators/PathValidator.cs
@@ -0,0 +1,8 @@
+﻿namespace LogForU.Core.Utilities.Validators
+{
+    public static class PathValidator
+    {
+        public static bool ValidatePath(string path)
+            => Directory.Exists(path);
+    }
+}
