3ed88c5788544680c77bef3302aff0aad5050f6b
Denis Yordanov <deyordanov777@gmail.com>
2023-05-17 22:17:49 +0300

    Added A New Exercise

---
+++ C# Advanced/Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,17 @@
+﻿int size = int.Parse(Console.ReadLine());
+int[,] matrix = new int[size, size];
+for (int row = 0; row < size; row++)
+{
+    int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    for (int col = 0; col < size; col++)
+    {
+        matrix[row, col] = numbers[col];
+    }
+}
+
+int diagonalSum = 0;
+for(int row = 0; row < size; row++)
+{
+    diagonalSum += matrix[row, row];
+}
+Console.WriteLine(diagonalSum);
