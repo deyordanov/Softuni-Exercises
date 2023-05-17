f270dd6804ee46a6f62ac56a36a74df66d8f300d
Denis Yordanov <deyordanov777@gmail.com>
2023-05-17 22:16:29 +0300

    Added A New Exercise

---
+++ C# Advanced/Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] size = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[,] matrix = new int[size[0],size[1]];
+int sum = 0;
+for (int row = 0; row < size[0]; row++)
+{
+    int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    for (int col = 0; col < size[1]; col++)
+    {
+        matrix[row, col] = numbers[col];
+        sum += numbers[col];
+    }
+}
+
+Console.WriteLine(size[0]);
+Console.WriteLine(size[1]);
+Console.WriteLine(sum);
