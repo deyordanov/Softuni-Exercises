2f80fd221187678cb2f8317850f1cf2aa46b8853
Denis Yordanov <deyordanov777@gmail.com>
2023-05-17 22:18:06 +0300

    Added A New Exercise

---
+++ C# Advanced/Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿long rows = long.Parse(Console.ReadLine());
+long[][] jagged = new long[rows][];
+jagged[0] = new long[1] { 1 };
+Console.WriteLine(1);
+for (long row = 1; row < rows; row++)
+{
+    jagged[row] = new long[row + 1];
+    for(long i = 0; i < jagged[row].Length; i++)
+    {
+        if(i - 1 < 0)
+        {
+            jagged[row][i] = jagged[row - 1][i];
+        }
+        else if(i >= jagged[row - 1].Length)
+        {
+            jagged[row][i] = jagged[row - 1][i - 1];
+        }
+        else
+        {
+            jagged[row][i] = jagged[row - 1][i] + jagged[row - 1][i - 1];
+        }
+    }
+    Console.WriteLine(string.Join(" ", jagged[row]));
+}
