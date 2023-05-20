66b18106d3270c0c1ba8cab8abc1aafc7c627234
Denis Yordanov <deyordanov777@gmail.com>
2023-05-20 22:21:53 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Models/Interfaces/IMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace LogForU.Core.Models.Interfaces
+{
+    using Enums;
+
+    public interface IMessage
+    {
+        string Time { get; }
+        string Text { get; }
+        ReportLevel ReportLevel { get; }
+    }
+}
