bf176671bd9edd61f4e74ab4931658695141e4be
Denis Yordanov <deyordanov777@gmail.com>
2023-05-23 22:21:07 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Layouts/SimpleLayout.cs
@@ -0,0 +1,10 @@
+﻿namespace LogForU.Core.Layouts
+{
+    using Interfaces;
+
+    public class SimpleLayout : ILayout
+    {
+        private const string SimpleFormat = "{0} - {1} - {2}";
+        public string Format => SimpleFormat;
+    }
+}
