ffafaca9a4d5c0c31acb1a8c9741ca3dd7c6a979
Denis Yordanov <deyordanov777@gmail.com>
2023-05-24 22:20:23 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Exceptions/InvalidTimeFormatException.cs
@@ -0,0 +1,13 @@
+﻿namespace LogForU.Core.Exceptions
+{
+    public class InvalidTimeFormatException : Exception
+    {
+        private const string DefaultMessage = "Time format is not supported!";
+
+        public InvalidTimeFormatException()
+            :base(DefaultMessage) { }
+
+        public InvalidTimeFormatException(string message)
+            :base(message) { }
+    }
+}
