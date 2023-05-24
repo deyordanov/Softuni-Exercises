febf1dfe1f2b39660f7a206c4001df5e897df4f3
Denis Yordanov <deyordanov777@gmail.com>
2023-05-24 22:20:20 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Exceptions/InvalidTimeException.cs
@@ -0,0 +1,13 @@
+﻿namespace LogForU.Core.Exceptions
+{
+    public class InvalidTimeException : Exception
+    {
+        private const string DefaultMessage = "Time cannot be null or whitespace!";
+
+        public InvalidTimeException()   
+            : base(DefaultMessage) { }
+
+        public InvalidTimeException(string message)
+            : base(message) { }
+    }
+}
