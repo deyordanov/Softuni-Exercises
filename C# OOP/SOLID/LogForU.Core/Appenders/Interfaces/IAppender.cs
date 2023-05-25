e1b6068b5c01e8a9fa75b4fb9c3d424d5d7949fa
Denis Yordanov <deyordanov777@gmail.com>
2023-05-25 22:19:41 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Appenders/Interfaces/IAppender.cs
@@ -0,0 +1,14 @@
+﻿namespace LogForU.Core.Appenders.Interfaces
+{
+    using Enums;
+    using Layouts.Interfaces;
+    using Models;
+
+    public interface IAppender
+    {
+        ILayout Layout { get; }
+        int MessagesAppended { get; }
+        ReportLevel ReportLevel { get; set; }
+        void AppendMessage(Message message);
+    }
+}
