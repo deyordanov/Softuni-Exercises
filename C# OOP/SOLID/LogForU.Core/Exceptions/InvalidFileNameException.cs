e397ba600449e8a4f0b04250066b5610b3243227
Denis Yordanov <deyordanov777@gmail.com>
2023-05-25 22:20:02 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Exceptions/InvalidFileNameException.cs
@@ -0,0 +1,13 @@
+﻿namespace LogForU.Core.Exceptions
+{
+    public class InvalidFileNameException : Exception
+    {
+        private const string DefaultMessage = "File name cannot be null or whitespace!";
+
+        public InvalidFileNameException()
+            : base(DefaultMessage) { }
+
+        public InvalidFileNameException(string message)
+            : base(message) { }       
+    }
+}
