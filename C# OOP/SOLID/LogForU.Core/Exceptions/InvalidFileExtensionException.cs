551387b8f876cf8e5cc87dc2f61c5ed3d0f1c3ec
Denis Yordanov <deyordanov777@gmail.com>
2023-05-25 22:19:59 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Exceptions/InvalidFileExtensionException.cs
@@ -0,0 +1,13 @@
+﻿namespace LogForU.Core.Exceptions
+{
+    public class InvalidFileExtensionException : Exception
+    {
+        private const string DefaultMessage = "File extension cannot be null or whitespace!";
+
+        public InvalidFileExtensionException()
+            : base(DefaultMessage) { }
+
+        public InvalidFileExtensionException(string message)
+            : base(message) { }       
+    }
+}
