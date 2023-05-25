a97eb1eeca0b9270bc0370ae6a9057a560a1fc9c
Denis Yordanov <deyordanov777@gmail.com>
2023-05-25 22:20:06 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Exceptions/InvalidFilePathException.cs
@@ -0,0 +1,13 @@
+﻿namespace LogForU.Core.Exceptions
+{
+    public class InvalidFilePathException : Exception
+    {
+        private const string DefaultMessage = "File path does not exist!";
+
+        public InvalidFilePathException()
+            : base(DefaultMessage) { }
+
+        public InvalidFilePathException(string message)
+            : base(message) { }       
+    }
+}
