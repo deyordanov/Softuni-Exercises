9f3fefcab287c6c2c2a7275f3c6af7a01765e664
Denis Yordanov <deyordanov777@gmail.com>
2023-05-25 22:19:54 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Enums/ReportLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace LogForU.Core.Enums
+{
+    public enum ReportLevel
+    {
+        Info = 1,
+        Warning = 2,
+        Error = 3,
+        Critical = 4,
+        Fatal = 5,
+    }
+}
