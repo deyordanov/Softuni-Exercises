5662ca3a3b84f3f90fe36e40a99cb2490e6b252f
Denis Yordanov <deyordanov777@gmail.com>
2023-05-26 22:19:29 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.Core/Appenders/ConsoleAppender.cs
@@ -0,0 +1,27 @@
+﻿namespace LogForU.Core.Appenders
+{
+    using Enums;
+    using Interfaces;
+    using Layouts.Interfaces;
+    using Models;
+
+    public class ConsoleAppender : IAppender
+    {
+        public ConsoleAppender(ILayout layout, ReportLevel reportLevel = ReportLevel.Info)
+        {
+            this.Layout = layout;
+            this.ReportLevel = reportLevel;
+        }
+
+        public ILayout Layout { get; private set; }
+        public int MessagesAppended { get; private set; }
+        public ReportLevel ReportLevel { get; set; }
+
+        public void AppendMessage(Message message)
+        {
+            Console.WriteLine(string.Format(Layout.Format, message.Time, message.ReportLevel, message.Text));
+
+            this.MessagesAppended++;
+        }
+    }
+}
