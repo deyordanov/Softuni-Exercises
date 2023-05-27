43607f9b386c6891e3c7007baf2f289b95cc5fbe
Denis Yordanov <deyordanov777@gmail.com>
2023-05-27 12:07:03 +0300

    Added A New Exercise

---
+++ C# OOP/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+HashSet<string> set = new HashSet<string>();
+for(int i = 0; i < n; i++)
+{
+    string[] elements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    foreach(string element in elements)
+    {
+        set.Add(element);
+    }
+}
+set = set.OrderBy(x => x).ToHashSet();
+Console.WriteLine(string.Join(" ", set));
