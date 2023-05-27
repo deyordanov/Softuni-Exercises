31321cf0603c4fc77ceff29dca6491d788f0a51a
Denis Yordanov <deyordanov777@gmail.com>
2023-05-27 12:07:06 +0300

    Added A New Exercise

---
+++ C# OOP/Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<int, int> result = new Dictionary<int, int>();
+for(int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    if (!result.ContainsKey(number))
+        result.Add(number, 0);
+    result[number]++;
+}
+
+Console.WriteLine(string.Join(" ", result.Keys.Where(x => result[x] % 2 == 0)));
