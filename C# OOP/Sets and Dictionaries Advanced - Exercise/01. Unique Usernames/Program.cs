6e8a05300159910636e9ea980924e6cb3295043a
Denis Yordanov <deyordanov777@gmail.com>
2023-05-27 12:06:56 +0300

    Added A New Exercise

---
+++ C# OOP/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+HashSet<string> set = new HashSet<string>();
+for(int i = 0; i < n; i++)
+{
+    set.Add(Console.ReadLine());
+}
+foreach(string username in set)
+{
+    Console.WriteLine(username);
+}
