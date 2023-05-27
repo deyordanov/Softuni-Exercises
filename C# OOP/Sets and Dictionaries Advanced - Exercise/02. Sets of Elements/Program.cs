8c6cc654e9f54bb0239f0b367505484b629f22be
Denis Yordanov <deyordanov777@gmail.com>
2023-05-27 12:07:00 +0300

    Added A New Exercise

---
+++ C# OOP/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] lengths = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+HashSet<double> one = new HashSet<double>();
+HashSet<double> two = new HashSet<double>();
+for(int i = 0; i < lengths[0]; i++)
+{
+    one.Add(int.Parse(Console.ReadLine()));
+}
+for(int i = 0; i < lengths[1]; i++)
+{
+    two.Add(int.Parse(Console.ReadLine()));
+}
+one.IntersectWith(two);
+Console.WriteLine(string.Join(" ", one));
