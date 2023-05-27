0f22a99a43b8d4f8ea00d0788a74fec725b6aa00
Denis Yordanov <deyordanov777@gmail.com>
2023-05-27 12:07:08 +0300

    Added A New Exercise

---
+++ C# OOP/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,9 @@
+﻿char[] input = Console.ReadLine().ToCharArray();
+SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+foreach (char symbol in input)
+{
+    if (!symbols.ContainsKey(symbol))
+        symbols.Add(symbol, 0);
+    symbols[symbol]++;
+}
+Console.WriteLine(string.Join(Environment.NewLine, symbols.Select(x => $"{x.Key}: {symbols[x.Key]} time/s")));
