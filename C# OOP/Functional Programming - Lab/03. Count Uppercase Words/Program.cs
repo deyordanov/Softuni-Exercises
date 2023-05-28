914a7764519a0a3322132639d4e7572207de5f98
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 21:57:56 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Func<string, bool> predicate = x => char.IsUpper(x[0]);
+foreach(string word in words.Where(predicate))
+{
+    Console.WriteLine(word);
+}
