dd3a46aaf441d2b68a3b80bd40c2d6fd6ecc84e9
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 11:24:45 +0300

    Added A New Exercise

---
+++ C# OOP/Streams, Files and Directories - Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+
+namespace LineNumbers;
+public class LineNumbers
+{
+    static void Main()
+    {
+        string inputPath = @"..\..\..\Files\input.txt";
+        string outputPath = @"..\..\..\Files\output.txt";
+
+        RewriteFileWithLineNumbers(inputPath, outputPath);
+    }
+
+    public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+    {
+        int row = 0;
+        using(StreamReader reader = new StreamReader(inputFilePath))
+        {
+            using(StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    writer.WriteLine($"{++row}. {reader.ReadLine()}");
+                }
+            }
+        }
+    }
+}
