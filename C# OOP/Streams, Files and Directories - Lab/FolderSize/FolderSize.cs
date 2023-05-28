1095edce7bbb485030a367fac2060f10e1f21842
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 11:24:42 +0300

    Added A New Exercise

---
+++ C# OOP/Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,37 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            DirectoryInfo directory = new DirectoryInfo(folderPath);
+            double sum = 0;
+            foreach(FileInfo file in directory.GetFiles())
+            {
+                sum += file.Length;
+            }
+
+            foreach(DirectoryInfo dir in directory.GetDirectories())
+            {
+                foreach(FileInfo file in dir.GetFiles())
+                {
+                    sum += file.Length;
+                }
+            }
+
+            double sumInKb = sum / 1024;
+
+            File.WriteAllText(outputFilePath, $"{sumInKb} KB");
+        }
+    }
+}
