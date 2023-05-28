fd9fdf3060a3468a25abbdd00a26dafeb37f4fd8
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 11:24:49 +0300

    Added A New Exercise

---
+++ C# OOP/Streams, Files and Directories - Lab/MergeFiles/MergeFiles.cs
@@ -0,0 +1,48 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            int count = 1;
+            
+            using (StreamReader one = new StreamReader(firstInputFilePath))
+            {
+                using(StreamReader two = new StreamReader(secondInputFilePath))
+                {
+                    using(StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+                        while (true)
+                        {
+                            string lineOne = one.ReadLine();
+                            string lineTwo = two.ReadLine();
+                            if(lineOne == null && lineTwo == null)
+                            {
+                                break;
+                            }
+                            if(lineOne != null)
+                            {
+                                writer.WriteLine(lineOne);
+                            }
+                            if(lineTwo != null)
+                            {
+                                writer.WriteLine(lineTwo);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
