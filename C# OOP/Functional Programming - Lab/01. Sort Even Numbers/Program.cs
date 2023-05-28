7cfa9f606714730bb6487c26a20e3497e3cb1d70
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 21:57:48 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Func<string, int> parse = x => int.Parse(x);
+Func<int, bool> order = x => x % 2 == 0;
+int[] numbers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(parse)
+    .Where(order)
+    .OrderBy(x => x)
+    .ToArray();
+Console.WriteLine(string.Join(", ", numbers));
