a923f7770a0ff8949618f73353cdeea16faff3c8
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 11:24:52 +0300

    Added A New Exercise

---
+++ C# OOP/Streams, Files and Directories - Lab/OddLines/OddLines.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace OddLines;
+	
+public class OddLines
+{
+    static void Main()
+    {
+        string inputFilePath = @"..\..\..\Files\input.txt";
+        string outputFilePath = @"..\..\..\Files\output.txt";
+
+        ExtractOddLines(inputFilePath, outputFilePath);
+    }
+
+    public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+    {
+        int row = 0;
+        using(StreamReader reader = new StreamReader(inputFilePath))
+        {
+            using(StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();    
+                    if (row++ % 2 != 0)
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+        
+
+    }
+}
