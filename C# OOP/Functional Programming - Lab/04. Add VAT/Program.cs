b59eb0691b44d11eda48b5f15eaaca299c4b3c07
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 21:58:00 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,10 @@
+﻿Func<double, double> vat = x => x += x * 0.2;
+Func<double, string> func = x => $"{x:F2}";
+
+double[] numbers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .Select(vat)
+    .ToArray();
+
+Console.WriteLine(string.Join(Environment.NewLine, numbers.Select(func)));
