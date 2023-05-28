d296c3c487cca24e20cb92be48a7de72e0070583
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 21:57:52 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+Func<int[], int> sum = Sum;
+Func<int[], int> count = Count;
+Console.WriteLine(count(numbers));
+Console.WriteLine(sum(numbers));
+int Sum(int[] numbers)
+{
+    int sum = 0;
+    foreach(int number in numbers)
+    {
+        sum += number;
+    }
+    return sum;
+}
+
+
+int Count(int[] numbers)
+{
+    int count = 0;
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        count++;
+    }
+    return count;
+}
