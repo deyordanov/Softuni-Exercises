5bfac93dbef1b739d8af4d398362cb6fb14ef0cd
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 17:41:01 +0300

    Added A New Exercise

---
+++ C# OOP/Streams, Files and Directories - Exercises/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,30 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        public static object StreamFile { get; private set; }
+
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            FileStream reader = new FileStream(inputFilePath, FileMode.Open);
+            FileStream writer = new FileStream(outputFilePath, FileMode.Create);
+            byte[] buffer = new byte[1024];
+            int size = 0;
+            while ((size = reader.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                writer.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
