a4d5b7804a0e01645cf8d8ecf10fa6d5b20c05e9
Denis Yordanov <deyordanov777@gmail.com>
2023-05-28 11:24:58 +0300

    Added A New Exercise

---
+++ C# OOP/Streams, Files and Directories - Lab/WordCount/WordCount.cs
@@ -0,0 +1,54 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> occurances = new Dictionary<string, int>();
+            using(StreamReader words = new StreamReader(wordsFilePath))
+            {
+                using (StreamReader text = new StreamReader(textFilePath))
+                {
+                    using (StreamWriter output = new StreamWriter(outputFilePath))
+                    {
+                        List<string> wordsToFind = words.ReadLine().Split(" ").ToList();
+                        foreach(string word in wordsToFind)
+                        {
+                            if (!occurances.ContainsKey(word))
+                            {
+                                occurances.Add(word, 0);
+                            }
+                        }
+                        while (!text.EndOfStream)
+                        {
+                            foreach(string word in text.ReadLine().Split(new[] { ' ', '.', ',', '-', '?', '!' }))
+                            {
+                                if (wordsToFind.Contains(word.ToLower()))
+                                {
+                                    occurances[word.ToLower()]++;
+                                }
+                            }
+                        }
+                        foreach(var occurance in occurances.OrderByDescending(x => x.Value))
+                        {
+                            output.WriteLine($"{occurance.Key} - {occurance.Value}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
