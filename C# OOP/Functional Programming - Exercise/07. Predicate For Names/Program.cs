aed9bc2812be8252b0575a143e1e3964b8035652
Denis Yordanov <deyordanov777@gmail.com>
2023-05-29 11:04:29 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,4 @@
+﻿int length = int.Parse(Console.ReadLine());
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Func<string, int, bool> filter = (name, length) => { return name.Length <= length ? true : false; };
+Console.WriteLine(string.Join(Environment.NewLine, names.Where(x => filter(x, length))));
