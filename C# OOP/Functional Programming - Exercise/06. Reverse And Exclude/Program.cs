a8385d2e9fe70d3c49baaf3fe16a800c9f7f8efd
Denis Yordanov <deyordanov777@gmail.com>
2023-05-29 11:04:25 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+Func<int[], int[]> reverse = array =>
+{
+    int[] reversed = new int[array.Length];
+    int index = 0;
+    for (int i = reversed.Length - 1; i >= 0; i--)
+    {
+        reversed[index++] = array[i];
+    }
+    return reversed;
+};
+
+Func<int, int, bool> divide = (number, dividor) => number % dividor != 0;
+
+int dividor = int.Parse(Console.ReadLine());
+numbers = reverse(numbers);
+Console.WriteLine(string.Join(" ", numbers.Where(x => divide(x, dividor))));
