4007a2c90e4945d9cf1acf2a6fcc6ac21d5a2a79
Denis Yordanov <deyordanov777@gmail.com>
2023-05-29 11:04:41 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿int sum = int.Parse(Console.ReadLine());
+string[] people = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Func<int, string, bool> func = (int sum, string name) =>
+{
+    int charSum = 0;
+    foreach(char letter in name)
+    {
+        charSum += letter;
+    }
+    return charSum >= sum ? true : false;
+};
+
+Print(people, func);
+
+void Print(string[] people, Func<int, string, bool> function)
+{
+    Console.WriteLine(people.Where(x => function(sum ,x)).First());
+}
