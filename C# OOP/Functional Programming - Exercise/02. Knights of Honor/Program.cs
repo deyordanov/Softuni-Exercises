93cee1ea2d0d0cf212e1766d1aadc4e2232d003b
Denis Yordanov <deyordanov777@gmail.com>
2023-05-29 11:04:13 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] people = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Func<string, string> select = x => $"Sir {x}";
+Action<string[]> print = x => Console.WriteLine(string.Join(Environment.NewLine, x.Select(select)));
+print(people);
