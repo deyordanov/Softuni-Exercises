b682c5506380d8d7c54aa2ed631cfa2ea71feae6
Denis Yordanov <deyordanov777@gmail.com>
2023-05-29 11:04:35 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Exercise/09. Predicate Party!/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+List<string> people = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+string party;
+while((party = Console.ReadLine()) != "Party!")
+{
+    string[] input = party.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    if (input[0] == "Remove")
+    {
+        people.RemoveAll(GetPredicate(input[1], input[2]));
+    }
+    else
+    {
+        List<string> peopleToDouble = people.FindAll(GetPredicate(input[1], input[2]));
+        foreach(string person in peopleToDouble)
+        {
+            int index = people.IndexOf(person);
+            people.Insert(index, person);
+        }
+    }
+}
+
+string output = people.Count == 0
+    ? "Nobody is going to the party!"
+    : $"{string.Join(", ", people)} are going to the party!";
+Console.WriteLine(output);
+
+Predicate<string> GetPredicate(string command, string value)
+{
+    switch (command)
+    {
+        case "StartsWith":
+            return n => n.StartsWith(value);
+        case "EndsWith":
+            return n => n.EndsWith(value);  
+        case "Length":
+            return n => n.Length == int.Parse(value);
+        default:
+            return null;
+    }
+}
