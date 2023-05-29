0cf5b9f01adaba7067fa710672eb70c1ba89b917
Denis Yordanov <deyordanov777@gmail.com>
2023-05-29 11:04:32 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,22 @@
+﻿int range = int.Parse(Console.ReadLine());
+int[] dividors = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+Func<int, int[]> numsInRange = range =>
+{
+    int[] numbers = new int[range];
+    for(int i = 1; i <= range; i++)
+    {
+        numbers[i - 1] = i;
+    }
+    return numbers;
+};
+
+Func<int[], int, bool> filter = (dividors, number) =>
+{
+    return dividors.All(x => number % x == 0) ? true : false;
+};
+
+int[] numbers = numsInRange(range);
+Console.WriteLine(string.Join(" ", numbers.Where(x => filter(dividors, x))));
