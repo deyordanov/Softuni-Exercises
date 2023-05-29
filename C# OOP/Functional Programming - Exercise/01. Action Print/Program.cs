7a2dc082fe250efefe44230bc0be976ae0364a1c
Denis Yordanov <deyordanov777@gmail.com>
2023-05-29 11:04:08 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] people = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Action<string[]> print = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+
+print(people);
