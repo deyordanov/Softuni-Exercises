909b667d1f72c556bc0a2e9be161cd46da31c5c9
Denis Yordanov <deyordanov777@gmail.com>
2023-05-29 11:04:16 +0300

    Added A New Exercise

---
+++ C# OOP/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,18 @@
+﻿HashSet<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();
+Func<HashSet<int>, int> min = numbers =>
+{
+    int min = int.MaxValue;
+    foreach(int number in numbers)
+    {
+        if(number < min)
+        {
+            min = number;
+        }
+    }
+    return min;
+};
+
+Console.WriteLine(min(numbers));
