d683e0ce2d230a310b01ce0d0529b34c164f30ee
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 10:57:30 +0300

    Added A New Exercise

---
+++ C# OOP/Defining Classes - Lab/05. Special Cars/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower
+        {
+            get => this.horsePower;
+            set => this.horsePower = value;
+        }
+
+        public double CubicCapacity
+        {
+            get => this.cubicCapacity;
+            set => this.cubicCapacity = value;
+        }
+    }
+}
---
+++ C# OOP/Defining Classes - Lab/05. Special Cars/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string tire;
+            Dictionary<int, Tire[]> tires = new Dictionary<int, Tire[]>();
+            int index = 0;
+            while((tire = Console.ReadLine()) != "No more tires")
+            {
+                string[] info = tire.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                tires.Add(index, new Tire[4]);
+                tires[index][0] = new Tire(int.Parse(info[0]), double.Parse(info[1]));
+                tires[index][1] = new Tire(int.Parse(info[2]), double.Parse(info[3]));
+                tires[index][2] = new Tire(int.Parse(info[4]), double.Parse(info[5]));
+                tires[index++][3] = new Tire(int.Parse(info[6]), double.Parse(info[7]));
+            }
+
+            string engine;
+            Dictionary<int, Engine> engines = new Dictionary<int, Engine>();
+            index = 0;
+            while((engine = Console.ReadLine()) != "Engines done")
+            {
+                string[] info = engine.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                engines.Add(index++, new Engine(int.Parse(info[0]), double.Parse(info[1])));
+            }
+
+            string end;
+            List<Car> cars = new List<Car>();   
+            while((end = Console.ReadLine()) != "Show special")
+            {
+                string[] info = end.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string make = info[0];
+                string model = info[1];
+                int year = int.Parse(info[2]);
+                double fuelQuantity = double.Parse(info[3]);
+                double fuelConsumption = double.Parse(info[4]);
+                int engineIndex  = int.Parse(info[5]);
+                int tireIndex = int.Parse(info[6]);
+
+                Car car = new Car(make, model, year, fuelQuantity, fuelConsumption, engines[engineIndex], tires[tireIndex]);
+                cars.Add(car);
+            }
+
+            foreach(Car car in cars.Where(c => c.Year >= 2017 && c.Engine.HorsePower > 330))
+            {
+                double pressureSum = 0;
+                for(int i = 0; i < car.Tires.Length; i++)
+                {
+                    pressureSum += car.Tires[i].Pressure;
+                }
+                if(pressureSum >= 9 && pressureSum <= 10)
+                {
+                    car.Drive(20.0);
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
