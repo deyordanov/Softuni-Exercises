426da85608e13545963409d6e4400303c2f7b0cf
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 10:56:15 +0300

    Added A New Exercise

---
+++ C# OOP/Defining Classes - Lab/03. Car Constructors/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+            
+            Car car1 = new Car();
+            Car car2 = new Car(make, model, year);
+            Car car3 = new Car(make, model, year, fuelQuantity, fuelConsumption);
+        }
+    }
+}
