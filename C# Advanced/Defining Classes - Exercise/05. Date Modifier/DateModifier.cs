be6ae45ba655e0047b66e714d6119924b8ff06db
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 19:12:03 +0300

    Added A New Exercise

---
+++ C# Advanced/Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateeModifier
+{
+    public class DateModifier
+    {
+        public int CalculateDays(string firstDate, string secondDate)
+        {
+            DateTime first = DateTime.Parse(firstDate);
+            DateTime second = DateTime.Parse(secondDate);
+
+            return Math.Abs((int)(first - second).TotalDays);
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/05. Date Modifier/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DateeModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second  = Console.ReadLine();
+            DateModifier date = new DateModifier();
+            Console.WriteLine(date.CalculateDays(first, second));
+        }
+    }
+}
