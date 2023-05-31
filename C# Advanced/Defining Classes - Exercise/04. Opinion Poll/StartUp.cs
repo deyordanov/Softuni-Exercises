97e30f8b08ec6bb6015cd11d51c349eca41979ab
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 19:11:41 +0300

    Added A New Exercise

---
+++ C# Advanced/Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for(int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(int.Parse(info[1]), info[0]);
+                people.Add(person);
+            }
+
+            foreach(Person person in people.Where(p => p.Age > 30).OrderBy(p => p.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
