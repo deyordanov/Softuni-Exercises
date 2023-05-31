9e0769c645f28948393a98182f9c5488e07c36ab
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 10:55:10 +0300

    Added A New Exercise

---
+++ C# OOP/Defining Classes - Lab/01. Car/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make
+        {
+            get => this.make;
+            set => this.make = value;
+        }
+
+        public string Model
+        {
+            get => this.model;
+            set => this.model = value;
+        }
+
+        public int Year
+        {
+            get => this.year;
+            set => this.year = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/Defining Classes - Lab/01. Car/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine(car.ToString());
+        }
+    }
+}
