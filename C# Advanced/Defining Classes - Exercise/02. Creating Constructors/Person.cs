9902368f0225fbedd6adf4d7b2274fe93acfa12c
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 19:11:01 +0300

    Added A New Exercise

---
+++ C# Advanced/Defining Classes - Exercise/02. Creating Constructors/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private int age;
+        private string name;
+
+        public Person()
+        {
+            this.Age = 1;
+            this.Name = "No name";
+        }
+
+        public Person(int age)
+            : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(int age, string name)
+            :this(age)
+        {
+            this.Name = name;
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+    }
+}
