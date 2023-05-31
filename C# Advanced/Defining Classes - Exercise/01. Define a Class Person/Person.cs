e97a57cb84ee8bca9b0ad1bf9793822ade117bc9
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 19:10:31 +0300

    Added A New Exercise

---
+++ C# Advanced/Defining Classes - Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private int age;
+        private string name;
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+    }
+}
