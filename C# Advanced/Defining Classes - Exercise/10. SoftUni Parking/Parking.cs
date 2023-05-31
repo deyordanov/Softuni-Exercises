264d2696abab87e16a08aafa04e7c5e2c7a4824e
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 19:13:29 +0300

    Added A New Exercise

---
+++ C# Advanced/Defining Classes - Exercise/10. SoftUni Parking/Parking.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            cars = new List<Car>();
+            this.capacity = capacity;
+        }
+
+        public int Count => this.cars.Count;
+
+        public string AddCar(Car car)
+        {
+            if (cars.Any(c => c.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            if (this.cars.Count() + 1 > this.capacity)
+            {
+                return "Parking is full!";
+            }
+            this.cars.Add(car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            Car car = cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+
+            if (car == null)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            cars.Remove(car);
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            this.cars = cars.Where(c => !RegistrationNumbers.Contains(c.RegistrationNumber)).ToList();
+        }
+    }
+}
