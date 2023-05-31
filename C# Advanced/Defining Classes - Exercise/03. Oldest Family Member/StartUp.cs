42f8f9b1735f6c8dc1a1e8cb4463909f0ff81fbd
Denis Yordanov <deyordanov777@gmail.com>
2023-05-31 19:11:20 +0300

    Added A New Exercise

---
+++ C# Advanced/Defining Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for(int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(int.Parse(info[1]), info[0]);
+                people.Add(person);
+            }
+
+            int age = int.MinValue;
+            foreach(Person person in people)
+            {
+                if(person.Age > age)
+                {
+                    age = person.Age;
+                }
+            }
+
+            Console.WriteLine($"{people.Where(p => p.Age == age).First().Name} {age}");
+        }
+    }
+}
