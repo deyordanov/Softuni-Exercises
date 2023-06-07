ce7e9f5b6a3df8819c03ceedfd23dd88b04dd93d
Denis Yordanov <deyordanov777@gmail.com>
2023-06-07 22:15:57 +0300

    Added A New Exercise

---
+++ C# OOP/SOLID/LogForU.ConsoleApp/CustomLayouts/XmlLayout.cs
@@ -0,0 +1,24 @@
+﻿namespace LogForU.ConsoleApp.CustomLayouts
+{
+    using System.Text;
+    using Core.Layouts.Interfaces;
+
+    public class XmlLayout : ILayout
+    {
+        public string Format
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine("<log>");
+                sb.AppendLine("\t<date>{0}</date>");
+                sb.AppendLine("\t<level>{1}</level>");
+                sb.AppendLine("\t<message>{2}</message>");
+                sb.AppendLine("</log>");
+
+                return sb.ToString();
+            }
+        }
+    }
+}
