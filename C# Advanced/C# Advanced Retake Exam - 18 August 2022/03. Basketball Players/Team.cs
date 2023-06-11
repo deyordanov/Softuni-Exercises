70c6fe997cc3a3ab76f2074eb2301d7d8cf1bb04
Denis Yordanov <deyordanov777@gmail.com>
2023-06-11 22:13:47 +0300

    Added A New Exercise

---
+++ C# Advanced/C# Advanced Retake Exam - 18 August 2022/03. Basketball Players/Team.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basketball
+{
+    public class Team
+    {
+        public Team(string name, int pos, char group)
+        {
+            Name = name;
+            OpenPositions = pos;
+            Group = group;
+            Players = new List<Player>();
+        }
+        public List<Player> Players { get; set; }
+        public string Name { get; set; }
+        public int OpenPositions { get; set; }
+        public char Group { get; set; }
+        public int Count => Players.Count;
+
+        public string AddPlayer(Player player)
+        {
+            if(player.Name )
+            if(Count < OpenPositions)
+            {
+                Players.Add(player);
+
+            }
+        }
+    }
+}
