c2d6eec0107b9ce6825555bb5fd838e0f628a109
Denis Yordanov <deyordanov777@gmail.com>
2023-06-13 22:12:42 +0300

    Added A New Exercise

---
+++ C# Advanced/C# Advanced Retake Exam - 12 April 2023/03. ClothingMagazine/Magazine.cs
@@ -0,0 +1,69 @@
+﻿using ClothesMagazine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothingMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+            this.Clothes = new List<Cloth>();
+        }
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public List<Cloth> Clothes { get; set; }
+
+        public void AddCloth(Cloth cloth)
+        {
+            if (this.Clothes.Count() < this.Capacity)
+            {
+                this.Clothes.Add(cloth);
+            }
+        }
+
+        public bool RemoveCloth(string color)
+        {
+            if (this.Clothes.Any(c => c.Color == color))
+            {
+                this.Clothes = this.Clothes.Where(c => c.Color != color).ToList();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Cloth GetSmallestCloth()
+        {
+            return this.Clothes.OrderBy(c => c.Size).First();
+        }
+
+        public Cloth GetCloth(string color)
+        {
+            return this.Clothes.Where(c => c.Color == color).First();
+        }
+
+        public int GetClothCount()
+        {
+            return this.Clothes.Count();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Type} magazine contains:");
+            foreach (Cloth cloth in this.Clothes.OrderBy(c => c.Size))
+            {
+                sb.AppendLine(cloth.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
