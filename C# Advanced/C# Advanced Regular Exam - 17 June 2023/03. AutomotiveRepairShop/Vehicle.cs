9f452f06ad6a69748c6527f20d707f0ee19919f5
Denis Yordanov <deyordanov777@gmail.com>
2023-06-15 22:12:14 +0300

    Added A New Exercise

---
+++ C# Advanced/C# Advanced Regular Exam - 17 June 2023/03. AutomotiveRepairShop/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿namespace AutomotiveRepairShop
+{
+    public class Vehicle
+    {
+        public Vehicle(string vin, int mil, string damage)
+        {
+            VIN = vin;
+            Mileage = mil;
+            Damage = damage;
+        }
+        public string VIN { get; set; }
+        public int Mileage { get; set; }
+        public string Damage { get; set; }
+
+        public override string ToString()
+        {
+            return $"Damage: {Damage}, Vehicle: {VIN} ({Mileage} km)";
+        }
+    }
+}
