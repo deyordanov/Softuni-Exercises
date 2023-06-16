9bc1cba1cf2fe9909372c9d79d02a9c2e3c09e2d
Denis Yordanov <deyordanov777@gmail.com>
2023-06-16 22:12:03 +0300

    Added A New Exercise

---
+++ C# Advanced/C# Advanced Regular Exam - 17 June 2023/03. AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        public RepairShop(int cap)
+        {
+            Capacity = cap;
+            Vehicles = new List<Vehicle>();  
+        }
+        public List<Vehicle> Vehicles { get; set; }
+        public int Capacity { get; set; }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if(Vehicles.Count < Capacity)
+            {
+                Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin)
+        {
+            if(Vehicles.Any(v => v.VIN == vin))
+            {
+                Vehicles = Vehicles.Where(v => v.VIN != vin).ToList();
+                return true;
+            }
+            return false;
+        }
+
+        public int GetCount()
+        {
+            return Vehicles.Count;
+        }
+
+        public Vehicle GetLowestMileage()
+        {
+            return Vehicles.OrderBy(v => v.Mileage).First();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Vehicles in the preparatory:");
+            foreach(Vehicle vehicle in Vehicles)
+            {
+                sb.AppendLine(vehicle.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
