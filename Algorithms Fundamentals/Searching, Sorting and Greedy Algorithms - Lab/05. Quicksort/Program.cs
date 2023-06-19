90eec50046cf03ed5444fcd4dfada35c25665de6
Denis Yordanov <deyordanov777@gmail.com>
2023-06-19 22:10:27 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms - Lab/05. Quicksort/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Quicksort
+{
+    internal class Program
+    {
+        private static int[] numbers;
+        static void Main(string[] args)
+        {
+            numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            QuickSort(0, numbers.Length - 1);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void QuickSort(int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int pivot = start;
+            int left = start + 1;
+            int right = end;
+
+            while (left <= right)
+            {
+                if (numbers[left] > numbers[pivot]
+                    && numbers[right] < numbers[pivot])
+                {
+                    Swap(left, right);
+                }
+
+                if (numbers[left] <= numbers[pivot])
+                {
+                    left++;
+                }
+
+                if (numbers[right] >= numbers[pivot])
+                {
+                    right--;
+                }
+            }
+
+            Swap(pivot, right);
+
+            QuickSort(start, right - 1);
+            QuickSort(right + 1, end);
+        }
+
+        private static void Swap(int idx1, int idx2)
+        {
+            int element = numbers[idx1];
+            numbers[idx1] = numbers[idx2];
+            numbers[idx2] = element;
+        }
+    }
+}
