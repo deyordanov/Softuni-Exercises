b76f347ed227e23ee104ca098626ef5ae1b26719
Denis Yordanov <deyordanov777@gmail.com>
2023-06-19 22:10:08 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms - Lab/02. Selection Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace _02._Selection_Sort
+{
+    internal class Program
+    {
+        private static int[] numbers;
+        static void Main(string[] args)
+        {
+            numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            SelectionSort();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void SelectionSort()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[i])
+                    {
+                        Swap(j, i);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int idx1, int idx2)
+        {
+            int element = numbers[idx1];
+            numbers[idx1] = numbers[idx2];
+            numbers[idx2] = element;
+        }
+    }
+
+}
