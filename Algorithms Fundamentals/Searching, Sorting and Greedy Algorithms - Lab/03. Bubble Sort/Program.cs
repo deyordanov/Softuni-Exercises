acd1d9f81c8e9c5f0e658fd525a4ce0a4b76dee6
Denis Yordanov <deyordanov777@gmail.com>
2023-06-19 22:10:13 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms - Lab/03. Bubble Sort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Bubble_Sort
+{
+    internal class Program
+    {
+        private static int[] numbers;
+        static void Main(string[] args)
+        {
+            numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            BubbleSort();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void BubbleSort()
+        {
+            bool sorted = false;
+            int swapped = 0;
+
+            while (!sorted)
+            {
+                sorted = true;
+
+                for (int i = 1; i < numbers.Length - swapped; i++)
+                {
+                    int j = i - 1;
+                    if (numbers[j] > numbers[i])
+                    {
+                        Swap(j, i);
+                        sorted = false;
+                    }
+                }
+
+                swapped++;
+            }
+        }
+
+        private static void Swap(int idx1, int idx2)
+        {
+            int element = numbers[idx1];
+            numbers[idx1] = numbers[idx2];
+            numbers[idx2] = element;
+        }
+    }
+}
