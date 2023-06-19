24c06923b8d86fd221e6365235512bb7478bf688
Denis Yordanov <deyordanov777@gmail.com>
2023-06-19 22:09:58 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms - Lab/01. Binary Search - Recursive/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(numbers, number, 0, numbers.Length - 1));
+        }
+
+        public static int BinarySearch(int[] array, int number, int left, int right)
+        {
+            while (left <= right)
+            {
+                int middle = (right + left) / 2;
+
+                if (array[middle] == number)
+                {
+                    return middle;
+                }
+
+                if (array[middle] < number)
+                {
+                    BinarySearch(array, number, middle + 1, right);
+                }
+                else
+                {
+                    BinarySearch(array, number, left, middle - 1);
+                }
+            }
+            return -1;
+        }
+    }
+}
