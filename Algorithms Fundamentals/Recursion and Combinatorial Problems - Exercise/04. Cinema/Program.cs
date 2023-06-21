b45dc10f7a309815a2cf15d806c3e8ec6a98b649
Denis Yordanov <deyordanov777@gmail.com>
2023-06-21 22:09:15 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Recursion and Combinatorial Problems - Exercise/04. Cinema/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Cinema
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            string[] seats = new string[people.Count];
+            bool[] locked = new bool[people.Count];
+
+            string end;
+            while ((end = Console.ReadLine()) != "generate")
+            {
+                string[] input = end.Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+                people.Remove(input[0]);
+                seats[int.Parse(input[1]) - 1] = input[0];
+                locked[int.Parse(input[1]) - 1] = true;
+            }
+
+            Permute(0);
+
+            void Permute(int index)
+            {
+                if (index >= people.Count())
+                {
+                    Print();
+                    return;
+                }
+
+                Permute(index + 1);
+
+                for (int i = index + 1; i < people.Count(); i++)
+                {
+                    Swap(index, i);
+                    Permute(index + 1);
+                    Swap(index, i);
+                }
+            }
+
+            void Print()
+            {
+                int idx = 0;
+                for (int i = 0; i < seats.Length; i++)
+                {
+                    if (!locked[i])
+                    {
+                        seats[i] = people[idx++];
+                    }
+                }
+
+                Console.WriteLine(string.Join(" ", seats));
+            }
+
+
+            void Swap(int idx1, int idx2)
+            {
+                string person = people[idx1];
+                people[idx1] = people[idx2];
+                people[idx2] = person;
+            }
+        }
+
+    }
+}
