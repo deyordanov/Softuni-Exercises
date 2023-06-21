24fb8472d3afb4ba2962d518679dddc337429430
Denis Yordanov <deyordanov777@gmail.com>
2023-06-21 22:09:03 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Recursion and Combinatorial Problems - Exercise/02. Nested Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._Nested_Loops
+{
+    internal class Program
+    {
+        static int[] permutation;
+        static int n;
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            permutation = new int[n];
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if(index == permutation.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutation));
+                return;
+            }
+
+            for(int i = 1; i <= n; i++)
+            {
+                permutation[index] = i;
+                Permute(index + 1);
+            }
+        }
+    }
+}
