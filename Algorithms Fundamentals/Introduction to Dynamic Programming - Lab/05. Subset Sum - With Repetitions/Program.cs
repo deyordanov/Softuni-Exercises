597bd7b74998a66354b0a8e54c76057bbe3d1006
Denis Yordanov <deyordanov777@gmail.com>
2023-06-22 22:08:49 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Introduction to Dynamic Programming - Lab/05. Subset Sum - With Repetitions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Subset_Sum___With_Repetitions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int target = 16;
+
+            bool[] sums = new bool[target + 1];
+            sums[0] = true;
+
+            for (int sum = 0; sum < sums.Length; sum++)
+            {
+                if (!sums[sum])
+                {
+                    continue;
+                }
+
+                foreach (int number in numbers)
+                {
+                    int newSum = sum + number;
+
+                    if (newSum > target)
+                    {
+                        continue;
+                    }
+
+                    sums[newSum] = true;
+                }
+            }
+
+            List<int> subset = new List<int>();
+            while (target > 0)
+            {
+                foreach (int number in numbers)
+                {
+                    int prevSum = target - number;
+                    if (prevSum >= 0 && sums[prevSum])
+                    {
+                        subset.Add(number);
+                        target = prevSum;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", subset));
+        }
+    }
+}
