afe9b525a4fdd0e768472f01b2525e412c52abc1
Denis Yordanov <deyordanov777@gmail.com>
2023-06-24 22:07:58 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Introduction to Dynamic Programming - Lab/01. Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fib = int.Parse(Console.ReadLine());
+            Dictionary<int ,long> memoization = new Dictionary<int ,long>();
+
+            Console.WriteLine(CalculateFibonacci(fib));
+
+            long CalculateFibonacci(int fib)
+            {
+                if (memoization.ContainsKey(fib))
+                {
+                    return memoization[fib];
+                }
+
+                if (fib < 2)
+                {
+                    return fib;
+                }
+
+                long result = CalculateFibonacci(fib - 1) + CalculateFibonacci(fib - 2);
+
+                memoization.Add(fib, result);
+
+                return result;
+            }
+        }
+    }
+}
