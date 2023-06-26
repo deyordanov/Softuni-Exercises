bb8ca8cf89135779082f6c9fe9b9eaa574e751ba
Denis Yordanov <deyordanov777@gmail.com>
2023-06-26 22:07:19 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Introduction to Dynamic Programming - Exercise/03. Sum with Unlimited Coins/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Sum_with_Unlimited_Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Sum());
+
+            int Sum()
+            {
+                int[] sums = new int[target + 1];
+                sums[0] = 1;
+
+                foreach (int number in numbers)
+                {
+                    for (int sum = number; sum <= target; sum++)
+                    {
+                        sums[sum] += sums[sum - number];
+                    }
+                }
+
+                return sums[target];
+            }
+        }
+    }
+}
