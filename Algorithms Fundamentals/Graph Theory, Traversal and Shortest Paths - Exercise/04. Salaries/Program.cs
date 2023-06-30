d5426261b75a6a8ecb87dd23405a78c3121c44c6
Denis Yordanov <deyordanov777@gmail.com>
2023-06-30 22:05:36 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Graph Theory, Traversal and Shortest Paths - Exercise/04. Salaries/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Salaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int>[] graph = new List<int>[n];
+            Dictionary<int, int> visited = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                graph[i] = new List<int>();
+                string input = Console.ReadLine();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[j] == 'Y')
+                    {
+                        graph[i].Add(j);
+                    }
+                }
+            }
+
+            int salaries = 0;
+            for (int i = 0; i < graph.Length; i++)
+            {
+                salaries += DFS(i);
+            }
+
+            Console.WriteLine(salaries);
+
+            int DFS(int node)
+            {
+                if (visited.ContainsKey(node))
+                {
+                    return visited[node];
+                }
+
+                int salary = 0;
+
+                if (graph[node].Count == 0)
+                {
+                    salary = 1;
+                }
+                else
+                {
+                    foreach (int child in graph[node])
+                    {
+                        salary += DFS(child);
+                    }
+                }
+
+                visited[node] = salary; 
+                return salary;
+            }
+        }
+    }
+}
