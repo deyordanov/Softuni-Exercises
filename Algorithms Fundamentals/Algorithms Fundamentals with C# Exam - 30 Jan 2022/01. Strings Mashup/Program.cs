5f53fdc6eef3f7fddf39bf920ac59e3208ace1ea
Denis Yordanov <deyordanov777@gmail.com>
2023-07-03 22:03:35 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Algorithms Fundamentals with C# Exam - 30 Jan 2022/01. Strings Mashup/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01._Strings_Mashup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] str = Console.ReadLine().ToCharArray().OrderBy(x => x).ToArray();
+            int size = int.Parse(Console.ReadLine());
+            char[] combination = new char[size];
+
+            Combinations(0, 0);
+
+            void Combinations(int index, int start)
+            {
+                if (index == combination.Length)
+                {
+                    Console.WriteLine(string.Join("", combination));
+                    return;
+                }
+
+                for (int i = start; i < str.Length; i++)
+                {
+                    combination[index] = str[i];
+                    Combinations(index + 1, i);
+                }
+            }
+        }
+    }
+}
