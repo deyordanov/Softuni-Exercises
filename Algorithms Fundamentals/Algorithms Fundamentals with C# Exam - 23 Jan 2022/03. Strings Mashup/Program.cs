1c0ba38575f07832215a4e9bd535bf7c71575b1b
Denis Yordanov <deyordanov777@gmail.com>
2023-07-04 22:03:24 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Algorithms Fundamentals with C# Exam - 23 Jan 2022/03. Strings Mashup/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Strings_Mashup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] str = Console.ReadLine().ToCharArray();
+
+            Permute(0);
+
+            void Permute(int index)
+            {
+                if (index >= str.Length)
+                {
+                    Console.WriteLine(str);
+                    return;
+                }
+
+                Permute(index + 1);
+
+                if (char.IsLetter(str[index]))
+                {
+                    str[index] = char.IsLower(str[index]) ? char.ToUpper(str[index]) : char.ToLower(str[index]);
+                    Permute(index + 1);
+                }
+            }
+        }
+    }
+}
