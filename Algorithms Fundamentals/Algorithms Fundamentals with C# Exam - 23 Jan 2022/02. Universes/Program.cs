141e39ec88c1a3d7431c959e4510c84665c1c07a
Denis Yordanov <deyordanov777@gmail.com>
2023-07-04 22:03:19 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Algorithms Fundamentals with C# Exam - 23 Jan 2022/02. Universes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Universes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nodes = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+            HashSet<string> visited = new HashSet<string>();
+            for (int i = 0; i < nodes; i++)
+            {
+                string[] input = Console.ReadLine().Split(" - ");
+                if (!graph.ContainsKey(input[0]))
+                {
+                    graph.Add(input[0], new List<string>());
+                }
+
+                if (!graph.ContainsKey(input[1]))
+                {
+                    graph.Add(input[1], new List<string>());
+                }
+
+                graph[input[1]].Add(input[0]);
+                graph[input[0]].Add(input[1]);
+            }
+
+            int universes = 0;
+            foreach (string node in graph.Keys)
+            {
+                if (!visited.Contains(node))
+                {
+                    DFS(node);
+                    universes++;
+                }
+            }
+
+            Console.WriteLine(universes);
+
+            void DFS(string node)
+            {
+                if (visited.Contains(node))
+                {
+                    return;
+                }
+
+                visited.Add(node);
+
+                foreach (string child in graph[node])
+                {
+                    DFS(child);
+                }
+            }
+        }
+    }
+}
