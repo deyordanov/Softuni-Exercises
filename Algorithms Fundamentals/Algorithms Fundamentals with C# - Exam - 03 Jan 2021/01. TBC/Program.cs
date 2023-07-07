a062ee6a4270bc848bf56f61dd2fc6fe0652921b
Denis Yordanov <deyordanov777@gmail.com>
2023-07-07 22:00:46 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Algorithms Fundamentals with C# - Exam - 03 Jan 2021/01. TBC/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _01._TBC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size1 = int.Parse(Console.ReadLine());
+            int size2 = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size1, size2];
+            bool[,] visited = new bool[size1, size2];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string symbols = Console.ReadLine();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = symbols[col];
+                }
+            }
+
+            int tunnels = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (!visited[row, col] && matrix[row, col] != 'd')
+                    {
+                        FindTunnels(row, col);
+                        tunnels++;
+                    }
+                }
+            }
+
+            Console.WriteLine(tunnels);
+
+            void FindTunnels(int row, int col)
+            {
+                if (row < 0 || row >= matrix.GetLength(0)
+                            || col < 0 || col >= matrix.GetLength(1)
+                            || matrix[row, col] == 'd'
+                            || visited[row, col])
+                {
+                    return;
+                }
+
+                visited[row, col] = true;
+
+                FindTunnels(row - 1, col); //up
+                FindTunnels(row, col + 1); //right
+                FindTunnels(row, col - 1); //left
+                FindTunnels(row + 1, col); //down
+                FindTunnels(row - 1, col - 1); //left diagonal up
+                FindTunnels(row - 1, col + 1); //right diagonal up
+                FindTunnels(row + 1, col - 1); //left diagonal down
+                FindTunnels(row + 1, col + 1); //right diagonal down
+            }
+        }
+    }
+}
---
+++ Algorithms Fundamentals/Algorithms Fundamentals with C# - Exam - 03 Jan 2021/02. Socks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _02._Socks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] nums2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[nums1.Length + 1, nums2.Length + 1];
+
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    if (nums1[row - 1] == nums2[col - 1])
+                    {
+                        matrix[row, col] = matrix[row - 1, col - 1] + 1;
+                    }
+                    else
+                    {
+                        matrix[row, col] = Math.Max(matrix[row - 1, col], matrix[row, col - 1]);
+                    }
+                }
+            }
+
+            Console.WriteLine(matrix[nums1.Length, nums2.Length]);
+        }
+    }
+}
