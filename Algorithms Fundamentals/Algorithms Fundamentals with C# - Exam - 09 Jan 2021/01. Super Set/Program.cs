2c92cec99dc7c4a2f78d968babcc03b349427906
Denis Yordanov <deyordanov777@gmail.com>
2023-07-07 22:01:49 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Algorithms Fundamentals with C# - Exam - 09 Jan 2021/01. Super Set/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Super_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine();
+            for (int i = 1; i <= numbers.Length; i++)
+            {
+                Combinations(new int[i], 0, 0);
+            }
+
+            void Combinations(int[] combination, int index, int start)
+            {
+                if (index >= combination.Length)
+                {
+                    Console.WriteLine(string.Join(" ", combination));
+                    return;
+                }
+
+                for (int i = start; i < numbers.Length; i++)
+                {
+                    combination[index] = numbers[i];
+                    Combinations(combination, index + 1,i + 1);
+                }
+            }
+        }
+    }
+}
