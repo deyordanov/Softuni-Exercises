55ef5f6449186e2df890f439b6620b3413e7d8d6
Denis Yordanov <deyordanov777@gmail.com>
2023-07-08 22:00:26 +0300

    Added A New Exercise

---
+++ Algorithms Fundamentals/Algorithms Exam Preparation 2020/02. Time/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] nums2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[nums1.Length + 1, nums2.Length + 1];
+
+            Dictionary<string, int> seq = new Dictionary<string, int>();
+
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    if (nums1[row - 1] == nums2[col - 1])
+                    {
+                        matrix[row, col] = matrix[row - 1, col - 1] + 1;
+                    }
+                    else
+                    {
+                        matrix[row, col] = Math.Max(matrix[row - 1, col], matrix[row, col - 1]);
+                    }
+                }
+            }
+
+            int r = nums1.Length;
+            int c = nums2.Length;
+            Stack<int> path = new Stack<int>();
+            while (r > 0 && c > 0)
+            {
+                if (nums1[r - 1] == nums2[c - 1])
+                {
+                    path.Push(nums1[r - 1]);
+                    r--;
+                    c--;
+                }
+                else if (matrix[r, c - 1] >= matrix[r - 1, c])
+                {
+                    c--;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", path));
+            Console.WriteLine(matrix[nums1.Length, nums2.Length]);
+        }
+    }
+}
---
+++ Algorithms Fundamentals/Algorithms Exam Preparation 2020/03. The Story Telling/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._The_Story_Telling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string end;
+            Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+            HashSet<string> visited = new HashSet<string>();
+            while ((end = Console.ReadLine()) != "End")
+            {
+                string[] input = end
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+                if (!graph.ContainsKey(input[0]))
+                {
+                    if (input.Length == 1)
+                    {
+                        graph.Add(input[0], new List<string>());
+                    }
+                    else if (input.Length == 2)
+                    {
+                        graph.Add(input[0], input[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList());
+                    }
+                }
+            }
+
+            Stack<string> path = new Stack<string>();
+            foreach (var node in graph)
+            {
+                if (!visited.Contains(node.Key))
+                {
+                    DFS(node.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", path));
+
+            void DFS(string node)
+            {
+                if (visited.Contains(node))
+                {
+                    return;
+                }
+
+                visited.Add(node);
+
+                foreach (string child in graph[node])
+                {
+                    DFS(child);
+                }
+
+                path.Push(node);
+            }
+        }
+    }
+}
