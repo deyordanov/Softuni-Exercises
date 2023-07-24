a983f0b96c536231ef42c4ad52214e1e610c65ab
Denis Yordanov <deyordanov777@gmail.com>
2023-07-24 10:56:40 +0300

    Added A New Exercise

---
+++ C# OOP/Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿List<int> validNumbers = new List<int>();
+while (validNumbers.Count < 10)
+{
+    string input = Console.ReadLine();
+    try
+    {
+        if(!input.All(c => char.IsDigit(c)))
+        {
+            throw new ArgumentException("Invalid Number!");
+        }
+
+        int number = int.Parse(input);
+
+        if (number <= 1 || number >= 100)
+        {
+            throw new ArgumentException("Your number is not in range (1 - 100)");
+        }
+
+        validNumbers.Add(number);
+    }
+    catch (ArgumentException ae)
+    {
+        Console.WriteLine(ae.Message);
+    }
+}
+
+Console.WriteLine(string.Join(", ", validNumbers));
