709de7443d487044a254c53ece9cc5f3459efc6e
Denis Yordanov <deyordanov777@gmail.com>
2023-07-24 10:57:52 +0300

    Added A New Exercise

---
+++ C# OOP/Exceptions and Error Handling - Lab/04. Sum of Integers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+string[] numbers = Console.ReadLine().Split(" ");
+
+long sum = 0;
+foreach (string number in numbers)
+{
+    try
+    {
+        int num = int.Parse(number);
+
+        sum += num;
+    }
+    catch (FormatException fe)
+    { 
+        Console.WriteLine($"The element '{number}' is in wrong format!");
+    }
+    catch (OverflowException oe)
+    {
+        Console.WriteLine($"The element '{number}' is out of range!");
+    }
+    finally
+    {
+        Console.WriteLine($"Element '{number}' processed - current sum: {sum}");
+    }
+}
+
+Console.WriteLine($"The total sum of all integers is: {sum}");
