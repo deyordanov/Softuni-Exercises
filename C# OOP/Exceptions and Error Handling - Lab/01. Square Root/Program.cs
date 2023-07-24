a21eb0c0c0744089ac55ccb64caf568457267384
Denis Yordanov <deyordanov777@gmail.com>
2023-07-24 10:56:29 +0300

    Added A New Exercise

---
+++ C# OOP/Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+try
+{
+    Console.WriteLine(CalculateSquareRoot(number));
+}
+catch (ArgumentException ae)
+{
+    Console.WriteLine(ae.Message);
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
+
+int CalculateSquareRoot(int num)
+{
+    if (num < 0)
+    {
+        throw new ArgumentException("Invalid number.");
+    }
+
+    return (int)Math.Sqrt(num);
+}
