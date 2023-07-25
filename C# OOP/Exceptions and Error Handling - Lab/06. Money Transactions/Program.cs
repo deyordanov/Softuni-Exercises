565e736207bff837217d4ca5668b4f2642a5ccf4
Denis Yordanov <deyordanov777@gmail.com>
2023-07-25 10:59:22 +0300

    Added A New Exercise

---
+++ C# OOP/Exceptions and Error Handling - Lab/06. Money Transactions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Principal;
+
+string[] input = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries);
+Dictionary<int, decimal> accounts = new Dictionary<int, decimal>();
+foreach (string str in input)
+{
+    string[] account = str.Split('-');
+    accounts.Add(int.Parse(account[0]), decimal.Parse(account[1]));
+}
+
+string end;
+while ((end = Console.ReadLine()) != "End")
+{
+    try
+    {
+        string[] commandArgs = end.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        string command = commandArgs[0];
+        int accNumber = int.Parse(commandArgs[1]);
+        decimal amount = decimal.Parse(commandArgs[2]);
+
+        ValidateInput(command, accNumber, amount);
+
+        if (command == "Deposit")
+        {
+            accounts[accNumber] += amount;
+        }
+        else if (command == "Withdraw")
+        {
+            accounts[accNumber] -= amount;
+        }
+
+        Console.WriteLine($"Account {accNumber} has new balance: {accounts[accNumber]:F2}");
+    }
+    catch (ArgumentException ae)
+    {
+        Console.WriteLine(ae.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Enter another command");
+    }
+}
+
+void ValidateInput(string command, int accNumber, decimal amount)
+{
+    if (command != "Deposit" && command != "Withdraw")
+    {
+        throw new ArgumentException("Invalid command!");
+    }
+
+    if (!accounts.ContainsKey(accNumber))
+    {
+        throw new ArgumentException("Invalid account!");
+    }
+
+    if (command == "Withdraw" && amount > accounts[accNumber])
+    {
+        throw new ArgumentException("Insufficient balance!");
+    }
+}
---
+++ C# OOP/Reflection and Attributes - Exercise -  Archive/01.HarvestingFields/HarvestingFieldTest.cs
@@ -0,0 +1,52 @@
+﻿namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            var type = Type.GetType("P01_HarvestingFields.HarvestingFields");
+
+            FieldInfo[] fields = type.GetFields((BindingFlags)60);
+
+            string end;
+            while ((end = Console.ReadLine()) != "HARVEST")
+            {
+                string accessModifier = end;
+
+                switch (accessModifier)
+                {
+                    case "private":
+                        Console.WriteLine(PrintFields(f => f.IsPrivate));
+                        break;
+                    case "protected":
+                        Console.WriteLine(PrintFields(f => f.IsFamily));
+                        break;
+                    case "public":
+                        Console.WriteLine(PrintFields(f => f.IsPublic));
+                        break;
+                    case "all":
+                        Console.WriteLine(PrintFields(f => f.IsPrivate || f.IsPublic || f.IsFamily || f.IsStatic));
+                        break;
+                }
+
+            }
+
+            string PrintFields(Func<FieldInfo, bool> func)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (FieldInfo field in fields.Where(func))
+                {
+                    string modifier = field.IsPrivate ? "private" : field.IsFamily ? "protected" : "public";
+                    sb.AppendLine(@$"{modifier} {field.FieldType.Name} {field.Name}");
+                }
+
+                return sb.ToString().Trim();
+            }
+        }
+    }
+}
