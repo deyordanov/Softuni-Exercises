9958baa2cd01de3d9c247154ab453ba1fc3e9df2
Denis Yordanov <deyordanov777@gmail.com>
2023-07-25 10:59:49 +0300

    Added A New Exercise

---
+++ C# OOP/Reflection and Attributes - Exercise -  Archive/02. Black Box Integer/BlackBoxIntegerTests.cs
@@ -0,0 +1,41 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            Type type = Type.GetType("P02_BlackBoxInteger.BlackBoxInteger");
+
+            var instance = Activator.CreateInstance(type, true);
+
+            MethodInfo[] methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            string end;
+            while ((end = Console.ReadLine()) != "END")
+            {
+                string[] commandArgs = end.Split("_");
+                string command = commandArgs[0];
+                int number = int.Parse(commandArgs[1]);
+
+                InvokeMethod(command, number);
+            }
+
+
+
+            void InvokeMethod(string methodName, int number)
+            {
+                foreach (MethodInfo method in methods.Where(m => m.Name == methodName))
+                {
+                    method.Invoke(instance, new object[] { number });
+                }
+
+                FieldInfo field = type.GetField("innerValue", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+                Console.WriteLine(field.GetValue(instance));
+            }
+        }
+    }
+}
---
+++ C# OOP/Reflection and Attributes - Exercise -  Archive/03.BarrackWars-ANewFactory/Attributes/InjectAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace _03.BarrackWars_ANewFactory.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class InjectAttribute : Attribute
+    {
+    }
+}
