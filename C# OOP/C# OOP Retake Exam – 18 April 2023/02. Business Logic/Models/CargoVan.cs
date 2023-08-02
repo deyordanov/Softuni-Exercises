b830e06048035b033f4cab668179ab4ac8e44dd0
Denis Yordanov <deyordanov777@gmail.com>
2023-08-02 21:18:40 +0300

    Added A New Exercise

---
+++ C# OOP/C# OOP Retake Exam – 18 April 2023/02. Business Logic/Models/CargoVan.cs
@@ -0,0 +1,9 @@
+﻿namespace EDriveRent.Models;
+
+public class CargoVan : Vehicle
+{
+    private const double maxMileage = 180;
+    public CargoVan(string brand, string model, string licensePlateNumber) : base(brand, model, maxMileage, licensePlateNumber)
+    {
+    }
+}
