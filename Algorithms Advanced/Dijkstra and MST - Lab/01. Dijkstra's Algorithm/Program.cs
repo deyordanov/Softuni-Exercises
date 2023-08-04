55d6f68dce255b06dc86e7994c174afbc634d68d
Denis Yordanov <deyordanov777@gmail.com>
2023-08-04 21:24:28 +0300

    Added A New Exercise

---
+++ Algorithms Advanced/Dijkstra and MST - Lab/01. Dijkstra's Algorithm/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace _01._Dijkstra_s_Algorithm
+{
+    class Edge
+    {
+        public Edge(int first, int second, int weight)
+        {
+            this.First = first;
+            this.Second = second;
+            this.Weight = weight;
+        }
+
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Weight { get; set; }
+    }
+
+    internal class Program
+    {
+        private static Dictionary<int, List<Edge>> edgesByNode;
+        private static double[] distance;
+        private static int[] parent;
+        static void Main(string[] args)
+        {
+            edgesByNode = new Dictionary<int, List<Edge>>();
+
+            int edgesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                List<int> edgeArgs = Console.ReadLine()
+                    .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                int firstNode = edgeArgs[0];
+                int secondNode = edgeArgs[1];
+                int weight = edgeArgs[2];
+
+                Edge edge = new Edge(firstNode, secondNode, weight);
+
+                if (!edgesByNode.ContainsKey(firstNode))
+                {
+                    edgesByNode.Add(firstNode, new List<Edge>());
+                }
+
+                if (!edgesByNode.ContainsKey(secondNode))
+                {
+                    edgesByNode.Add(secondNode, new List<Edge>());
+                }
+
+                edgesByNode[firstNode].Add(edge);
+                edgesByNode[secondNode].Add(edge);
+            }
+
+            int largestNode = edgesByNode.Keys.Max();
+
+            distance = new double[largestNode + 1];
+            for (int i = 0; i < distance.Length; i++)
+            {
+                distance[i] = double.PositiveInfinity;
+            }
+
+            parent = new int[largestNode + 1];
+            for (int i = 0; i < parent.Length; i++)
+            {
+                parent[i] = -1;
+            }
+
+            int startNode = int.Parse(Console.ReadLine());
+            int endNode = int.Parse(Console.ReadLine());
+
+            distance[startNode] = 0;
+
+            OrderedBag<int> priorityQueue = new OrderedBag<int>(Comparer<int>.Create((f, s) => (int)(distance[f] - distance[s])));
+            priorityQueue.Add(startNode);
+
+            while (priorityQueue.Count > 0)
+            {
+                int minNode = priorityQueue.RemoveFirst();
+
+                if (double.IsPositiveInfinity(distance[minNode]))
+                {
+                    break;
+                }
+
+                if (minNode == endNode)
+                {
+                    break;
+                }
+
+                foreach (Edge edge in edgesByNode[minNode])
+                {
+                    int otherNode = edge.First == minNode ? edge.Second : edge.First;
+
+                    if (double.IsPositiveInfinity(distance[otherNode]))
+                    {
+                        priorityQueue.Add(otherNode);
+                    }
+
+                    double currentDistance = distance[minNode] + edge.Weight;
+
+                    if (currentDistance < distance[otherNode])
+                    {
+                        parent[otherNode] = minNode;
+                        distance[otherNode] = currentDistance;
+                        priorityQueue = new OrderedBag<int>(priorityQueue, Comparer<int>.Create((f, s) => (int)(distance[f] - distance[s])));
+                    } 
+                }
+            }
+
+            if (double.IsPositiveInfinity(distance[endNode]))
+            {
+                Console.WriteLine("There is no such path.");
+                return;
+            }
+
+            Stack<int> path = new Stack<int>();
+            int currentNode = endNode;
+            while (currentNode != -1)
+            {
+                path.Push(currentNode);
+                currentNode = parent[currentNode];
+            }
+
+            Console.WriteLine(distance[endNode]);
+            Console.WriteLine(string.Join(" ", path));
+        }
+    }
+}
---
+++ Algorithms Advanced/Dijkstra and MST - Lab/02. Kruskal's Algorithm/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace _02._Kruskal_s_Algorithm
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Edge
+    {
+        public Edge(int first, int second, int weight)
+        {
+            First = first;
+            Second = second;
+            Weight = weight;
+        }
+
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Weight { get; set; }
+    }
+
+    internal class Program
+    {
+        private static List<Edge> edges;
+        private static List<Edge> forest;
+        private static int[] parent;
+
+        static void Main(string[] args)
+        {
+            edges = new List<Edge>();
+            forest = new List<Edge>();
+
+            int countOfEdges = int.Parse(Console.ReadLine());
+            int maxNode = -1;
+
+            for (int i = 0; i < countOfEdges; i++)
+            {
+                int[] edgeArgs = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int firstNode = edgeArgs[0];
+                int secondNode = edgeArgs[1];
+                int weight = edgeArgs[2];
+
+                if (firstNode > maxNode)
+                {
+                    maxNode = firstNode;
+                }
+
+                if (secondNode > maxNode)
+                {
+                    maxNode = secondNode;
+                }
+
+                edges.Add(new Edge(firstNode, secondNode, weight));
+            }
+
+            parent = new int[maxNode + 1];
+
+            for (int node = 0; node < parent.Length; node++)
+            {
+                parent[node] = node;
+            }
+
+            List<Edge> sortedEdges = edges.OrderBy(e => e.Weight).ToList();
+
+            foreach (Edge edge in sortedEdges)
+            {
+                int firstNodeRoot = FindRoot(edge.First);
+                int secondNodeRoot = FindRoot(edge.Second);
+
+                if (firstNodeRoot == secondNodeRoot)
+                {
+                    continue;
+                }
+
+                parent[firstNodeRoot] = secondNodeRoot;   
+                forest.Add(edge);
+            }
+
+            foreach (Edge edge in forest)
+            {
+                Console.WriteLine($"{edge.First} - {edge.Second}");
+            }
+
+            int FindRoot(int node)
+            {
+                while (node != parent[node])
+                {
+                    node = parent[node];
+                }
+
+                return node;
+            }
+        }
+    }
+}
