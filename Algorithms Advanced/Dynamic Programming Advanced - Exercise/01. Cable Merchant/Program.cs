9f88424e9b1b075a1f80079be4a30e263a45cc39
Denis Yordanov <deyordanov777@gmail.com>
2023-08-15 17:10:46 +0300

    Added A New Exercise

---
+++ Algorithms Advanced/Dynamic Programming Advanced - Exercise/01. Cable Merchant/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _01._Cable_Merchant
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cable = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int connectorPrice = int.Parse(Console.ReadLine());
+
+            int[] memoization = new int[cable.Length + 1];
+            List<int> prices = new List<int>();
+
+            CutCable(cable.Length);
+
+            Console.WriteLine(string.Join(" ", prices));
+
+            int CutCable(int length)
+            {
+                if (memoization[length] != 0)
+                {
+                    return memoization[length];
+                }
+
+                int bestPrice = cable[length - 1];
+
+                for (int i = 1; i < length; i++)
+                {
+                    int currentPrice = cable[i - 1] + CutCable(length - i);
+
+                    if (currentPrice - (2 * connectorPrice) > bestPrice)
+                    {
+                        bestPrice = currentPrice - (2 * connectorPrice);
+                    }
+                }
+
+                memoization[length] = bestPrice;
+                prices.Add(bestPrice);
+
+                return bestPrice;
+            }
+        }
+    }
+}
