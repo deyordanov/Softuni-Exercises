6aafe1259a52bb703217ba01461279821f97bdf4
Denis Yordanov <deyordanov777@gmail.com>
2023-08-23 08:08:40 +0300

    Added A New Exercise

---
+++ JS Advanced/Arrays and Nested Arrays - Exercise/12. Orbit.js
@@ -0,0 +1,11 @@
+function solve([rows, cols, row, col]) {
+  const matrix = new Array(rows).fill().map((a) => new Array(cols).fill(0));
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      matrix[r][c] = Math.max(Math.abs(r - row), Math.abs(c - col)) + 1;
+    }
+  }
+  console.log(matrix.map((arr) => arr.join(" ")).join("\n"));
+}
+
+solve([4, 4, 0, 0]);
