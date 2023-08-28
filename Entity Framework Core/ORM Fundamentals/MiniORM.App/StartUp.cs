3a10e8031e21c889f87be19addf3a7c0e80c69b6
Denis Yordanov <deyordanov777@gmail.com>
2023-08-28 12:17:20 +0300

    Added A New Exercise

---
+++ Entity Framework Core/ORM Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using System.Linq;
+    using Data;
+    using Data.Entities;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = @"Server=localhost,1433;Database=MiniORM;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
