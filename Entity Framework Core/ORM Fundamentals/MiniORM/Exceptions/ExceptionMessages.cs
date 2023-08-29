c9958070706a159f8b915f12348fbe85c70adb1b
Denis Yordanov <deyordanov777@gmail.com>
2023-08-29 12:18:07 +0300

    Added A New Exercise

---
+++ Entity Framework Core/ORM Fundamentals/MiniORM/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace MiniORM.Exceptions;
+
+public class ExceptionMessages
+{
+    public const string NullItemException = "Item cannot be null!";
+    public const string InvalidEntitiesException = "{0} Invalid Entities found in {1}!";
+}
