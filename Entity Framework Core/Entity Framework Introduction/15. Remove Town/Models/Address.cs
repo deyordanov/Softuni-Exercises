8b3b0799f2ace78f3aeca4f3b0eceee1c759e717
Denis Yordanov <deyordanov777@gmail.com>
2023-08-31 14:46:28 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Entity Framework Introduction/15. Remove Town/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace SoftUni.Models;
+
+using System.Collections.Generic;
+
+public class Address
+{
+    public Address()
+    {
+        this.Employees = new HashSet<Employee>();
+    }
+
+    public int? AddressId { get; set; }
+    public string AddressText { get; set; } = null!;
+    public int? TownId { get; set; }
+    public virtual Town? Town { get; set; }
+    public virtual ICollection<Employee> Employees { get; set; }
+}
---
+++ Entity Framework Core/Entity Framework Introduction/15. Remove Town/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(RemoveTown(dbContext));
+    }
+
+    public static string RemoveTown(SoftUniContext context)
+    {
+        var seattleAddresses = context
+            .Addresses
+            .Where(a => a.Town.Name == "Seattle")
+            .ToArray();
+
+        var employeesInSeattle = context
+            .Employees
+            .ToArray()
+            .Where(e => seattleAddresses.Any(a => a.AddressId == e.AddressId))
+            .ToArray();
+
+        foreach (Employee employee in employeesInSeattle)
+        {
+            employee.AddressId = null;
+        }
+
+        context.Addresses.RemoveRange(seattleAddresses);
+
+        var seattleTown = context
+            .Towns
+            .First(t => t.Name == "Seattle");
+
+        context.Towns.Remove(seattleTown);
+
+        context.SaveChanges();
+
+        return $"{seattleAddresses.Length} addresses in Seattle were deleted";
+    }
+}
