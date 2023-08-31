08fe4fac1808456edb8ba378c97f92802e56b942
Denis Yordanov <deyordanov777@gmail.com>
2023-08-31 14:45:05 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Entity Framework Introduction/10. Departments with More Than 5 Employees/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetDepartmentsWithMoreThan5Employees(dbContext));
+    }
+
+    public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var departments = context
+            .Departments
+            .Where(d => d.Employees.Count > 5)
+            .OrderBy(d => d.Employees.Count)
+            .ThenBy(d => d.Name)
+            .Select(d => new
+            {
+                DepartmentName = d.Name,
+                ManagerFirstName = d.Manager.FirstName,
+                ManagerLastName = d.Manager.LastName,
+                Employees = d.Employees.Select(e => new
+                {
+                    EmployeeFistName = e.FirstName,
+                    EmployeeLastName = e.LastName,
+                    EmployeeJobTitle = e.JobTitle,
+                })
+                    .OrderBy(e => e.EmployeeFistName)
+                    .ThenBy(e => e.EmployeeLastName)
+                    .ToArray()
+            }).ToArray();
+
+        foreach (var department in departments)
+        {
+            sb.AppendLine($"{department.DepartmentName} - {department.ManagerFirstName} {department.ManagerLastName}");
+            foreach (var employee in department.Employees)
+            {
+                sb.AppendLine($"{employee.EmployeeFistName} {employee.EmployeeLastName} - {employee.EmployeeJobTitle}");
+            }
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
---
+++ Entity Framework Core/Entity Framework Introduction/11. Find Latest 10 Projects/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetLatestProjects(dbContext));
+    }
+
+    public static string GetLatestProjects(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var projects = context
+            .Projects
+            .OrderByDescending(p => p.StartDate)
+            .Take(10)
+            .OrderBy(p => p.Name)
+            .ToArray();
+
+        foreach (var project in projects)
+        {
+            sb.AppendLine(project.Name);
+            sb.AppendLine(project.Description);
+            sb.AppendLine(project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture));
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
