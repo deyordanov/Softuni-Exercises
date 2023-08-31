e3cb7471ccb1d1c550440faabd23c403f3b689e5
Denis Yordanov <deyordanov777@gmail.com>
2023-08-31 14:40:17 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Entity Framework Introduction/04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetEmployeesWithSalaryOver50000(dbContext));
+    }
+
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(x => x.Salary > 50000)
+            .OrderBy(x => x.FirstName)
+            .ToList();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
