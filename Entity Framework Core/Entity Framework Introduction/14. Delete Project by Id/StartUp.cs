f73d42b423c9dcef1596dd7f323ef038e43e3e15
Denis Yordanov <deyordanov777@gmail.com>
2023-08-31 14:46:25 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Entity Framework Introduction/14. Delete Project by Id/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(DeleteProjectById(dbContext));
+    }
+
+    public static string DeleteProjectById(SoftUniContext context)
+    {
+        var projectsToDelete = context
+            .EmployeesProjects
+            .Where(p => p.Project.ProjectId == 2);
+
+        context.EmployeesProjects.RemoveRange(projectsToDelete);
+
+        var projectToDelete = context.Projects.Find(2);
+
+        context.Projects.Remove(projectToDelete!);
+
+        context.SaveChanges();
+
+        return string.Join(Environment.NewLine, context.Projects.Take(10).Select(p => p.Name));
+    }
+}
