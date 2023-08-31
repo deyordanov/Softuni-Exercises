4e8a3e3069de89b984a61756cd21858f5d339fff
Denis Yordanov <deyordanov777@gmail.com>
2023-08-31 14:43:00 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Entity Framework Introduction/08. Addresses by Town/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetAddressesByTown(dbContext));
+    }
+
+    public static string GetAddressesByTown(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+        var addresses = context
+            .Addresses
+            .OrderByDescending(a => a.Employees.Count)
+            .ThenBy(a => a.Town.Name)
+            .ThenBy(a => a.AddressText)
+            .Take(10)
+            .Select(a => new
+            {
+                Text = a.AddressText,
+                TownName = a.Town.Name,
+                EmployeeCount = a.Employees.Count
+            })
+            .ToArray();
+
+        foreach (var address in addresses)
+        {
+            sb.AppendLine($"{address.Text}, {address.TownName} - {address.EmployeeCount} employees");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
