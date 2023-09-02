c294229c35db69c0964c2d33eae50de85e125b74
Denis Yordanov <deyordanov777@gmail.com>
2023-09-02 11:21:40 +0300

    Added A New Exercise

---
+++ Data Structures Fundamentals/04. Trees Representation and Traversal (BFS and DFS) - Exercise/01. Create Tree/Tree.cs
@@ -0,0 +1,162 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private readonly List<Tree<T>> children;
+
+        private Tree()
+        {
+            this.children = new List<Tree<T>>();
+        }
+
+        public Tree(T key, params Tree<T>[] children)
+            : this()
+        {
+            this.Key = key;
+
+            foreach (Tree<T> child in children)
+            {
+                this.children.Add(child);
+                child.Parent = this;
+            }
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+        public IReadOnlyCollection<Tree<T>> Children => this.children.AsReadOnly();
+
+        public void AddChild(Tree<T> child)
+        {
+            this.children.Add(child);
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            this.Parent = parent;
+        }
+
+        public string AsString()
+        {
+            StringBuilder sb = new StringBuilder();
+            this.DfsToString(sb, this, 0);
+            return sb.ToString().TrimEnd();
+        }
+
+        public IEnumerable<T> GetInternalKeys()
+            => this.InternalAndLeafKeysBfs(x => x.children.Count > 0 && x.Parent != null)
+                .Select(tree => tree.Key);
+
+        public IEnumerable<T> GetLeafKeys()
+            => this.InternalAndLeafKeysBfs(x => x.children.Count == 0 && x.Parent != null)
+                .Select(tree => tree.Key);
+
+        public T GetDeepestKey()
+        {
+            int minDepth = int.MinValue;
+            Tree<T> deepestNode = this;
+            foreach(Tree<T> leaf in this.InternalAndLeafKeysBfs((x => x.children.Count == 0 && x.Parent != null)))
+            {
+                int depth = this.GetNodeDepth(leaf);
+                if (depth > minDepth)
+                {
+                    minDepth = depth;
+                    deepestNode = leaf;
+                }
+            }
+
+            return deepestNode.Key;
+        }
+
+        public IEnumerable<T> GetLongestPath()
+        {
+            Tree<T> deepestNode = this.FindNodeByKey(this,this.GetDeepestKey());
+
+            Stack<T> path = new Stack<T>();
+
+            while (deepestNode != null)
+            {
+                path.Push(deepestNode.Key);
+                deepestNode = deepestNode.Parent;
+            }
+
+            return path;
+        }
+
+        private IEnumerable<Tree<T>> InternalAndLeafKeysBfs(Predicate<Tree<T>> predicate)
+        {
+            List<Tree<T>> result = new List<Tree<T>>();
+
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                Tree<T> node = queue.Dequeue();
+
+                if (predicate(node))
+                {
+                    result.Add(node);
+                }
+
+                foreach (Tree<T> child in node.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        private void DfsToString(StringBuilder sb, Tree<T> tree, int level)
+        {
+            sb.AppendLine(new string(' ', level) + tree.Key);
+
+            foreach (Tree<T> child in tree.Children)
+            {
+                this.DfsToString(sb, child, level + 2);
+            }
+        }
+
+        private int GetNodeDepth(Tree<T> tree)
+        {
+            int depth = 0;
+            while (tree.Parent != null)
+            {
+                tree = tree.Parent;
+                depth++;
+            }
+
+            return depth;
+        }
+
+        private Tree<T> FindNodeByKey(Tree<T> tree, T key)
+        {
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+            queue.Enqueue(tree);
+
+            while (queue.Count > 0)
+            {
+                Tree<T> node = queue.Dequeue();
+
+                if (node.Key.Equals(key))
+                {
+                    return node;
+                }
+
+                foreach (Tree<T> child in node.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Data Structures Fundamentals/04. Trees Representation and Traversal (BFS and DFS) - Exercise/01. Create Tree/TreeFactory.cs
@@ -0,0 +1,62 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TreeFactory
+    {
+        private Dictionary<int, IntegerTree> nodesByKey;
+
+        public TreeFactory()
+        {
+            this.nodesByKey = new Dictionary<int, IntegerTree>();
+        }
+
+        public IntegerTree CreateTreeFromStrings(string[] input)
+        {
+            foreach (string pair in input)
+            {
+                int[] nodes = pair.Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                this.AddEdge(nodes[0], nodes[1]);
+            }
+
+            return this.GetRoot();
+        }
+
+        public IntegerTree CreateNodeByKey(int key)
+        {
+            if (!this.nodesByKey.ContainsKey(key))
+            {
+                this.nodesByKey.Add(key, new IntegerTree(key));
+            }
+
+            return this.nodesByKey[key];
+        }
+
+        public void AddEdge(int parent, int child)
+        {
+            IntegerTree parentNode = CreateNodeByKey(parent);
+            IntegerTree childNode = CreateNodeByKey(child);
+
+            parentNode.AddChild(childNode);
+            childNode.AddParent(parentNode);
+        }
+
+        public IntegerTree GetRoot()
+        {
+            foreach (var kvp in this.nodesByKey)
+            {
+                if (kvp.Value.Parent == null)
+                {
+                    return kvp.Value;
+                }
+            }
+
+            return null;
+        }
+    }
+}
