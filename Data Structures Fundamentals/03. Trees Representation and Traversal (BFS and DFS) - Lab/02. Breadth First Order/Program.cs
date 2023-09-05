12f4080fee9bebf7440b86e75993153a10aaff19
Denis Yordanov <deyordanov777@gmail.com>
2023-09-05 11:23:05 +0300

    Added A New Exercise

---
+++ Data Structures Fundamentals/03. Trees Representation and Traversal (BFS and DFS) - Lab/02. Breadth First Order/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Tree<int> tree = new Tree<int>(10,
+                new Tree<int>(14),
+                            new Tree<int>(28),
+                            new Tree<int>(40,
+                                                new Tree<int>(15),
+                                                new Tree<int>(214),
+                                                new Tree<int>(102)));
+
+            Console.WriteLine(string.Join(" ", tree.OrderDfs()));
+        }
+    }
+}
