6d993923980e1174761bea2158afbc8609d0bfac
Denis Yordanov <deyordanov777@gmail.com>
2023-09-05 11:23:46 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/05. Not Released In/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBooksNotReleasedIn(context, year));
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate!.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
+
+
