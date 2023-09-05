9ec30e5e47980c1de1776911d002a3679d0d7640
Denis Yordanov <deyordanov777@gmail.com>
2023-09-05 11:23:38 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/04. Books by Price/BookShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace BookShop
+{
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+            Console.WriteLine(GetBooksByPrice(context).Length);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context.Books
+                .Where(b => b.Price > 40)
+                .Select(b => new { b.Title, b.Price })
+                .OrderByDescending(b => b.Price)
+                .ToArray();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - ${book.Price:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
