7d6d61f2b6e7857ca5857d5917b2a16873ddcab6
Denis Yordanov <deyordanov777@gmail.com>
2023-09-05 11:23:23 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/03. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetGoldenBooks(context));
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.EditionType == EditionType.Gold &&
+                            b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
+
+
