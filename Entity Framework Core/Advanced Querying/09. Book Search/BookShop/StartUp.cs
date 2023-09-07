0d0178ef1e4fe096e60e241ebcf673bac11217e2
Denis Yordanov <deyordanov777@gmail.com>
2023-09-07 11:24:45 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/09. Book Search/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using System.Globalization;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            string input = Console.ReadLine()!;
+
+            Console.WriteLine(GetBookTitlesContaining(context, input));
+        }
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var books = context.Books
+                .Where(b => b.Title.ToLower().Contains(input.ToLower()))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
+
+
