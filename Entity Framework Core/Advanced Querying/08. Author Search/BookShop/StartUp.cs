c6f2dbcec0ef8dcf5805d912c9d985a850c0a6dc
Denis Yordanov <deyordanov777@gmail.com>
2023-09-07 11:24:38 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/08. Author Search/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop
+{
+    using System.Globalization;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            string input = Console.ReadLine()!;
+
+            Console.WriteLine(GetAuthorNamesEndingIn(context, input));
+        }
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var authors = context.Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => new { a.FirstName, a.LastName })
+                .OrderBy(a => a.FirstName)
+                .ThenBy(a => a.LastName);
+
+            foreach (var author in authors)
+            {
+                sb.AppendLine($"{author.FirstName} {author.LastName}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
