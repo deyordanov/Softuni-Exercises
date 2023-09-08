6abb50ede83a5ce376303f53a28c68f0354d03e4
Denis Yordanov <deyordanov777@gmail.com>
2023-09-08 11:25:06 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/10. Book Search by Author/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using System.Globalization;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            string input = Console.ReadLine()!;
+
+            Console.WriteLine(GetBooksByAuthor(context, input));
+        }
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            var books = context.Books
+                .Where(b => b.Author.LastName.ToLower().StartsWith(input.ToLower()))
+                .OrderBy(b => b.BookId)
+                .Select(b => $"{b.Title} ({b.Author.FirstName} {b.Author.LastName})")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
+
+
---
+++ Entity Framework Core/Advanced Querying/11. Count Books/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => @"Server=localhost,1433;Database=MinionsDB;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True";
+    }
+}
