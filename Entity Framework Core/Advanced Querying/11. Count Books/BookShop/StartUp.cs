57602174bba823168563bb109d84522672744998
Denis Yordanov <deyordanov777@gmail.com>
2023-09-09 11:25:29 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/11. Count Books/BookShop/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace BookShop
+{
+    using System.Globalization;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountBooks(context, length));
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+            => context.Books.Count(b => b.Title.Length > lengthCheck);
+    }
+}
+
+
