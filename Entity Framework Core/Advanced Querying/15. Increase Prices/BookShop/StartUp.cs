d277b60b4df51e414fa19b7ea090e870b7f9955c
Denis Yordanov <deyordanov777@gmail.com>
2023-09-11 11:26:44 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/15. Increase Prices/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop
+{
+    using System.Globalization;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            IncreasePrices(context);
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate!.Value.Year < 2010)
+                .ToArray();
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
