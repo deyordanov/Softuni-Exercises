92f99e6b80621070c756c506946bae7bd3bf2afc
Denis Yordanov <deyordanov777@gmail.com>
2023-09-11 11:26:30 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/14. Most Recent Books/BookShop/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace BookShop
+{
+    using System.Globalization;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetMostRecentBooks(context));
+        }
+
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var categories  = context.Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    Books = c.CategoryBooks
+                        .OrderByDescending(b => b.Book.ReleaseDate!.Value)
+                        .Select(b => new 
+                        {
+                            b.Book.Title,
+                            Year = b.Book.ReleaseDate!.Value.Year,
+                        })
+                        .Take(3)
+                        .ToArray()
+                })
+                .OrderBy(c => c.Name)
+                .ToArray();
+
+            foreach (var category in categories)
+            {
+                sb.AppendLine($"--{category.Name}");
+                sb.AppendLine(string.Join(Environment.NewLine, category.Books.Select(b => $"{b.Title} ({b.Year})")));
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
