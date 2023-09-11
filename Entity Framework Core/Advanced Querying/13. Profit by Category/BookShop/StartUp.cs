579f271dcc97ec0729b9ee7178e31b1a77a1a152
Denis Yordanov <deyordanov777@gmail.com>
2023-09-11 11:26:13 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Advanced Querying/13. Profit by Category/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop
+{
+    using System.Globalization;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetTotalProfitByCategory(context));
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var profits = context.Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    TotalProfit = c.CategoryBooks.Sum(b => b.Book.Price * b.Book.Copies)
+                })
+                .OrderByDescending(c => c.TotalProfit)
+                .ThenBy(c => c.Name)
+                .ToArray();
+
+            foreach (var profit in profits.Select(c => $"{c.Name} ${c.TotalProfit:F2}"))
+            {
+                sb.AppendLine(profit);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
