69de52e199b0be0eb49d0b37b0e9b12982f4fcce
Denis Yordanov <deyordanov777@gmail.com>
2023-09-12 11:28:11 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/06. Export Sold Products/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using System.Text;
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using DTOs.Export;
+    using DTOs.Import;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string xml = File.ReadAllText("../../../Datasets/categories-products.xml");
+
+            Console.WriteLine(GetSoldProducts(context));
+        }
+
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ProductShopProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            StringBuilder sb = new StringBuilder();
+
+            ExportUserOneProductDto[] dtos = context.Users
+                .Where(u => u.ProductsSold.Count >= 1)
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .Take(5)
+                .ProjectTo<ExportUserOneProductDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            XmlRootAttribute root = new XmlRootAttribute("Users");
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportUserOneProductDto[]), root);
+
+            using StringWriter writer = new StringWriter(sb);
+
+            serializer.Serialize(writer, dtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
