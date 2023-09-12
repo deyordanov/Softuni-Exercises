918d5c9e909d7e1fc6129de34d0c977f47560d36
Denis Yordanov <deyordanov777@gmail.com>
2023-09-12 11:27:24 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/02. Import Products/ProductShopProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+namespace ProductShop
+{
+    using DTOs.Import;
+    using Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            this.CreateMap<ImportUserDto, User>();
+
+            //Product
+            this.CreateMap<ImportProductDto, Product>();
+
+            //Category
+            this.CreateMap<ImportCategoryDto, Category>();
+        }
+    }
+}
---
+++ Entity Framework Core/Extensible Markup Language - XML/02. Import Products/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using DTOs.Import;
+    using Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string xml = File.ReadAllText("../../../Datasets/products.xml");
+
+            Console.WriteLine(ImportProducts(context, xml));
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ProductShopProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            XmlRootAttribute root = new XmlRootAttribute("Products");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportProductDto[]), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            ImportProductDto[]? importedDtos =
+                (ImportProductDto[]?)serializer.Deserialize(reader);
+
+            Product[] products =
+                mapper.Map<Product[]>(importedDtos);
+
+            context.Products.AddRange(products);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+    }
+}
