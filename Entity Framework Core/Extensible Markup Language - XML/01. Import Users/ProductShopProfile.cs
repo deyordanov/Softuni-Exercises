870ad6b0f4ee7c8944f82625e796c713e07c0329
Denis Yordanov <deyordanov777@gmail.com>
2023-09-12 11:27:18 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/01. Import Users/ProductShopProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace ProductShop
+{
+    using DTOs.Import;
+    using Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            this.CreateMap<ImportUserDto, User>();
+        }
+    }
+}
---
+++ Entity Framework Core/Extensible Markup Language - XML/01. Import Users/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using DTOs.Import;
+    using Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string xml = File.ReadAllText("../../../Datasets/users.xml");
+
+            Console.WriteLine(ImportUsers(context, xml));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ProductShopProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            XmlRootAttribute root = new XmlRootAttribute("Users");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportUserDto[]), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            ImportUserDto[]? importedDtos 
+                = (ImportUserDto[]?)serializer.Deserialize(reader);
+
+            User[] users =
+                mapper.Map<User[]>(importedDtos);
+
+            context.Users.AddRange(users);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+    }
+}
