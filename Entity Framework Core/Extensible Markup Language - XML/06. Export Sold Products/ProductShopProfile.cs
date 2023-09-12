bf86de06dde0cc0c539e725c47db76082ed5f1c0
Denis Yordanov <deyordanov777@gmail.com>
2023-09-12 11:28:08 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/06. Export Sold Products/ProductShopProfile.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+
+namespace ProductShop
+{
+    using DTOs.Export;
+    using DTOs.Import;
+    using Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //IMPORT
+
+            //User
+            this.CreateMap<ImportUserDto, User>();
+
+            //Product
+            this.CreateMap<ImportProductDto, Product>();
+
+            //Category
+            this.CreateMap<ImportCategoryDto, Category>();
+
+            //CategoryProduct
+            this.CreateMap<ImportCategoriesAndProductsDto, CategoryProduct>();
+
+            //EXPORT
+
+            //Product
+            this.CreateMap<Product, ExportProductsInRangeDto>()
+                .ForMember(dest => dest.BuyerName,
+                    opt => opt.MapFrom(src => $"{src.Buyer.FirstName} {src.Buyer.LastName}"));
+
+            //User
+            this.CreateMap<User, ExportUserOneProductDto>()
+                .ForMember(dest => dest.SoldProducts,
+                    opt => opt.MapFrom(src => src.ProductsSold
+                        .Select(p => new ExportProductOneProductDto()
+                        {
+                            Name = p.Name,
+                            Price = p.Price,
+                        })));
+        }
+    }
+}
