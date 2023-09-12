27e9e7a1e81b4787dcc3e34b8cf7c126d560bf7f
Denis Yordanov <deyordanov777@gmail.com>
2023-09-12 11:27:42 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/04. Import Categories and Products/DTOs/Import/ImportCategoriesAndProductsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductShop.DTOs.Import;
+
+using System.Xml.Serialization;
+
+[XmlType("CategoryProduct")]
+public class ImportCategoriesAndProductsDto
+{
+    [XmlElement("CategoryId")]
+    public int CategoryId { get; set; }
+    [XmlElement("ProductId")]
+    public int ProductId { get; set; }
+}
