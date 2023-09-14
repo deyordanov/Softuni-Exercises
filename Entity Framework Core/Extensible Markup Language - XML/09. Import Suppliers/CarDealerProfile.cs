3aa85d411e90ab23f634cd6e1d63ab1f87f1989a
Denis Yordanov <deyordanov777@gmail.com>
2023-09-14 11:29:02 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/09. Import Suppliers/CarDealerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace CarDealer
+{
+    using DTOs.Import;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+        }
+    }
+}
