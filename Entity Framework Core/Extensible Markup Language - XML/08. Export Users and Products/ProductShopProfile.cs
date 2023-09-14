911d690a8afd54e084db450ad39cd7cc80f4c8ae
Denis Yordanov <deyordanov777@gmail.com>
2023-09-14 11:28:57 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/08. Export Users and Products/ProductShopProfile.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+
+namespace ProductShop
+{
+    using DTOs.Export;
+    using DTOs.Import;
+    using Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //IMPORT
+
+            //User
+            this.CreateMap<ImportUserDto, User>();
+
+            //Product
+            this.CreateMap<ImportProductDto, Product>();
+
+            //Category
+            this.CreateMap<ImportCategoryDto, Category>();
+
+            //CategoryProduct
+            this.CreateMap<ImportCategoriesAndProductsDto, CategoryProduct>();
+
+            //EXPORT
+
+            //Product
+            this.CreateMap<Product, ExportProductsInRangeDto>()
+                .ForMember(dest => dest.BuyerName,
+                    opt => opt.MapFrom(src => $"{src.Buyer.FirstName} {src.Buyer.LastName}"));
+
+            //User
+            this.CreateMap<User, ExportUserOneProductDto>()
+                .ForMember(dest => dest.SoldProducts,
+                    opt => opt.MapFrom(src => src.ProductsSold
+                        .Select(p => new ExportProductOneProductDto()
+                        {
+                            Name = p.Name,
+                            Price = p.Price,
+                        })));
+
+            this.CreateMap<User, ExportUsersAndProductsUserDto>()
+                .ForMember(dest => dest.SoldProducts, 
+                    opt => opt.MapFrom(src =>  new ExportUsersAndProductsSoldProductsDto()
+                    {
+                        Count = src.ProductsSold.Count,
+                        Products = src.ProductsSold.Select(ps => new ExportUsersAndProductsProductDto()
+                        {
+                            Name = ps.Name,
+                            Price = ps.Price,
+                        })
+                            .OrderByDescending(p => p.Price)
+                            .ToHashSet()
+                    }));
+
+            //Category
+            this.CreateMap<Category, ExportCategoryProductCountDto>()
+                .ForMember(dest => dest.Count,
+                    opt => opt.MapFrom(src => src.CategoryProducts.Count))
+                .ForMember(dest => dest.AveragePrice,
+                    opt => opt.MapFrom(src => src.CategoryProducts.Sum(cp => cp.Product.Price) / (decimal)src.CategoryProducts.Count))
+                .ForMember(dest => dest.TotalRevenue,
+                    opt => opt.MapFrom(src => src.CategoryProducts.Sum(cp => cp.Product
+                        .Price)));
+        }
+    }
+}
---
+++ Entity Framework Core/Extensible Markup Language - XML/08. Export Users and Products/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using System.Text;
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using DTOs.Export;
+    using DTOs.Import;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            Console.WriteLine(GetUsersWithProducts(context));
+        }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ProductShopProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            StringBuilder sb = new StringBuilder();
+
+            ExportUsersAndProductsUserDto[] dtos = context.Users
+                .AsNoTracking()
+                .Where(u => u.ProductsSold.Count >= 1)
+                .ProjectTo<ExportUsersAndProductsUserDto>(mapper.ConfigurationProvider)
+                .OrderByDescending(d => d.SoldProducts.Count)
+                .Take(10)
+                .ToArray();
+
+            ExportUsersAndProductsWrapperDto wrapper = new ExportUsersAndProductsWrapperDto()
+            {
+                Count = context.Users
+                    .Count(u => u.ProductsSold.Count >= 1),
+                Users = dtos
+            };
+
+            XmlRootAttribute root = new XmlRootAttribute("Users");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportUsersAndProductsWrapperDto), root);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+
+            serializer.Serialize(writer, wrapper, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
