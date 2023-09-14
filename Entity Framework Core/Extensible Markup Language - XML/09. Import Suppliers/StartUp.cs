6f2f78e886a269339e4dad74dac58376c0f57c17
Denis Yordanov <deyordanov777@gmail.com>
2023-09-14 11:29:07 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer
+{
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using CarDealer.Models;
+    using DTOs.Import;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string xml = File.ReadAllText("../../../Datasets/suppliers.xml");
+
+            Console.WriteLine(ImportSuppliers(context, xml));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = new Mapper(config);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            XmlRootAttribute root = new XmlRootAttribute("Suppliers");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportSupplierDto[]), root);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            ImportSupplierDto[]? importedDtos = (ImportSupplierDto[]?)serializer.Deserialize(reader);
+
+            Supplier[] suppliers =
+                mapper.Map<Supplier[]>(importedDtos);
+
+            context.Suppliers.AddRange(suppliers);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+    }
+}
---
+++ Entity Framework Core/Extensible Markup Language - XML/10. Import Parts/CarDealerProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace CarDealer
+{
+    using DTOs.Import;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //IMPORT
+
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+        }
+    }
+}
