374156cb6d47c5213951474032def83175bae88b
Denis Yordanov <deyordanov777@gmail.com>
2023-09-16 11:29:47 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/12. Import Customers/DTOs/Import/ImportSupplierDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using System.Xml.Serialization;
+
+[XmlType("Supplier")]
+public class ImportSupplierDto
+{
+    [XmlElement("name")]
+    public string Name { get; set; } = null!;
+    [XmlElement("isImporter")]
+    public bool IsImporter { get; set; }
+}
