ed49e7afe217b45cd5188b81ffc6408f25297b36
Denis Yordanov <deyordanov777@gmail.com>
2023-09-16 11:29:43 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/11. Import Cars/Utilities/XmlHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace CarDealer.Utilities;
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Xml.Serialization;
+
+public class XmlHelper
+{
+    public static T[] CollectionDeserializer<T>(string objectsAsString, string rootName)
+    {
+        XmlRootAttribute root = new XmlRootAttribute(rootName);
+
+        XmlSerializer serializer = new XmlSerializer(typeof(T[]), root);
+
+        using StringReader reader = new StringReader(objectsAsString);
+
+        return serializer.Deserialize(reader) as T[];
+    }
+
+    public static T Deserializer<T>(string objectAsString, string rootName)
+        where T : class
+    {
+        XmlRootAttribute root = new XmlRootAttribute(rootName);
+
+        XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+        using StringReader reader = new StringReader(objectAsString);
+
+        return serializer.Deserialize(reader) as T;
+    }
+}
---
+++ Entity Framework Core/Extensible Markup Language - XML/12. Import Customers/CarDealerProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+
+namespace CarDealer
+{
+    using Data;
+    using DTOs.Import;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //IMPORT
+
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>()
+                .ForMember(dest => dest.PartsCars,
+                    opt => opt.MapFrom(src => src
+                        .Parts
+                        .DistinctBy(x => x.PartId)
+                        .Select(pi => new PartCar()
+                        {
+                            PartId = pi.PartId,
+                        })));
+
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>();
+        }
+    }
+}
