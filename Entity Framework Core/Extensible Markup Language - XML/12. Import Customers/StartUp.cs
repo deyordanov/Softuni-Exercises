66335b7cc4445851ca8c105005ba31c8c3dea793
Denis Yordanov <deyordanov777@gmail.com>
2023-09-16 11:30:01 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/12. Import Customers/StartUp.cs
@@ -0,0 +1,73 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer
+{
+    using System.IO;
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using Models;
+    using DTOs.Import;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string xml = File.ReadAllText("../../../Datasets/customers.xml");
+
+            Console.WriteLine(ImportCustomers(context, xml));
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = new Mapper(config);
+
+            XmlRootAttribute root = new XmlRootAttribute("Cars");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportCarDto[]), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            ImportCarDto[]? importedDtos =
+                (ImportCarDto[]?)serializer.Deserialize(reader);
+
+            Car[] cars =
+                mapper.Map<Car[]>(importedDtos);
+
+            context.Cars
+                .AddRange(cars);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Length}";
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputXml)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = new Mapper(config);
+
+            XmlRootAttribute root = new XmlRootAttribute("Customers");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportCustomerDto[]), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            ImportCustomerDto[]? importedDtos =
+                (ImportCustomerDto[]?)serializer.Deserialize(reader);
+
+            Customer[] customers =
+                mapper.Map<Customer[]>(importedDtos);
+
+            context.Customers.AddRange(customers);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Length}";
+        }
+    }
+}
