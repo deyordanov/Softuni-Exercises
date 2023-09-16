016bfd1a4d5209895507ab3bb67ffe59eab6e1bb
Denis Yordanov <deyordanov777@gmail.com>
2023-09-16 11:30:05 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/13. Import Sales/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer
+{
+    using System.IO;
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using Models;
+    using DTOs.Import;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string xml = File.ReadAllText("../../../Datasets/sales.xml");
+
+            Console.WriteLine(ImportSales(context, xml));
+        }
+
+        public static string ImportSales(CarDealerContext context, string inputXml)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = new Mapper(config);
+
+            XmlRootAttribute root = new XmlRootAttribute("Sales");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportSaleDto[]), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            ImportSaleDto[]? importedDtos =
+                (ImportSaleDto[]?)serializer.Deserialize(reader);
+
+            Sale[] sales =
+                mapper.Map<Sale[]>(importedDtos
+                    .Where(s => context.Cars
+                        .Find(s.CarId) != null));
+
+            context.Sales.AddRange(sales);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Length}";
+        }
+    }
+}
---
+++ Entity Framework Core/Extensible Markup Language - XML/14. Export Cars With Distance/CarDealerProfile.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+
+namespace CarDealer
+{
+    using Data;
+    using DTOs.Export;
+    using DTOs.Import;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //IMPORT
+
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>()
+                .ForMember(dest => dest.PartsCars,
+                    opt => opt.MapFrom(src => src
+                        .Parts
+                        .DistinctBy(x => x.PartId)
+                        .Select(pi => new PartCar()
+                        {
+                            PartId = pi.PartId,
+                        })));
+
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>();
+
+            //Sale
+            this.CreateMap<ImportSaleDto, Sale>();
+
+            //EXPORT
+
+            //Car
+            this.CreateMap<Car, ExportCarDto>();
+        }
+    }
+}
