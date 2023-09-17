35805e517a26b7934112bf20f3bf4bcdf63ed13e
Denis Yordanov <deyordanov777@gmail.com>
2023-09-17 11:35:34 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/15. Export Cars From Make BMW/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer
+{
+    using System.IO;
+    using System.Text;
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using DTOs.Export;
+    using Models;
+    using DTOs.Import;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetCarsFromMakeBmw(context));
+        }
+
+        public static string GetCarsFromMakeBmw(CarDealerContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = new Mapper(config);
+
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute root = new XmlRootAttribute("cars");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportBmwCarDto[]), root);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+
+            ExportBmwCarDto[] dtos = context.Cars
+                .Where(c => c.Make == "BMW")
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .ProjectTo<ExportBmwCarDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            serializer.Serialize(writer, dtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
