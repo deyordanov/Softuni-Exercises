9363427dbe871ce09bdea077c5c41ca55c0f2eaa
Denis Yordanov <deyordanov777@gmail.com>
2023-09-17 11:35:50 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/16. Export Local Suppliers/DTOs/Import/ImportCarDto.cs
@@ -0,0 +1,35 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using System.Xml.Serialization;
+
+[XmlType("Car")]
+public class ImportCarDto
+{
+    [XmlElement("make")]
+    public string Make { get; set; } = null!;
+    [XmlElement("model")]
+    public string Model { get; set; } = null!;
+    [XmlElement("traveledDistance")]
+    public long TraveledDistance { get; set; }
+    [XmlArray("parts")]
+    public ImportCarPartDto[] Parts { get; set; } = null!;
+
+    //  <Car>
+    // <make>Opel</make>
+    // <model>Omega</model>
+    // <traveledDistance>176664996</traveledDistance>
+    // <parts>
+    // <partId id="38"/>
+    // <partId id="102"/>
+    // <partId id="23"/>
+    // <partId id="116"/>
+    // <partId id="46"/>
+    // <partId id="68"/>
+    // <partId id="88"/>
+    // <partId id="104"/>
+    // <partId id="71"/>
+    // <partId id="32"/>
+    // <partId id="114"/>
+    // </parts>
+    // </Car>
+}
