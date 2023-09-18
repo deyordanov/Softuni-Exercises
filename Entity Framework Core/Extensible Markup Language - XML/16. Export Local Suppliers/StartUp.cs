23478ff4f975fcda9c39de2552bed7e4db9f26a0
Denis Yordanov <deyordanov777@gmail.com>
2023-09-18 11:36:03 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/16. Export Local Suppliers/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer
+{
+    using System.IO;
+    using System.Text;
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using DTOs.Export;
+    using Models;
+    using DTOs.Import;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetLocalSuppliers(context));
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = new Mapper(config);
+
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute root = new XmlRootAttribute("suppliers");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportSupplierDto[]), root);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+
+            ExportSupplierDto[] dtos = context.Suppliers
+                .Where(s => s.IsImporter == false)
+                .ProjectTo<ExportSupplierDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            serializer.Serialize(writer, dtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
