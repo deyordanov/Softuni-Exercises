37e259bdcdba1f5a28a25489f898b59751a8b591
Denis Yordanov <deyordanov777@gmail.com>
2023-09-18 11:36:11 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/17. Export Cars With Their List Of Parts/DTOs/Export/ExportSupplierDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System.Xml.Serialization;
+
+[XmlType("supplier")]
+public class ExportSupplierDto
+{
+    [XmlAttribute("id")]
+    public int Id { get; set; }
+    [XmlAttribute("name")]
+    public string Name { get; set; } = null!;
+    [XmlAttribute("parts-count")]
+    public int PartsCount { get; set; }
+}
