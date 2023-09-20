6ea17bc034d82102acfa538b178033f0ba0c1fc5
Denis Yordanov <deyordanov777@gmail.com>
2023-09-20 11:37:35 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/01. Import Users/DTOs/Import/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductShop.DTOs.Import;
+
+public class UserDto
+{
+    public string? FirstName { get; set; }
+    public string LastName { get; set; } = null!;
+    public int? Age { get; set; }
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/01. Import Users/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using AutoMapper;
+    using Data.Profiles;
+    using DTOs.Import;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string json = File.ReadAllText("../../../Datasets/users.json");
+            
+            Console.WriteLine(ImportUsers(context, json));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new UserProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            UserDto[]? userDtos = 
+                JsonConvert.DeserializeObject<UserDto[]>(inputJson);
+
+            User[]? users =
+                mapper.Map<User[]>(userDtos);
+
+            context.AddRange(users);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+
+    }
+}
