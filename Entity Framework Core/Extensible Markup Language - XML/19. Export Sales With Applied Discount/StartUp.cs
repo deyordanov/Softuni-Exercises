f3b53d8d7565a3af14f2e4c780110ce7ce327203
Denis Yordanov <deyordanov777@gmail.com>
2023-09-20 11:37:29 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/19. Export Sales With Applied Discount/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer
+{
+    using System.IO;
+    using System.Text;
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using DTOs.Export;
+    using Models;
+    using DTOs.Import;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetSalesWithAppliedDiscount(context));
+        }
+
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = new Mapper(config);
+
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute root = new XmlRootAttribute("sales");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportSaleDto[]), root);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+
+            ExportSaleDto[] dtos = context.Sales
+                .ProjectTo<ExportSaleDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            serializer.Serialize(writer, dtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/01. Import Users/Data/Profiles/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductShop.Data.Profiles;
+
+using AutoMapper;
+using DTOs.Import;
+using Models;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<User, UserDto>().ReverseMap();
+    }
+}
