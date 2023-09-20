8b077c456bad97b022a13759860edbec1f07c046
Denis Yordanov <deyordanov777@gmail.com>
2023-09-20 11:37:12 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/19. Export Sales With Applied Discount/CarDealerProfile.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+
+namespace CarDealer
+{
+    using Data;
+    using DTOs.Export;
+    using DTOs.Import;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //IMPORT
+
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>()
+                .ForMember(dest => dest.PartsCars,
+                    opt => opt.MapFrom(src => src
+                        .Parts
+                        .DistinctBy(x => x.PartId)
+                        .Select(pi => new PartCar()
+                        {
+                            PartId = pi.PartId,
+                        })));
+
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>();
+
+            //Sale
+            this.CreateMap<ImportSaleDto, Sale>();
+
+            //EXPORT
+
+            //Car
+            this.CreateMap<Car, ExportCarDto>();
+
+            this.CreateMap<Car, ExportBmwCarDto>();
+
+            //Supplier
+            this.CreateMap<Supplier, ExportSupplierDto>()
+                .ForMember(d => d.PartsCount,
+                    o => o.MapFrom(s => s.Parts.Count));
+
+            //Product
+            this.CreateMap<Part, ExportCarPartDto>();
+
+            //Customer
+            this.CreateMap<Customer, ExportCustomerDto>()
+                .ForMember(d => d.FullName,
+                    o => o.MapFrom(s => s.Name))
+                .ForMember(d => d.CarsBought,
+                    o => o.MapFrom(s => s.Sales.Count))
+                .ForMember(d => d.MoneySpent,
+                    o => o.MapFrom(s => s.Sales.Select(s => s.Car)
+                        .SelectMany(c => c.PartsCars).Sum(pc => pc.Part.Price)));
+
+                //Sale
+                this.CreateMap<Sale, ExportSaleDto>()
+                    .ForMember(d => d.Price,
+                        o => o.MapFrom(s => (double)s.Car.PartsCars
+                            .Sum(pc => pc.Part.Price)))
+                    .ForMember(d => d.PriceWithDiscount,
+                        o => o.MapFrom(s => (double)(s.Car.PartsCars
+                                                         .Sum(pc => pc.Part.Price) -
+                                                     (s.Car.PartsCars.Sum(pc => pc.Part.Price) * (s.Discount / 100M)))));
+        }
+    }
+}
