440aa9d864b2668135c2e21ba78d119310705b8b
Denis Yordanov <deyordanov777@gmail.com>
2023-09-20 11:37:18 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Extensible Markup Language - XML/19. Export Sales With Applied Discount/DTOs/Export/ExportCustomerDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System.Xml.Serialization;
+
+[XmlType("customer")]
+public class ExportCustomerDto
+{
+    [XmlAttribute("full-name")]
+    public string FullName { get; set; } = null!;
+    [XmlAttribute("bought-cars")]
+    public int CarsBought { get; set; }
+
+    [XmlAttribute("spent-money")]
+    public decimal MoneySpent { get; set; }
+}
---
+++ Entity Framework Core/Extensible Markup Language - XML/19. Export Sales With Applied Discount/DTOs/Export/ExportSaleDto.cs
@@ -0,0 +1,18 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System.Xml.Serialization;
+
+[XmlType("sale")]
+public class ExportSaleDto
+{
+    [XmlElement("car")]
+    public ExportCarDto Car { get; set; } = null!;
+    [XmlElement("discount")]
+    public decimal Discount { get; set; }
+    [XmlElement("customer-name")]
+    public string CustomerName { get; set; } = null!;
+    [XmlElement("price")]
+    public double Price { get; set; }
+    [XmlElement("price-with-discount")]
+    public double PriceWithDiscount { get; set; }
+}
