3eb69516dc82ff8e82656055ca21dc93b29e4243
Denis Yordanov <deyordanov777@gmail.com>
2023-09-21 11:38:34 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/06. Export Sold Products/DTOs/Export/ProductsInRangeDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductShop.DTOs.Export;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+public class ProductsInRangeDto
+{
+    [JsonProperty("name")]
+    public string ProductName { get; set; } = null!;
+    [JsonProperty("price")]
+    public decimal ProductPrice { get; set; }
+    [JsonProperty("seller")]
+    public string SellerName { get; set; } = null!;
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/06. Export Sold Products/DTOs/Export/SoldProducts/SoldProductsProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductShop.DTOs.Export.SoldProducts;
+
+using Newtonsoft.Json;
+
+public class SoldProductsProductDto
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+    [JsonProperty("price")]
+    public decimal Price { get; set; }
+    [JsonProperty("buyerFirstName")]
+    public string BuyerFirstName { get; set; } = null!;
+    [JsonProperty("buyerLastName")]
+    public string BuyerLastName { get; set; } = null!;
+}
