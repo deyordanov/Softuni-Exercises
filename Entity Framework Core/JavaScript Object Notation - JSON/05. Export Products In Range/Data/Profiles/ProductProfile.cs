a671ebe7621e729fc52dfd470c9c372dc4727125
Denis Yordanov <deyordanov777@gmail.com>
2023-09-21 11:38:14 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/05. Export Products In Range/Data/Profiles/ProductProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductShop.Data.Profiles;
+
+using AutoMapper;
+using DTOs.Import;
+using Models;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductDto>().ReverseMap();
+    }
+}
