a4d1255a1b361f9c06d106e2ee746096205c29d4
Denis Yordanov <deyordanov777@gmail.com>
2023-09-21 11:38:24 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/05. Export Products In Range/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data.Profiles;
+    using DTOs.Export;
+    using DTOs.Import;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+            
+            Console.WriteLine(GetProductsInRange(context));
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ProductInRangeProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            ProductsInRangeDto[] dtos = context.Products
+                .AsNoTracking()
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(x => x.Price)
+                .ProjectTo<ProductsInRangeDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(dtos, Formatting.Indented);
+        }
+    }
+}
