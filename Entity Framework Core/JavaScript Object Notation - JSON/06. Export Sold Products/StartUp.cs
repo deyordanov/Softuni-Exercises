6fc4424752e68f0aa938197e72f11f83b1721986
Denis Yordanov <deyordanov777@gmail.com>
2023-09-22 11:38:44 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/06. Export Sold Products/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data.Profiles;
+    using DTOs.Export;
+    using DTOs.Export.SoldProducts;
+    using DTOs.Import;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+            
+            Console.WriteLine(GetSoldProducts(context));
+        }
+
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new SoldProductsProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            var a = context.Products
+                .ProjectTo<SoldProductsProductDto>(mapper.ConfigurationProvider);
+
+            SoldProductsUserDto[] dtos = context.Users
+                .Include(u => u.ProductsSold)
+                .AsNoTracking()
+                .Where(u => u.ProductsSold.Any(p => p.Buyer != null))
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .ProjectTo<SoldProductsUserDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(dtos, Formatting.Indented);
+        }
+    }
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/07. Export Categories By Products Count/Data/Configurations/CategoryProductConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductShop.Data.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+
+public class CategoryProductConfiguration : IEntityTypeConfiguration<CategoryProduct>
+{
+    public void Configure(EntityTypeBuilder<CategoryProduct> builder)
+    {
+        builder
+            .HasKey(k => new { k.CategoryId, k.ProductId });
+    }
+}
