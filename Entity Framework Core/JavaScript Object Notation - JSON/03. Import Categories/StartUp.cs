7907c5fc0b299725701daf4a936261693c87dcad
Denis Yordanov <deyordanov777@gmail.com>
2023-09-22 11:39:02 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/03. Import Categories/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using AutoMapper;
+    using Data.Profiles;
+    using DTOs.Import;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string json = File.ReadAllText("../../../Datasets/categories.json");
+            
+            Console.WriteLine(ImportCategories(context, json));
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new CategoryProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            CategoryDto[]? categoryDtos =
+                JsonConvert.DeserializeObject<CategoryDto[]>(inputJson);
+
+            Category[]? categories =
+                mapper.Map<Category[]>(categoryDtos
+                    .Where(dto => dto.Name != null));
+
+            context.Categories.AddRange(categories);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Length}";
+        }
+    }
+}
