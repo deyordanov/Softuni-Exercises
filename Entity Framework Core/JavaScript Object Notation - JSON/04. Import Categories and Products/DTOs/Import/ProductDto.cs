0ce1c7a73e87461f737df899e65525512bb88856
Denis Yordanov <deyordanov777@gmail.com>
2023-09-22 11:38:58 +0300

    v

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/04. Import Categories and Products/DTOs/Import/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductShop.DTOs.Import;
+
+using Newtonsoft.Json;
+
+public class ProductDto
+{
+    [JsonProperty(nameof(Name))]
+    public string Name { get; set; } = null!;
+    [JsonProperty(nameof(Price))]
+    public decimal Price { get; set; }
+    [JsonProperty(nameof(SellerId))]
+    public int SellerId { get; set; }
+    [JsonProperty(nameof(BuyerId))]
+    public int? BuyerId { get; set; }
+}
