eb6e1f1fa5bcf8f74681f134bfe47177e45d373d
Denis Yordanov <deyordanov777@gmail.com>
2023-09-23 11:39:31 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/07. Export Categories By Products Count/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data.Profiles;
+    using DTOs.Export;
+    using DTOs.Export.SoldProducts;
+    using DTOs.Import;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+            
+            Console.WriteLine(GetCategoriesByProductsCount(context));
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new CategoriesByCountProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            CategoriesByCountDto[] dtos = context.Categories
+                .OrderByDescending(c => c.CategoriesProducts.Count)
+                .ProjectTo<CategoriesByCountDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(dtos, Formatting.Indented);
+        }
+    }
+}
