518ecaa5caeeeef997fe9d1bec92543f6f7375fb
Denis Yordanov <deyordanov777@gmail.com>
2023-09-24 11:39:57 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/08. Export Users and Products/Data/Profiles/SoldProductsProfile.cs
@@ -0,0 +1,28 @@
+﻿namespace ProductShop.Data.Profiles;
+
+using AutoMapper;
+using DTOs.Export.SoldProducts;
+using Models;
+
+public class SoldProductsProfile : Profile
+{
+    public SoldProductsProfile()
+    {
+        //Applying filter through the AutoMapper
+        CreateMap<User, SoldProductsUserDto>()
+            .ForMember(dest => dest.ProductsSold, 
+                opt => opt.MapFrom(x => x.ProductsSold.Where(p => p.Buyer != null)));
+
+        CreateMap<Product, SoldProductsProductDto>();
+
+        // IMapper mapper = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<Product, SoldProductsProductDto>()
+        //     .ForMember(dest => dest.BuyerFirstName, 
+        //         opt => opt.MapFrom(x => x.Seller.FirstName))
+        //     .ForMember(dest => dest.BuyerLastName,
+        //         opt => opt.MapFrom(x => x.Seller.LastName))));
+
+        // CreateMap<User, SoldProductsUserDto>()
+        //     .ForMember(dest => dest.SoldProducts,
+        //         opt => opt.MapFrom(x => x.ProductsBought.Select(p => mapper.Map<SoldProductsProductDto>(p))));
+    }
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/08. Export Users and Products/Data/Profiles/UsersAndProductsProfile.cs
@@ -0,0 +1,28 @@
+﻿namespace ProductShop.Data.Profiles;
+
+using AutoMapper;
+using DTOs.Export.UsersAndProducts;
+using Models;
+
+public class UsersAndProductsProfile : Profile
+{
+    public UsersAndProductsProfile()
+    {
+        CreateMap<User, UserDestination>()
+            .ForMember(dest => dest.SoldProducts, opt => opt.MapFrom(src => new SoldProductsResult
+            {
+                Count = src.ProductsSold
+                    .Count(p => p.Buyer != null),
+                Products = src.ProductsSold
+                    .Where(p => p.Buyer != null)
+                    .Select(p =>
+                    new ProductDestination
+                    {
+                        Price = p.Price,
+                        Name = p.Name,
+                    }).ToList()
+            }));
+
+        CreateMap<Product, ProductDestination>();
+    }
+}
