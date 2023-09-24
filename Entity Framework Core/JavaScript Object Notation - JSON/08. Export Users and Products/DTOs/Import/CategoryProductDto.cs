f5e0b2c708a51d642182fd2ffc379c5442850933
Denis Yordanov <deyordanov777@gmail.com>
2023-09-24 11:40:16 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/08. Export Users and Products/DTOs/Import/CategoryProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductShop.DTOs.Import;
+
+using Newtonsoft.Json;
+
+public class CategoryProductDto
+{
+    [JsonProperty(nameof(CategoryId))]
+    public int CategoryId { get; set; }
+    [JsonProperty(nameof(ProductId))]
+    public int ProductId { get; set; }
+}
