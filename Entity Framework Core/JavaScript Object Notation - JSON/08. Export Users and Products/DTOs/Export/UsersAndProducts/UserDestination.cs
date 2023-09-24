20cabb17b2d7c2a9a63e9a8ff5999d50b784c13d
Denis Yordanov <deyordanov777@gmail.com>
2023-09-24 11:40:44 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/08. Export Users and Products/DTOs/Export/UsersAndProducts/UserDestination.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductShop.DTOs.Export.UsersAndProducts;
+
+using Newtonsoft.Json;
+
+public class UserDestination
+{
+    [JsonProperty("firstName")]
+    public string? FirstName { get; set; }
+    [JsonProperty("lastName")]
+    public string LastName { get; set; } = null!;
+    [JsonProperty("age")]
+    public int? Age { get; set; }
+    [JsonProperty("soldProducts")]
+    public SoldProductsResult SoldProducts { get; set; } = null!;
+}
