650d37174bd7d9279fdc192e0d2b03d0efa14963
Denis Yordanov <deyordanov777@gmail.com>
2023-09-26 11:42:11 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/11. Import Cars/DTOs/Import/SupplierDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using Newtonsoft.Json;
+
+public class SupplierDto
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+    [JsonProperty("isImporter")]
+    public bool IsImporter { get; set; }
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/11. Import Cars/Models/Sale.cs
@@ -0,0 +1,21 @@
+﻿namespace CarDealer.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Sale
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public decimal Discount { get; set; }
+
+        [ForeignKey(nameof(Car))]
+        public int CarId { get; set; }
+        public Car Car { get; set; } = null!;
+
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!; 
+    }
+}
