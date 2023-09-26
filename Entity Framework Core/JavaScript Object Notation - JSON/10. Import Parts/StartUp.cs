95567cc2c46e4cd4d16229e1c3806b0ad34d3c60
Denis Yordanov <deyordanov777@gmail.com>
2023-09-26 11:41:56 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/10. Import Parts/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using Data;
+    using Data.Profiles;
+    using DTOs.Import;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string json = File.ReadAllText("../../../Datasets/parts.json");
+
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(ImportParts(context, json));
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var config = new MapperConfiguration(cfg => cfg
+                .AddProfile(new PartProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            PartDto[]? partDtos = JsonConvert
+                .DeserializeObject<PartDto[]>(inputJson);
+
+            Part[] parts = mapper
+                .Map<Part[]>(partDtos)
+                .Where(p => context.Suppliers.Find(p.SupplierId) != null)
+                .ToArray();
+
+            context.Parts
+                .AddRange(parts);
+
+            context
+                .SaveChanges();
+
+            return $"Successfully imported {parts.Length}.";
+        }
+    }
+}
