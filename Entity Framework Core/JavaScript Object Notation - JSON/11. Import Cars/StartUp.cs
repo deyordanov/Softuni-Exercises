079103bd4b276272aa4ca59ecb8d63ed2e5082dd
Denis Yordanov <deyordanov777@gmail.com>
2023-09-26 11:42:15 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/11. Import Cars/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using Data;
+    using Data.Profiles;
+    using DTOs.Import;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string json = File.ReadAllText("../../../Datasets/cars.json");
+
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(ImportCars(context, json));
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new CarProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            CarDto[]? carDtos = JsonConvert.DeserializeObject<CarDto[]>(inputJson);
+
+            Car[]? cars = mapper.Map<Car[]>(carDtos);
+
+            context.Cars.AddRange(cars);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Length}.";
+        }
+    }
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/12. Import Customers/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=localhost,1433;Database=CarDealerDb;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True";
+    }
+}
