860714f9eea58029938e0cefc012a7ad1967e846
Denis Yordanov <deyordanov777@gmail.com>
2023-09-27 11:42:37 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/12. Import Customers/Data/Profiles/CarProfile.cs
@@ -0,0 +1,18 @@
+﻿namespace CarDealer.Data.Profiles;
+
+using AutoMapper;
+using DTOs.Import;
+using Models;
+
+public class CarProfile : Profile
+{
+    public CarProfile()
+    {
+        CreateMap<CarDto, Car>()
+            .ForMember(dest => dest.PartsCars,
+                opt => opt.MapFrom(src => src.PartsId.ToHashSet().Select(pi => new PartCar()
+                {
+                    PartId = pi
+                })));
+    }
+}
