7a7b4b35668a16d565a2902820409bb384606644
Denis Yordanov <deyordanov777@gmail.com>
2023-09-27 11:43:00 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/12. Import Customers/Data/Profiles/SupplierProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealer.Data.Profiles;
+
+using AutoMapper;
+using DTOs.Import;
+using Models;
+
+public class SupplierProfile : Profile
+{
+    public SupplierProfile()
+    {
+        CreateMap<SupplierDto, Supplier>();
+    }
+}
