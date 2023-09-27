dc1cbce6c6a8b0bac8c086d3a6307d80fd5a7fc0
Denis Yordanov <deyordanov777@gmail.com>
2023-09-27 11:42:44 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/12. Import Customers/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using Castle.Core.Resource;
+    using Data;
+    using Data.Profiles;
+    using DTOs.Import;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string json = File.ReadAllText("../../../Datasets/customers.json");
+
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(ImportCustomers(context, json));
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new CustomerProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            CustomerDto[]? customerDtos = JsonConvert.DeserializeObject<CustomerDto[]>(inputJson);
+
+            Customer[] customers = mapper.Map<Customer[]>(customerDtos);
+
+            context.Customers.AddRange(customers);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Length}.";
+        }
+    }
+}
