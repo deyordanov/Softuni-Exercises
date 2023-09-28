7b7f2d700b5f226105d558109d5f65334c924e32
Denis Yordanov <deyordanov777@gmail.com>
2023-09-28 11:43:19 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/13. Import Sales/DTOs/Import/CarDto.cs
@@ -0,0 +1,19 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using Newtonsoft.Json;
+
+public class CarDto
+{
+    public CarDto()
+    {
+        this.PartsId = new List<int>();
+    }
+    [JsonProperty("make")]
+    public string Make { get; set; } = null!;
+    [JsonProperty("model")]
+    public string Model { get; set; } = null!;
+    [JsonProperty("traveledDistance")]
+    public long TravelledDistance { get; set; }
+    [JsonProperty("partsId")]
+    public ICollection<int> PartsId { get; set; }
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/13. Import Sales/Data/Profiles/PartProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealer.Data.Profiles;
+
+using AutoMapper;
+using DTOs.Import;
+using Models;
+
+public class PartProfile : Profile
+{
+    public PartProfile()
+    {
+        CreateMap<PartDto, Part>();
+    }
+}
