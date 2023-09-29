8ad484f46fb4ed5a1f0136b784f27c818ebd6eac
Denis Yordanov <deyordanov777@gmail.com>
2023-09-29 11:43:56 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/16. Export Local Suppliers/DTOs/Export/CarDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using Newtonsoft.Json;
+
+public class CarDto
+{
+    [JsonProperty(nameof(Id))]
+    public int Id { get; set; }
+    [JsonProperty(nameof(Make))]
+    public string Make { get; set; } = null!;
+    [JsonProperty(nameof(Model))]
+    public string Model { get; set; } = null!;
+    [JsonProperty(nameof(TraveledDistance))]
+    public long TraveledDistance { get; set; }
+}
