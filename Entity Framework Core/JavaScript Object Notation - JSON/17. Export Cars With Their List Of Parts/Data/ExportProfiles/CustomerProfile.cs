3394428756d0f1b1e8cf56e928394fda1f5e4431
Denis Yordanov <deyordanov777@gmail.com>
2023-09-29 11:44:07 +0300

    v

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/17. Export Cars With Their List Of Parts/Data/ExportProfiles/CustomerProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealer.Data.ExportProfiles;
+
+using AutoMapper;
+using DTOs.Export;
+using Models;
+
+public class CustomerProfile : Profile
+{
+    public CustomerProfile()
+    {
+        CreateMap<Customer, CustomerDto>()
+            .ForMember(dest => dest.BirthDate,
+                opt => opt.MapFrom(src => src.BirthDate.ToString("dd/MM/yyyy")));
+    }
+}
