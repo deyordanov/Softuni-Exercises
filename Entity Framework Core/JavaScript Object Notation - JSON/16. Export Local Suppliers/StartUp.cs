e357de9dc62bc7836800cf6c2c3dfe05a0d716bc
Denis Yordanov <deyordanov777@gmail.com>
2023-09-29 11:44:02 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/16. Export Local Suppliers/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using Data.ExportProfiles;
+    using DTOs.Export;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetLocalSuppliers(context));
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new SupplierProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            SupplierDto[] dtos = context.Suppliers
+                .Where(s => s.IsImporter == false)
+                .ProjectTo<SupplierDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(dtos, Formatting.Indented);
+        }
+    }
+}
