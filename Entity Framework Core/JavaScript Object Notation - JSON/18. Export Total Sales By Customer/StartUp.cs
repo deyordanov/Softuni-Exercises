aa2ab0275d577bd29cb5ddfdcce92ddd5e7f3eb0
Denis Yordanov <deyordanov777@gmail.com>
2023-09-30 11:45:39 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/18. Export Total Sales By Customer/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using Data.ExportProfiles;
+    using DTOs.Export;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetTotalSalesByCustomer(context));
+        }
+
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new SalesCustomerProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            SalesCustomerDto[] dtos = context.Customers
+                .AsNoTracking()
+                .Where(c => c.Sales.Count >= 1)
+                .ProjectTo<SalesCustomerDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(dtos
+                .OrderByDescending(c => c.MoneySpent)
+                .ThenByDescending(c => c.CarsBought), Formatting.Indented);
+        }
+    }
+}
