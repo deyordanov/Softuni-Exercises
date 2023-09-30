8164ce14e7f8311a8e2d6ae574455524e4d3b23e
Denis Yordanov <deyordanov777@gmail.com>
2023-09-30 11:45:18 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/18. Export Total Sales By Customer/DTOs/Import/PartDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using Newtonsoft.Json;
+
+public class PartDto
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+    [JsonProperty("price")]
+    public decimal Price { get; set; }
+    [JsonProperty("quantity")]
+    public int Quantity { get; set; }
+    [JsonProperty("supplierId")]
+    public int SupplierId { get; set; }
+}
