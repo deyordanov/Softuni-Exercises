a4a5ca6e1b38ecdd819ee3df18cf98ff46695605
Denis Yordanov <deyordanov777@gmail.com>
2023-10-01 11:46:02 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/17. Export Cars With Their List Of Parts/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using Data.ExportProfiles;
+    using DTOs.Export;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetCarsWithTheirListOfParts(context));
+        }
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var config = new MapperConfiguration(src => src.AddProfile(new PartsProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            PartsWrapperDto[] dtos = context.Cars
+                .ProjectTo<PartsWrapperDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(dtos, Formatting.Indented);
+        }
+    }
+}
