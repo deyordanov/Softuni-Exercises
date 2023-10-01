7fad428d79ce26294c421cab41ad187dde575b26
Denis Yordanov <deyordanov777@gmail.com>
2023-10-01 11:46:12 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/19. Export Sales With Applied Discount/Data/ImportProfiles/SaleProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealer.Data.Profiles;
+
+using AutoMapper;
+using DTOs.Import;
+using Models;
+
+public class SaleProfile : Profile
+{
+    public SaleProfile()
+    {
+        CreateMap<SaleDto, Sale>();
+    }
+}
