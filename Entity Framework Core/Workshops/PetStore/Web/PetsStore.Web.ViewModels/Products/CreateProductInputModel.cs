31f80034876e25d5052dc8d02a6e2491dde4edd7
Denis Yordanov <deyordanov777@gmail.com>
2023-10-03 11:47:29 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/PetStore/Web/PetsStore.Web.ViewModels/Products/CreateProductInputModel.cs
@@ -0,0 +1,23 @@
+﻿namespace PetsStore.Web.ViewModels.Products;
+
+using Data.Models;
+using PetsStore.Data.Models.Common;
+using Services.Mapping;
+using System.ComponentModel.DataAnnotations;
+
+public class CreateProductInputModel : IMapTo<Product>
+{
+    [Required(ErrorMessage = ProductValidationConstants.NameIsRequired)]
+    [MaxLength(ProductValidationConstants.NameMaxLength, ErrorMessage = ProductValidationConstants.NameMaxLengthMessage)]
+    [MinLength(ProductValidationConstants.NameMinLength, ErrorMessage = ProductValidationConstants.NameMinLengthMessage)]
+    public string Name { get; set; }
+
+    [Required]
+    [Range(typeof(decimal), ProductValidationConstants.PriceMinValue, ProductValidationConstants.PriceMaxValue, ErrorMessage = ProductValidationConstants.PriceRangeMessage)]
+    public decimal Price { get; set; }
+
+    [Required]
+    public string ImageURL { get; set; }
+
+    public int CategoryId { get; set; }
+}
---
+++ Entity Framework Core/Workshops/PetStore/Web/PetsStore.Web/Controllers/ProductsController.cs
@@ -0,0 +1,86 @@
+﻿namespace PetsStore.Web.Controllers;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Data.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Services.Data;
+using Services.Mapping;
+using ViewModels.Products;
+
+public class ProductsController : BaseController
+{
+    private readonly IProductsService productsService;
+    private readonly ICategoryService categoryService;
+
+    public ProductsController(IProductsService productsService, ICategoryService categoryService)
+    {
+        this.productsService = productsService;
+        this.categoryService = categoryService;
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        ICollection<ListCategoriesOnProductCreateViewModel> allCategories = this.categoryService
+            .All()
+            .To<ListCategoriesOnProductCreateViewModel>()
+            .ToArray();
+
+        return this.View(allCategories);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProductInputModel inputModel)
+    {
+        if (!this.ModelState.IsValid)
+        {
+            return this.RedirectToAction("Create", "Products");
+        }
+
+        if (!this.categoryService.ExistsById(inputModel.CategoryId))
+        {
+            return this.RedirectToAction("Create", "Products");
+        }
+
+        Product product = AutoMapperConfig.MapperInstance.Map<Product>(inputModel);
+        await this.productsService.AddProduct(product);
+
+        return this.RedirectToAction("All", "Products");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(string id)
+    {
+        Product product = await this.productsService.GetById(id);
+
+        if (product == null)
+        {
+            return this.RedirectToAction("Error", "Home");
+        }
+
+        DetailsProductViewModel viewModel = AutoMapperConfig.MapperInstance.Map<DetailsProductViewModel>(product);
+
+        return this.View(viewModel);
+    }
+
+    [HttpGet]
+    public IActionResult All(string search)
+    {
+        AllProductsViewModel viewModel = new AllProductsViewModel()
+        {
+           AllProducts = this.productsService
+               .GetAllByName(search)
+               .To<ListAllProductsViewModel>()
+               .ToArray(),
+           Categories = this.productsService
+               .GetAllProductsCategories(),
+           SearchQuery = search,
+        };
+
+        return this.View(viewModel);
+    }
+}
