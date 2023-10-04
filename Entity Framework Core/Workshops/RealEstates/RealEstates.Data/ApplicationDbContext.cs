428d6c0e839cd70c4409a8d8160a9992fe39a6e2
Denis Yordanov <deyordanov777@gmail.com>
2023-10-04 11:48:15 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿namespace RealEstates.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() { }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<BuildingType> BuildingTypes { get; set; } = null!;
+        public DbSet<District> Districts { get; set; } = null!;
+        public DbSet<Property> Properties { get; set; } = null!;
+        public DbSet<PropertyType> PropertyTypes { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace RealEstates.Data;
+
+public class Configuration
+{
+    public const string ConnectionString = @"Server=localhost,1433;Database=ApplicationDb;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True";
+
+}
