aa93975ae151b69593ec4d2d00bce93254d953a2
Denis Yordanov <deyordanov777@gmail.com>
2023-10-04 11:48:25 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Data/Migrations/20230922041405_PropertyPropertiesUpdate2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstates.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PropertyPropertiesUpdate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "TotalFloors",
+                table: "Properties",
+                type: "tinyint",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Floor",
+                table: "Properties",
+                type: "tinyint",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalFloors",
+                table: "Properties",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Floor",
+                table: "Properties",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldNullable: true);
+        }
+    }
+}
