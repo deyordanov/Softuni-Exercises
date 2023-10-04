04168cb7b3ad0cd79eb11a817bae3072574608dd
Denis Yordanov <deyordanov777@gmail.com>
2023-10-04 11:48:32 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Data/Migrations/20230922055908_TagImportanceNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstates.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagImportanceNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Importance",
+                table: "Tags",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Importance",
+                table: "Tags",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
