ff0f3e83fd106a886730621a248d555caa7a4542
Denis Yordanov <deyordanov777@gmail.com>
2023-10-04 11:48:18 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Data/Migrations/20230921171702_AddedPropertyImportanceToTag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstates.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPropertyImportanceToTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Importance",
+                table: "Tags",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Importance",
+                table: "Tags");
+        }
+    }
+}
