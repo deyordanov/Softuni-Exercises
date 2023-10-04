942f32274b0f53dde93b797ba2ee18c7351bff11
Denis Yordanov <deyordanov777@gmail.com>
2023-10-04 11:48:22 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Data/Migrations/20230922041014_PropertyPropertiesUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstates.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PropertyPropertiesUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
