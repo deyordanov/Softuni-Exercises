bc988cf99d29be1f36cd6a976cb4c0cf8acac9c8
Denis Yordanov <deyordanov777@gmail.com>
2023-10-04 11:48:29 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Data/Migrations/20230922041956_PropertyPriceNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstates.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PropertyPriceNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Properties",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Properties",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
