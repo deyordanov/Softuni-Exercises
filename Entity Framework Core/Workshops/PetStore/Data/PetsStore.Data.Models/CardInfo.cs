c38d0ee19dcf009bd7b42c580de30693b8161224
Denis Yordanov <deyordanov777@gmail.com>
2023-10-05 11:49:33 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/PetStore/Data/PetsStore.Data.Models/CardInfo.cs
@@ -0,0 +1,34 @@
+﻿namespace PetsStore.Data.Models;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using PetsStore.Data.Common.Models;
+using PetsStore.Data.Models.Common;
+
+public class CardInfo : BaseDeletableModel<string>
+{
+    public CardInfo()
+    {
+        this.Id = Guid.NewGuid().ToString();
+    }
+
+    [MaxLength(CardInfoValidationConstants.CardNumberMaxLength)]
+    public string CardNumber { get; set; } = null!;
+
+    [MaxLength(CardInfoValidationConstants.ExpirationDateMaxLength)]
+    public string ExpirationDate { get; set; } = null!;
+
+    [MaxLength(CardInfoValidationConstants.CardHolderMaxLength)]
+    public string CardHolder { get; set; } = null!;
+
+    // ReSharper disable once InconsistentNaming
+    [MaxLength(CardInfoValidationConstants.CvcMaxLength)]
+    public string CVC { get; set; } = null!;
+
+    [ForeignKey(nameof(Client))]
+    public string ClientId { get; set; }
+
+    public virtual Client Client { get; set; }
+}
---
+++ Entity Framework Core/Workshops/PetStore/Data/PetsStore.Data/Migrations/20230926045602_FixedGrammaticalError.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetsStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedGrammaticalError : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ClientCards_AspNetUsers_ClientIdd",
+                table: "ClientCards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClientCards_ClientIdd",
+                table: "ClientCards");
+
+            migrationBuilder.RenameColumn(
+                name: "ClientIdd",
+                table: "ClientCards",
+                newName: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientCards_ClientId",
+                table: "ClientCards",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ClientCards_AspNetUsers_ClientId",
+                table: "ClientCards",
+                column: "ClientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ClientCards_AspNetUsers_ClientId",
+                table: "ClientCards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClientCards_ClientId",
+                table: "ClientCards");
+
+            migrationBuilder.RenameColumn(
+                name: "ClientId",
+                table: "ClientCards",
+                newName: "ClientIdd");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientCards_ClientIdd",
+                table: "ClientCards",
+                column: "ClientIdd",
+                unique: true,
+                filter: "[ClientIdd] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ClientCards_AspNetUsers_ClientIdd",
+                table: "ClientCards",
+                column: "ClientIdd",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
