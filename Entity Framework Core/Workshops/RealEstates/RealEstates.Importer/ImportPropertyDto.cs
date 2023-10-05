1aed5c3e4f4e766997f90cb4fec8a302d100b8f0
Denis Yordanov <deyordanov777@gmail.com>
2023-10-05 11:49:00 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Importer/ImportPropertyDto.cs
@@ -0,0 +1,15 @@
+﻿namespace RealEstates.Importer;
+
+public class ImportPropertyDto
+{
+    public string Url { get; set; } = null!;
+    public int Size { get; set; }
+    public int YardSize { get; set; }
+    public int Floor { get; set; }
+    public int TotalFloors { get; set; }
+    public string District { get; set; } = null!;
+    public int Year { get; set; }
+    public string Type { get; set; } = null!;
+    public string BuildingType { get; set; } = null!;
+    public int Price { get; set; }
+}
---
+++ Entity Framework Core/Workshops/RealEstates/RealEstates.Importer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using RealEstates.Data;
+using RealEstates.Importer;
+using RealEstates.Services;
+using RealEstates.Services.Contracts;
+
+ApplicationDbContext context = new ApplicationDbContext();
+IPropertiesService service = new PropertiesService(context);
+
+string json = File.ReadAllText("../../../Properties1.json.txt");
+
+ImportJson(json);
+
+void ImportJson(string jsonFile)
+{
+    ImportPropertyDto[]? importedDtos = JsonConvert.DeserializeObject<ImportPropertyDto[]>(jsonFile);
+
+    foreach (ImportPropertyDto dto in importedDtos)
+    {
+        service.Add(dto.Size, dto.YardSize, dto.Floor, dto.TotalFloors,
+            dto.District, dto.Year, dto.Type,
+            dto.BuildingType, dto.Price);
+    }
+}
