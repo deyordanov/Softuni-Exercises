7b7a012d0ffb45bb2a544d15526e3fd330446189
Denis Yordanov <deyordanov777@gmail.com>
2023-10-05 11:49:41 +0300

    Added A New Exercise

---
+++ Entity Framework Core/Workshops/PetStore/Data/PetsStore.Data.Models/Address.cs
@@ -0,0 +1,32 @@
+﻿namespace PetsStore.Data.Models;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using PetsStore.Data.Common.Models;
+using Common;
+
+public class Address : BaseDeletableModel<string>
+{
+    public Address()
+    {
+        this.Id = Guid.NewGuid().ToString();
+
+        this.Clients = new HashSet<Client>();
+    }
+
+    [MaxLength(AddressValidationConstants.AddressTextMaxLength)]
+    public string AddressText { get; set; } = null!;
+
+    [MaxLength(AddressValidationConstants.TownNameMaxLength)]
+    public string TownName { get; set; } = null!;
+
+    public virtual ICollection<Client> Clients { get; set; }
+
+    [ForeignKey(nameof(Store))]
+    public string StoreId { get; set; }
+
+    public virtual Store Store { get; set; }
+}
