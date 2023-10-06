1fe3e476bb153448262727e4210eec0cc814226d
Denis Yordanov <deyordanov777@gmail.com>
2023-10-06 11:50:00 +0300

    Added A New Exercise

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/15. Export Cars From Make Toyota/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using Data.ExportProfiles;
+    using DTOs.Export;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetCarsFromMakeToyota(context));
+        }
+
+        public static string GetCarsFromMakeToyota(CarDealerContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new CarProfile()));
+
+            IMapper mapper = new Mapper(config);
+
+            List<CarDto> dtos = context.Cars
+                .AsNoTracking()
+                .Where(c => c.Make == "Toyota")
+                .ProjectTo<CarDto>(mapper.ConfigurationProvider)
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .ToList();
+
+            return JsonConvert.SerializeObject(dtos, Formatting.Indented);
+        }
+    }
+}
---
+++ Entity Framework Core/JavaScript Object Notation - JSON/16. Export Local Suppliers/Data/ExportProfiles/SupplierProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealer.Data.ExportProfiles;
+
+using AutoMapper;
+using DTOs.Export;
+using Models;
+
+public class SupplierProfile : Profile
+{
+    public SupplierProfile()
+    {
+        CreateMap<Supplier, SupplierDto>()
+            .ForMember(dest => dest.PartsCount,
+                opt => opt.MapFrom(src => src.Parts.Count));
+    }
+}
