20572ba5e7f6abf542d2f5d55e58e08c2a28a5e3
Denis Yordanov <deyordanov777@gmail.com>
2023-10-08 08:12:14 +0300

    Added A New Exercise

---
+++ Data Structures Fundamentals/MoreDataStructures/AVLTree/IAVLTree.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+namespace AVLTree;
+
+public interface IAVLTree<T>
+    where T : IComparable<T>
+{
+    void Insert(T value);
+    void Delete(T value);
+    bool Search(T value);
+    T GetMinValue(CustomAVLTree<T>.Node node);
+    T GetMaxValue(CustomAVLTree<T>.Node node);
+    IEnumerable<T> PreOrderTraversal();
+    IEnumerable<T> InOrderTraversal();
+    IEnumerable<T> PostOrderTraversal();
+    IEnumerable<T> BFS();
+}
---
+++ Data Structures Fundamentals/MoreDataStructures/AVLTree/Testing.cs
@@ -0,0 +1,67 @@
+﻿namespace AVLTree
+{
+    internal class Testing
+    {
+        static void Main(string[] args)
+        {
+            CustomAVLTree<int> tree = new CustomAVLTree<int>();
+
+            // Test Insertion
+            tree.Insert(50);
+            tree.Insert(60);
+            tree.Insert(70);
+            tree.Insert(20);
+            tree.Insert(10);
+            tree.Insert(25);
+            tree.Insert(5);
+
+            Console.WriteLine("In-order Traversal after insertion:");
+            foreach (var item in tree.InOrderTraversal())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+
+            // Test Search
+            Console.WriteLine("Search for 25: " + tree.Search(25));  // Should be true
+            Console.WriteLine("Search for 100: " + tree.Search(100));  // Should be false
+
+            // Test Min and Max values
+            Console.WriteLine("Min Value: " + tree.GetMinValue(tree.root));
+            Console.WriteLine("Max Value: " + tree.GetMaxValue(tree.root));
+
+            // Test Deletion
+            tree.Delete(5);
+            tree.Delete(60);
+
+            Console.WriteLine("In-order Traversal after deletion:");
+            foreach (var item in tree.InOrderTraversal())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+
+            // Test other Traversals
+            Console.WriteLine("Pre-order Traversal:");
+            foreach (var item in tree.PreOrderTraversal())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Post-order Traversal:");
+            foreach (var item in tree.PostOrderTraversal())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("BFS/Level-order Traversal:");
+            foreach (var item in tree.BFS())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
