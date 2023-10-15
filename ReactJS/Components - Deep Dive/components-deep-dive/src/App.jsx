696886f4da8c000208831e58a098313b27a1ff8f
Denis Yordanov <deyordanov777@gmail.com>
2023-10-15 21:52:24 +0300

    Added A New Exercise

--- ReactJS/Components - Deep Dive/components-deep-dive/src/App.jsx
+++ ReactJS/Components - Deep Dive/components-deep-dive/src/App.jsx
@@ -1,13 +1,38 @@
 import "./App.css";
-import { movies } from "./movies";
+// import { movies as movieData } from "./movies";
 import MovieList from "./Components/MovieList";
+import { useState, useEffect } from "react";
 
 function App() {
+  // const [movies, setMovies] = useState(movieData);
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5173/moviesJson.json")
+      .then((response) => response.json())
+      .then((movieData) => {
+        console.log(movieData);
+        setMovies(movieData);
+      });
+  }, []);
+
+  const onMovieDelete = (id) => {
+    setMovies((state) => state.filter((x) => x.id !== id));
+  };
+
+  const onMovieSelect = (id) => {
+    setMovies((state) => state.map((x) => ({ ...x, selected: x.id === id })));
+  };
+
   return (
     <div>
       <h1>Movie Collection:</h1>
-
-      <MovieList movies={movies.slice(0, 30)} />
+      <MovieList
+        movies={movies}
+        onMovieDelete={onMovieDelete}
+        onMovieSelect={onMovieSelect}
+      />
     </div>
   );
 }
