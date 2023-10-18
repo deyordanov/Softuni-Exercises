12d94820cb3bafd4daea7058d08b1d7af4895e4b
Denis Yordanov <deyordanov777@gmail.com>
2023-10-18 20:18:49 +0300

    Added A New Exercise

---
+++ ReactJS/Components - Workshop/client/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="logo">
+                <span className="course">React Course - October 2023</span>
+                <span className="description">User List Demo</span>
+            </div>
+        </header>
+    );
+}
