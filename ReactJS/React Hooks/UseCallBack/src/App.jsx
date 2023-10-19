9ec178e307c72ab36a6f6f59646f2921cba4b86c
Denis Yordanov <deyordanov777@gmail.com>
2023-10-19 20:09:10 +0300

    Added A New Exercise

--- ReactJS/React Hooks/UseCallBack/src/App.jsx
+++ ReactJS/React Hooks/UseCallBack/src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import List from "./Components/List";
 
 function App() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  const getItems = () => {
-    return [number, number + 1, number + 2];
-  };
+  //Storing the function itself based on whether the 'number' variable has changed its value
+  //Instead of creating the function every time we re-render the component, we can do it only when the 'number' changes
+  const getItems = useCallback(
+    (incrementer) => {
+      return [number, number + incrementer, number + incrementer * 2];
+    },
+    [number]
+  );
 
   const theme = {
     backgroundColor: dark ? "#333" : "#FFF",
--- ReactJS/React Hooks/UseCallBack/src/Components/List.jsx
+++ ReactJS/React Hooks/UseCallBack/src/Components/List.jsx
@@ -4,7 +4,7 @@ export default function List({ getItems }) {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    setItems(getItems());
+    setItems(getItems(5));
     console.log("Updating Items!");
   }, [getItems]);
 
