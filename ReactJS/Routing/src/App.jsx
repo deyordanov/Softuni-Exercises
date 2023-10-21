9ae578a1f29a4445b80b2f4a26e42ad22f5a17d5
Denis Yordanov <deyordanov777@gmail.com>
2023-10-21 20:19:48 +0300

    Added A New Exercise

--- ReactJS/Routing/src/App.jsx
+++ ReactJS/Routing/src/App.jsx
@@ -3,14 +3,35 @@ import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Error from "./Components/Error";
+import Navigation from "./Components/Navigation";
+import CharacterList from "./Components/CharacterList";
+import Character from "./Components/Character";
+import Movie from "./Components/Movie";
+import Starship from "./Components/Starship";
+import Vehicle from "./Components/Vehicle";
 
 function App() {
     return (
         <>
+            <Navigation />
             <Routes>
                 <Route path="*" element={<Error />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/characters" element={<CharacterList />} />
+                <Route
+                    path="/characters/:characterId/*"
+                    element={<Character />}
+                />
+                <Route path="/movies/:movieId" element={<Movie />}></Route>
+                <Route
+                    path="/starships/:starshipId"
+                    element={<Starship />}
+                ></Route>
+                <Route
+                    path="/vehicles/:vehicleId"
+                    element={<Vehicle />}
+                ></Route>
             </Routes>
         </>
     );
