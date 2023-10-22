1d6f78ed07544623f27ffab270e590b6ba46d24c
Denis Yordanov <deyordanov777@gmail.com>
2023-10-22 08:04:19 +0300

    Added A New Exercise

--- ReactJS/Routing/src/Components/Navigation.jsx
+++ ReactJS/Routing/src/Components/Navigation.jsx
@@ -1,64 +1,14 @@
 import style from "./Navigation.module.css";
-import { Link, NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
 
-export default function Navigation() {
+export default function Navigation({ children }) {
     return (
-        <nav className={style.navigation}>
-            <ul
-                style={{
-                    listStyleType: "none",
-                }}
-            >
-                {/* With anchor tags we will always refresh the page */}
-                {/* <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li> */}
-
-                {/* This way we do not refresh the page  */}
-                {/* NavLink -> +style && +className */}
-                <li>
-                    <NavLink
-                        style={({ isActive }) => ({
-                            color: isActive ? "lime" : "",
-                        })}
-                        // className={({ isActive }) =>
-                        //     isActive ? style["nav-active"] : ""
-                        // }
-                        to={"/"}
-                    >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        style={({ isActive }) => ({
-                            color: isActive ? "lime" : "",
-                        })}
-                        // className={({ isActive }) =>
-                        //     isActive ? style["nav-active"] : ""
-                        // }
-                        to={"/about"}
-                    >
-                        About
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        style={({ isActive }) => ({
-                            color: isActive ? "lime" : "",
-                        })}
-                        // className={({ isActive }) =>
-                        //     isActive ? style["nav-active"] : ""
-                        // }
-                        to={"/characters"}
-                    >
-                        Characters
-                    </NavLink>
-                </li>
-            </ul>
+        <nav>
+            <ul className={style.navigation}>{children}</ul>
         </nav>
     );
 }
+
+Navigation.propTypes = {
+    children: PropTypes.array,
+};
