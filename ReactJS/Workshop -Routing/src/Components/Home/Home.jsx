4ddd5927a36efad431d6571fff56a8fce7c9356c
Denis Yordanov <deyordanov777@gmail.com>
2023-10-22 19:00:47 +0300

    Added A New Exercise

---
+++ ReactJS/Workshop -Routing/src/Components/Home/Home.jsx
@@ -0,0 +1,79 @@
+export default function Home() {
+  return (
+    <section id="welcome-world">
+      <div className="welcome-message">
+        <h2>ALL new games are</h2>
+        <h3>Only in GamesPlay</h3>
+      </div>
+      <img
+        src="./images/DBD-Alien.png"
+        alt="hero"
+        width={"900px"}
+        height={"900px"}
+      />
+
+      <div id="home-page">
+        <h1>Latest Games</h1>
+
+        {/* <!-- Display div: with information about every game (if any) --> */}
+        <div className="game">
+          <div className="image-wrap">
+            <img src="./images/CoverFire.png" />
+          </div>
+          <h3>Cover Fire</h3>
+          <div className="rating">
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+          </div>
+          <div className="data-buttons">
+            <a href="#" className="btn details-btn">
+              Details
+            </a>
+          </div>
+        </div>
+        <div className="game">
+          <div className="image-wrap">
+            <img src="./images/ZombieLang.png" />
+          </div>
+          <h3>Zombie Lang</h3>
+          <div className="rating">
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+          </div>
+          <div className="data-buttons">
+            <a href="#" className="btn details-btn">
+              Details
+            </a>
+          </div>
+        </div>
+        <div className="game">
+          <div className="image-wrap">
+            <img src="./images/MineCraft.png" />
+          </div>
+          <h3>MineCraft</h3>
+          <div className="rating">
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+          </div>
+          <div className="data-buttons">
+            <a href="#" className="btn details-btn">
+              Details
+            </a>
+          </div>
+        </div>
+
+        {/* <!-- Display paragraph: If there is no games  --> */}
+        <p className="no-articles">No games yet</p>
+      </div>
+    </section>
+  );
+}
