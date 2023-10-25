de87cf6c308ea9ba7ad0bf70d4dbfb4a690d05f1
Denis Yordanov <deyordanov777@gmail.com>
2023-10-25 06:09:49 +0300

    Added A New Exercise

---
+++ ReactJS/React Hooks/src/services/todoService.js
@@ -0,0 +1,22 @@
+import * as requester from "./requestser";
+
+const baseUrl = "http://localhost:3030/jsonstore/todos";
+
+export const getAll = async () => {
+    const data = await requester.get(baseUrl);
+
+    return Object.values(data);
+};
+
+export const updateTodo = async (data) => {
+    const response = await requester.put(
+        JSON.stringify(data),
+        `${baseUrl}/${data._id}`
+    );
+
+    return response;
+};
+
+export const create = async (data) => {
+    await requester.post(JSON.stringify(data), baseUrl);
+};
