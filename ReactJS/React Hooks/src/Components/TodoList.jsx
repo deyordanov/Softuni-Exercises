d5bf6c24a7522942c9ff67a8d869cb80be5259c2
Denis Yordanov <deyordanov777@gmail.com>
2023-10-25 17:53:45 +0300

    Added A New Exercise

--- ReactJS/React Hooks/src/Components/TodoList.jsx
+++ ReactJS/React Hooks/src/Components/TodoList.jsx
@@ -2,16 +2,11 @@ import ListGroup from "react-bootstrap/ListGroup";
 import PropTypes from "prop-types";
 import TodoItem from "./TodoItem";
 
-export default function TodoList({ todos, onTodoUpdate, onTodoDelete }) {
+export default function TodoList({ todos }) {
     return (
         <ListGroup className="w-[50%] shadow-xl shadow-rose-500 border-solid border-black border-2">
             {todos.map((x) => (
-                <TodoItem
-                    key={x._id}
-                    {...x}
-                    onTodoUpdate={onTodoUpdate}
-                    onTodoDelete={onTodoDelete}
-                />
+                <TodoItem key={x._id} {...x} />
             ))}
         </ListGroup>
     );
