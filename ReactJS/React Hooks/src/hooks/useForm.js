95cccf18420780b6c81fdc3b9fa5945b86f9397d
Denis Yordanov <deyordanov777@gmail.com>
2023-10-25 07:19:42 +0300

    Added A New Exercise

--- ReactJS/React Hooks/src/hooks/useForm.js
+++ ReactJS/React Hooks/src/hooks/useForm.js
@@ -1,3 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import * as todoService from "../services/todoService";
+import { useNavigate } from "react-router-dom";
 
-export const useForm = () => {};
+//Trying to make this as generic as possible
+export const useForm = (initialFormValues) => {
+    const [formValues, setFormValues] = useState(initialFormValues);
+    const navigator = useNavigate();
+
+    const onChangeHandler = (e) => {
+        setFormValues((state) => ({
+            ...state,
+            [e.target.name]:
+                e.target.name === "isCompleted"
+                    ? !state.isCompleted
+                    : e.target.value,
+        }));
+    };
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        todoService
+            .create(formValues)
+            .then(navigator("/"))
+            .catch((error) => console.log(error));
+    };
+
+    return { formValues, onChangeHandler, onSubmitHandler };
+};
