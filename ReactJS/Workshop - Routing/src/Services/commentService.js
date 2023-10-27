3fe30646681d94f68f6983bdff08e0a62c86e167
Denis Yordanov <deyordanov777@gmail.com>
2023-10-27 20:51:58 +0300

    Added A New Exercise

--- ReactJS/Workshop - Routing/src/Services/commentService.js
+++ ReactJS/Workshop - Routing/src/Services/commentService.js
@@ -1,6 +1,14 @@
 import * as requester from "./requester";
 
-const baseUrl = "http://localhost:3030/jsonstore/comments";
+const baseUrl = "http://localhost:3030/data/comments";
+
+const getAuthHeaders = () => {
+    const token = JSON.parse(localStorage.getItem("auth")).accessToken;
+    return {
+        "Content-Type": "application/json",
+        "X-Authorization": token,
+    };
+};
 
 export const getAll = async () => {
     const response = await requester.get(baseUrl);
@@ -9,20 +17,20 @@ export const getAll = async () => {
 };
 
 export const create = async (data) => {
-    const response = await requester.post(JSON.stringify(data), baseUrl);
+    const headers = getAuthHeaders();
+
+    const response = await requester.authorizationPost(
+        headers,
+        JSON.stringify(data),
+        baseUrl
+    );
 
     return response;
 };
 
 export const getGameComments = async (gameId) => {
-    const comments = await getAll();
-
-    // const query = encodeURIComponent(`gameId="${gameId}"`);
-    // const gameComments = await requester.get(`${baseUrl}?where=${query}`);
-
-    const gameComments = Object.values(comments).filter(
-        (x) => x.gameId === gameId
-    );
+    const query = encodeURIComponent(`gameId="${gameId}"`);
+    const gameComments = await requester.get(`${baseUrl}?where=${query}`);
 
     return gameComments;
 };
