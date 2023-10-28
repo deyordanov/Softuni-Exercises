d2f4f66d8a261c1ee93ec421fff1e18d577b80d7
Denis Yordanov <deyordanov777@gmail.com>
2023-10-28 09:43:09 +0300

    Added A New Exercise

--- ReactJS/Workshop - Routing/src/Components/Catalogue/CatalogueItem/Details/Comment/Comment.jsx
+++ ReactJS/Workshop - Routing/src/Components/Catalogue/CatalogueItem/Details/Comment/Comment.jsx
@@ -1,16 +1,37 @@
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 
-export default function Comment({ author, comment }) {
+export default function Comment({ comment, onCommentLike, userId }) {
+    const [liked, setLiked] = useState(false);
+
+    useEffect(() => {
+        setLiked(comment.likedBy.includes(userId));
+    }, [comment.likedBy, userId]);
+
     return (
-        <li className="comment bg-slate-500 flex items-center justify-center">
-            <p className="rounded-xl text-2xl text-center">
-                <b>{author}</b>: {comment}
+        <li className="comment bg-slate-500 flex items-center justify-center flex-col max-w-[300px] overflow-hidden break-all px-8 relative py-6">
+            <p className="rounded-xl text-2xl text-center mb-3">
+                <b>{comment.author}</b>: {comment.comment}
             </p>
+
+            <div className="flex absolute bottom-0 right-0 gap-2.5 items-center">
+                <p className="text-2xl p-0">{comment.likes || 0}</p>
+                <FontAwesomeIcon
+                    onClick={() => onCommentLike(comment, setLiked)}
+                    className={`${
+                        liked ? "text-red-500" : "text-slate-100"
+                    } text-4xl cursor-pointer`}
+                    icon={faHeart}
+                />
+            </div>
         </li>
     );
 }
 
 Comment.propTypes = {
-    author: PropTypes.string,
-    comment: PropTypes.string,
+    comment: PropTypes.object,
+    onCommentLike: PropTypes.func,
+    userId: PropTypes.string,
 };
