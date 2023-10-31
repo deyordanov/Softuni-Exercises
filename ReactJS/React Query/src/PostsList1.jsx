4b786e29867a2f6b8d578688269754254eba6b87
Denis Yordanov <deyordanov777@gmail.com>
2023-10-31 07:37:55 +0200

    Added A New Exercise

---
+++ ReactJS/React Query/src/PostsList1.jsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPosts } from "./api/posts";
+
+export default function PostsList1() {
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading....</h1>;
+  if (postsQuery.isError) return <h1>{JSON.stringify(postsQuery.error)}</h1>;
+
+  return (
+    <div>
+      <h1>Posts List 1</h1>
+      <ol>
+        {Object.values(postsQuery.data).map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
