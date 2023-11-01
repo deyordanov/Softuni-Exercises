50be612f1c11d98d1fdc1906715ed0b3bf5810fa
Denis Yordanov <deyordanov777@gmail.com>
2023-11-01 12:47:11 +0200

    Added A New Exercise

--- ReactJS/Workshop - Routing/src/main.jsx
+++ ReactJS/Workshop - Routing/src/main.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 
+import { AppQueryProvider } from "./Contexts/AppQueryContext.jsx";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-            <App />
+            <AppQueryProvider>
+                <App />
+            </AppQueryProvider>
         </BrowserRouter>
     </QueryClientProvider>
 );
