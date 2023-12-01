9876c0a642ccc2e464445584c3f410a3da239ddf
Denis Yordanov <deyordanov777@gmail.com>
2023-12-01 21:43:03 +0200

    Added A New Exercise

---
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/InvoicesProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace Invoices
+{
+    using Data.Models;
+    using DataProcessor.ImportDto.Clients;
+    using DataProcessor.ImportDto.Invoices;
+    using DataProcessor.ImportDto.Products;
+
+    public class InvoicesProfile : Profile
+    {
+        public InvoicesProfile()
+        {
+            CreateMap<InvoiceDto, Invoice>();
+
+            CreateMap<ProductDto, Product>();
+        }
+    }
+
+
+}
