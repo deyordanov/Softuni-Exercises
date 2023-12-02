27292a6d76078a52572b32a16f7b99d6917bf3ab
Denis Yordanov <deyordanov777@gmail.com>
2023-12-02 07:45:30 +0200

    Added A New Exercise

---
+++ Entity Framework Core/Exams/C# DB Advanced Exam - 01 April 2023/DataProcessor/ImportDto/Creators/ImportCreatorDto.cs
@@ -0,0 +1,25 @@
+﻿using Boardgames.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.DataProcessor.ImportDto.Creators;
+
+using System.Xml.Serialization;
+
+[XmlType("Creator")]
+public class ImportCreatorDto
+{
+    [Required]
+    [XmlElement(nameof(FirstName))]
+    [MinLength(ValidationConstants.CreatorFirstNameMinLength)]
+    [MaxLength(ValidationConstants.CreatorFirstNameMaxLength)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [XmlElement(nameof(LastName))]
+    [MinLength(ValidationConstants.CreatorLastNameMinLength)]
+    [MaxLength(ValidationConstants.CreatorLastNameMaxLength)]
+    public string LastName { get; set; } = null!;
+
+    [XmlArray(nameof(Boardgames))]
+    public ImportBoardgameDto[] Boardgames { get; set; } = null!;
+}
