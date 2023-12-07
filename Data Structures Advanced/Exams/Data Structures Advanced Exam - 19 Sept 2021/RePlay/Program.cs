0440ec1f58c051d5ff044b6580ec43d8f8d7427b
Denis Yordanov <deyordanov777@gmail.com>
2023-12-07 18:37:41 +0200

    Added A New Exercise

---
+++ Data Structures Advanced/Exams/Data Structures Advanced Exam - 19 Sept 2021/RePlay/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exam.RePlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RePlayer rePlayer = new RePlayer();
+
+            Track track = new Track("asd", "bsd", "csd", 100, 1);
+            Track track2 = new Track("dsd", "esd", "csd", 200, 2);
+            Track track3 = new Track("hsd", "isd", "jsd", 300, 5);
+            Track track4 = new Track("ksd", "lsd", "msd", 500, 4);
+            Track track5 = new Track("nsd", "osd", "csd", 400, 3);
+
+            rePlayer.AddTrack(track, "randomAlbum");
+            rePlayer.AddTrack(track2, "bandomAlbum");
+            rePlayer.AddTrack(track3, "aandomAlbum2");
+            rePlayer.AddTrack(track4, "aandomAlbum2");
+            rePlayer.AddTrack(track5, "aandomAlbum2");
+
+            var a = rePlayer.GetTracksInDurationRangeOrderedByDurationThenByPlaysDescending(2, 5);
+
+            ;
+        }
+    }
+}
