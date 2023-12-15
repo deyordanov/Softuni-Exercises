780da943da246a2f433855f88d8af32bb9428a85
Denis Yordanov <deyordanov777@gmail.com>
2023-12-15 08:48:35 +0200

    Added A New Exercise

---
+++ ASP.NET Fundamentals/ASP.NET Core Introduction/SimpleChat/Controllers/ChatController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleChat.Controllers;
+
+using ViewModels;
+
+public class ChatController : Controller
+{
+    private static List<KeyValuePair<string, string>> messages = new List<KeyValuePair<string, string>>();
+
+    public IActionResult Show()
+    {
+        if (messages.Count < 1)
+        {
+            return View(new ChatViewModel());
+        }
+
+        ChatViewModel chat = new ChatViewModel()
+        {
+            Messages = messages
+                .Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                })
+                .ToList()
+        };
+
+        return View(chat);
+    }
+
+    [HttpPost]
+    public IActionResult Send(ChatViewModel chat)
+    {
+        MessageViewModel newMessage = chat.CurrentMessage;
+
+        messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+
+        return RedirectToAction("Show");
+    }
+}
---
+++ ASP.NET Fundamentals/ASP.NET Core Introduction/SimpleChat/ViewModels/ChatViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleChat.ViewModels;
+
+public class ChatViewModel
+{
+    public MessageViewModel CurrentMessage { get; set; } = null!;
+
+    public List<MessageViewModel> Messages { get; set; } = null!;
+}
