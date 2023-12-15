1b8847c23f52a85689f54f6ae956ebd6688dca58
Denis Yordanov <deyordanov777@gmail.com>
2023-12-15 08:49:01 +0200

    Added A New Exercise

---
+++ ASP.NET Fundamentals/ASP.NET Core Introduction/SimplePages/Seeding/ProductsData.cs
@@ -0,0 +1,17 @@
+﻿namespace ASP.NET_Core_Introduction.Seeding;
+
+using ViewModels;
+
+public static class ProductsData
+{
+    public static IEnumerable<ProductViewModel> Products =
+        new List<ProductViewModel>()
+        {
+            new ProductViewModel(Guid.NewGuid(), "Bread", 1.20m),
+            new ProductViewModel(Guid.NewGuid(), "Milk", 2.5m),
+            new ProductViewModel(Guid.NewGuid(), "Bananas", 4.20m),
+            new ProductViewModel(Guid.NewGuid(), "Chocolate", 7.00m),
+            new ProductViewModel(Guid.NewGuid(), "Oats", 3.20m),
+            new ProductViewModel(Guid.NewGuid(), "Peanut Butter", 4.50m),
+        };
+}
---
+++ ASP.NET Fundamentals/ASP.NET Core Introduction/SimplePages/ViewModels/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NET_Core_Introduction.ViewModels;
+
+public class ProductViewModel(Guid id, string name, decimal price)
+{
+    public Guid Id { get; set; } = id;
+
+    public string Name { get; set; } = name;
+
+    public decimal Price { get; set; } = price;
+}
