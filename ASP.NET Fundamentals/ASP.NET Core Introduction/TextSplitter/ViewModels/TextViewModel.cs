f3b1782c4435bf9a579810570387ffd16280770f
Denis Yordanov <deyordanov777@gmail.com>
2023-12-16 10:39:00 +0200

    Added A New Exercise

---
+++ ASP.NET Fundamentals/ASP.NET Core Introduction/TextSplitter/ViewModels/TextViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TextSplitter.ViewModels;
+
+using System.ComponentModel.DataAnnotations;
+
+public class TextViewModel
+{
+    [Required]
+    [MaxLength(30)]
+    [MinLength(2)]
+    public string Text { get; set; } = null!;
+
+    public string SplitText { get; set; } = null!;
+}
