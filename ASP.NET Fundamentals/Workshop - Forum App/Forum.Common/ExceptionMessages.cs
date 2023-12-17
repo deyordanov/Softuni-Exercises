317ac765cdf4abac746dc0ba5f2a29997a39e229
Denis Yordanov <deyordanov777@gmail.com>
2023-12-17 06:31:05 +0200

    Added A New Exercise

---
+++ ASP.NET Fundamentals/Workshop - Forum App/Forum.Common/ExceptionMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace Forum.Common;
+
+public static class ExceptionMessages
+{
+    public static class Post
+    {
+        public const string UnexpectedErrorWhenAddingPostMessage = "Unexpected error occurred while adding your post!";
+        public const string UnexpectedErrorWhenEditingPostMessage = "Unexpected error occurred while editing your post!";
+
+        public const string InvalidPostIdMessage = "Post with such id has not been found!";
+    }
+}
---
+++ ASP.NET Fundamentals/Workshop - Forum App/Forum.Services/Contracts/IProductService.cs
@@ -0,0 +1,17 @@
+﻿namespace Forum.Services.Contracts;
+
+using Data.Models;
+using ViewModels.Post;
+
+public interface IProductService
+{
+    Task<IEnumerable<PostListViewModel>> ListAllAsync();
+
+    Task AddPostAsync (PostFormModel model);
+
+    Task<PostFormModel> GetPostById(string id);
+
+    Task EditPostByIdAsync (string id, PostFormModel model);
+
+    Task DeletePostByIdAsync (string id);
+}
