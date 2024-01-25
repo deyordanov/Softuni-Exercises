138a38ffdddab66ea9a6da5ca17047109341ded1
Denis Yordanov <deyordanov777@gmail.com>
2024-01-25 04:02:34 +0200

    Added A New Exercise

---
+++ Containers and Cloud/Exam/deyordanov-task-2/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.89.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_service_plan" "sp" {
+  name                = var.service_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "lwa" {
+  name                = var.linux_web_app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.sp.id
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.mssql.name},1433;Initial Catalog=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.mssql.administrator_login};Password=${azurerm_mssql_server.mssql.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+
+  site_config {
+    application_stack {
+      dotnet_version = var.application_stack_dotnet_version
+    }
+    always_on = false
+  }
+}
+
+resource "azurerm_app_service_source_control" "sc" {
+  repo_url               = var.source_control_repo_url
+  branch                 = "main"
+  use_manual_integration = true
+  app_id                 = azurerm_linux_web_app.lwa.id
+}
+
+resource "azurerm_mssql_server" "mssql" {
+  name                         = var.mssql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  administrator_login          = var.mssql_server_administrator_login
+  administrator_login_password = var.mssql_server_administrator_login_password
+  version                      = "12.0"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name           = var.mssql_database_name
+  server_id      = azurerm_mssql_server.mssql.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_firewall_rule" "dbf" {
+  name             = var.mssql_firewall_rule_name
+  server_id        = azurerm_mssql_server.mssql.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
