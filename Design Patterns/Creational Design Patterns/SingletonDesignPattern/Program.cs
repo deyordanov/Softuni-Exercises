4d3ab3491aa0ae177ca3a86313a02dd8a564375f
Denis Yordanov <deyordanov777@gmail.com>
2024-03-27 22:10:56 +0200

    Added A New Exercise

---
+++ Design Patterns/Creational Design Patterns/SingletonDesignPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using SingletonDesignPattern;
+
+Thread thread1 = new Thread(() =>
+{
+    TestSingleton("Thread1");
+});
+
+Thread thread2 = new Thread(() =>
+{
+    TestSingleton("Thread2");
+});
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+static void TestSingleton(string value)
+{
+    Singleton singleton = Singleton.Instance(value);
+    Console.WriteLine(singleton.Value);
+}
