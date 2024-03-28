3abf96151d73a165bb88a798ad567ed42f76ccc3
Denis Yordanov <deyordanov777@gmail.com>
2024-03-28 07:03:44 +0200

    Added A New Exercise

---
+++ Design Patterns/CreationalDesignPatterns/PrototypeDesignPattern/Vehicles/Car.cs
@@ -0,0 +1,44 @@
+﻿namespace PrototypeDesignPattern.Vehicles;
+
+using System.Text;
+
+public class Car : Vehicle
+{
+    private readonly string _topSpeed;
+
+    public Car(
+        string brand,
+        string model,
+        string color,
+        string topSpeed)
+        : base(
+            brand,
+            model,
+            color)
+    {
+        this._topSpeed = topSpeed;
+    }
+
+    public Car(Car vehicle)
+        : base(vehicle)
+    {
+        this._topSpeed = vehicle._topSpeed;
+    }
+
+    public override Vehicle Clone()
+        => new Car(this);
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+
+        sb
+            .AppendLine(base.ToString())
+            .AppendLine($"Top Speed: {this._topSpeed}");
+
+        return sb
+            .ToString()
+            .TrimEnd();
+    }
+}
---
+++ Design Patterns/CreationalDesignPatterns/PrototypeDesignPattern/Vehicles/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿namespace PrototypeDesignPattern.Vehicles;
+
+using System.Text;
+
+public abstract class Vehicle
+{
+    private readonly string _brand;
+    private readonly string _model;
+    private readonly string _color;
+
+    protected Vehicle(
+        string brand, 
+        string model,
+        string color)
+    {
+        this._brand = brand;
+        this._model = model;
+        this._color = color;
+    }
+
+    protected Vehicle(Vehicle vehicle)
+    {
+        this._brand = vehicle._brand;
+        this._model = vehicle._model;
+        this._color = vehicle._color;
+    }
+
+    public abstract Vehicle Clone();
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb
+            .AppendLine($"Brand: {this._brand}")
+            .AppendLine($"Model: {this._model}")
+            .AppendLine($"Color: {this._color}");
+
+        return sb
+            .ToString()
+            .TrimEnd();
+    }
+}
