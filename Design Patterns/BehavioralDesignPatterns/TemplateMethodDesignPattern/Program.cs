8dd444b6e8ef08f8943bbfb9ac65a6e729b3f70f
Denis Yordanov <deyordanov777@gmail.com>
2024-03-29 10:15:28 +0200

    Added A New Exercise

---
+++ Design Patterns/BehavioralDesignPatterns/TemplateMethodDesignPattern/Program.cs
@@ -0,0 +1,7 @@
+﻿using TemplateMethodDesignPattern.GameLoaders;
+
+BaseGameLoader deadByDaylightLoader = new DeadByDaylightLoader();
+deadByDaylightLoader.Load();
+
+BaseGameLoader ghostOfTsushimaLoader =  new GhostOfTsushimaLoader();
+ghostOfTsushimaLoader.Load();
