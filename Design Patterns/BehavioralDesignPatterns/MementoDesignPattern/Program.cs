ad2eb81c9559997e4e6e4a3462ded820b92f207c
Denis Yordanov <deyordanov777@gmail.com>
2024-03-30 22:18:00 +0200

    Added A New Exercise

---
+++ Design Patterns/BehavioralDesignPatterns/MementoDesignPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using MementoDesignPattern;
+
+Editor editor = new Editor();
+editor.Write("Like and");
+editor.PrintText();
+editor.Write("Like and Subscribe");
+editor.PrintText();
+editor.Write("Like and Subscribe to Geekific!");
+editor.PrintText();
+editor.Undo();
+editor.PrintText();
+editor.Undo();
+editor.PrintText();
+editor.Undo();
+editor.PrintText();
