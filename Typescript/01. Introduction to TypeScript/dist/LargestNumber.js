2282533655c1abaaef85cbde9185a461008588bf
Denis Yordanov <deyordanov777@gmail.com>
2024-06-02 20:46:18 +0300

    Added A New Exercise

---
+++ Typescript/01. Introduction to TypeScript/dist/LargestNumber.js
@@ -0,0 +1,6 @@
+function getLargestNumber(first, second, third) {
+    var largestNumber = Math.max(first, second, third);
+    console.log("The largest number is ".concat(largestNumber, "."));
+}
+// getLargestNumber(5, -3, 16);
+getLargestNumber(-3, -5, -22.5);
---
+++ Typescript/01. Introduction to TypeScript/src/LargestNumber.ts
@@ -0,0 +1,7 @@
+function getLargestNumber(first: number, second: number, third: number): void {
+  const largestNumber: number = Math.max(first, second, third);
+  console.log(`The largest number is ${largestNumber}.`);
+}
+
+// getLargestNumber(5, -3, 16);
+getLargestNumber(-3, -5, -22.5);
